114b4a98a02ebab78d24148d419c46f2d826ea9d
Sunny Sidhu <ssidhu@and.digital>
2022-11-15 15:16:47 +0000

    GOV.UK frontend template

--- DfE.PlanTechnologyForYourSchool.WebApp/Program.cs
+++ DfE.PlanTechnologyForYourSchool.WebApp/Program.cs
@@ -1,7 +1,10 @@
+using GovUk.Frontend.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddGovUkFrontend();
 
 var app = builder.Build();
 
