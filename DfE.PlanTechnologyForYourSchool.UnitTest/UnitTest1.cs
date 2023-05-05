0f2cab82a0dcbbfcc6913ba41e8611c92dd4a70e
Martin Sanders <martin.sanders@and.digital>
2023-05-05 16:06:06 +0100

    added: nuget library for xunit

---
+++ DfE.PlanTechnologyForYourSchool.UnitTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace DfE.PlanTechnologyForYourSchool.UnitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/Sts.PlanTech.Application.Tests/TemplateTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Sts.PlanTech.Application.Tests;
+
+public class TemplateTest
+{
+    [Fact]
+    public void Should_Do_Something_When_True()
+    {
+        // Arrange.
+        int A = 1;
+        int Expected = 3;
+
+        // Act.
+        int Actual = A + 2;
+
+        // Assert.
+        Assert.Equal(Expected, Actual);
+    }
+}
