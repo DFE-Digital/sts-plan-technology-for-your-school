f7966f36b4b5c889a33fc93f479e2c62c79e8851
Jim Washbrook <jim.washbrook@and.digital>
2023-05-25 14:42:59 +0100

    feat: Remove controller area, refactor index

---
+++ src/Dfe.PlanTech.Web/Controllers/PagesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Dfe.PlanTech.Application.Content.Queries;
+using Dfe.PlanTech.Domain.Core.Enums;
+using Dfe.PlanTech.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfe.PlanTech.Web.Controllers;
+
+public class PagesController : Controller
+{
+    private readonly ILogger<PagesController> _logger;
+
+    public PagesController(ILogger<PagesController> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<IActionResult> Index([FromServices] GetPageQuery getPageQuery)
+    {
+        var page = await getPageQuery.GetPageBySlug(nameof(Pages.Landing));
+
+        return View("Page", page);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
--- src/Dfe.PlanTech.Web/Program.cs
+++ src/Dfe.PlanTech.Web/Program.cs
@@ -16,7 +16,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Pages}/{action=Index}/{id?}");
 
 app.Run();
