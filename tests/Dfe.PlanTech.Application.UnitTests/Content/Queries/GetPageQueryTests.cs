63f8829ce3b7b5fabb305d89ea8daf38e335d5a6
Jim Washbrook <jim.washbrook@and.digital>
2023-05-25 17:15:28 +0100

    tests: Added assertions

--- tests/Dfe.PlanTech.Application.UnitTests/Content/Queries/GetPageQueryTests.cs
+++ tests/Dfe.PlanTech.Application.UnitTests/Content/Queries/GetPageQueryTests.cs
@@ -1,26 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Dfe.PlanTech.Application.Content.Queries;
 using Dfe.PlanTech.Application.Persistence.Interfaces;
 using Dfe.PlanTech.Domain.Content.Models;
 using Dfe.PlanTech.Infrastructure.Application.Models;
 using Moq;
-using Xunit;
 
 namespace Dfe.PlanTech.Application.UnitTests.Content.Queries;
 
 public class GetPageQueryTests
 {
+    private const string LANDING_PAGE_SLUG = "LandingPage";
     private readonly Mock<IContentRepository> _repoMock = new();
 
-    private List<Page> _mockData = new() {
+    private readonly List<Page> _mockData = new() {
         new Page(){
             Slug = "Index"
         },
         new Page(){
-            Slug = "LandingPage"
+            Slug = LANDING_PAGE_SLUG
         },
         new Page(){
             Slug = "AuditStart"
@@ -32,9 +28,15 @@ public class GetPageQueryTests
         _repoMock.Setup(client => client.GetEntities<Page>(It.IsAny<IEnumerable<IContentQuery>>(), It.IsAny<CancellationToken>()))
         .ReturnsAsync((IEnumerable<IContentQuery> queries, CancellationToken token) =>
         {
-            var pages = _mockData;
+            foreach (var query in queries)
+            {
+                if (query is ContentQueryEquals equalsQuery && query.Field == "fields.slug")
+                {
+                    return _mockData.Where(page => page.Slug == equalsQuery.Value);
+                }
+            }
 
-            return pages;
+            return Array.Empty<Page>();
         }).Verifiable();
     }
 
@@ -43,10 +45,12 @@ public class GetPageQueryTests
     {
         var query = new GetPageQuery(_repoMock.Object);
 
-        var slug = "LandingPage";
+        var result = await query.GetPageBySlug(LANDING_PAGE_SLUG);
 
-        var result = await query.GetPageBySlug(slug);
+        Assert.NotNull(result);
 
         _repoMock.VerifyAll();
+
+        Assert.Equal(LANDING_PAGE_SLUG, result.Slug);
     }
 }
