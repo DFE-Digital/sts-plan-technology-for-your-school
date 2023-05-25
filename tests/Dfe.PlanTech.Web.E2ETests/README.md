125dbbdfccd0b8031aed50473fdc51973d8ea11e
jimwashbrook <131891854+jimwashbrook@users.noreply.github.com>
2023-05-25 16:13:37 +0100

    feat: Add phase header/beta header (#18)
    * feat: Add beta header to layout
    
    * feat: e2e testing
    
    * docs: Update UI Testing ADR
    
    * minor: Remove unused files

---
+++ tests/Dfe.PlanTech.Web.E2ETests/README.md
@@ -0,0 +1,14 @@
+# Dfe.PlanTech.Web.E2ETests
+
+## Overview
+
+End-to-end testing for the [Dfe.PlanTech.Web](../../src/Dfe.PlanTech.Web/) project.
+
+Created using [Cypress](https://cypress.io)
+
+## Setup
+
+1. Copy the `cypress.env.json.example` file and rename it to `cypress.env.json` in the root of the `Dfe.PlanTech.Web.E2ETests` folder
+2. Populate the variables within it
+3. Install the necessary packages by running `npm install`
+4. Run Cypress by running `npx cypress open`
---
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/beta-header.cy.js
@@ -0,0 +1,32 @@
+describe("template spec", () => {
+  const url = Cypress.env("URL");
+
+  const expectedFeedbackLink = "https://feedback";
+
+  const phaseBannerSelector = "div.govuk-phase-banner";
+
+  beforeEach(() => {
+    cy.visit(url);
+  });
+
+  it("displays phase banner", () => {
+    cy.get(phaseBannerSelector).should("exist").and("be.visible");
+  });
+
+  it("should display 'Beta' tag", () => {
+    const tag = cy.get(`${phaseBannerSelector} strong.govuk-tag`);
+
+    tag.should("exist").and("be.visible");
+
+    tag.should("have.text", "Beta");
+  });
+
+  it("should link to feedback url", () => {
+    const feedbackLink = cy.get(`${phaseBannerSelector} a`);
+
+    feedbackLink
+      .should("exist")
+      .and("have.attr", "href")
+      .and("include", expectedFeedbackLink);
+  });
+});
