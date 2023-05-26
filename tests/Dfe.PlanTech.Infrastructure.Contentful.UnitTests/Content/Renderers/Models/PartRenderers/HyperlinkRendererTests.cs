c347a2a3c2f9b3347880dbd5e1a6854d4c49a7ee
Jim Washbrook <jim.washbrook@and.digital>
2023-05-26 16:24:44 +0100

    tests: Check reject for invalid content

--- tests/Dfe.PlanTech.Infrastructure.Contentful.UnitTests/Content/Renderers/Models/PartRenderers/HyperlinkRendererTests.cs
+++ tests/Dfe.PlanTech.Infrastructure.Contentful.UnitTests/Content/Renderers/Models/PartRenderers/HyperlinkRendererTests.cs
@@ -35,7 +35,23 @@ public class HyperlinkRendererTests
 
         Assert.True(accepted);
     }
-    
+
+    [Fact]
+    public void Should_Reject_When_NotHyperlink()
+    {
+        var content = new RichTextContent()
+        {
+            NodeType = "paragraph",
+            Value = "paragraph text"
+        };
+
+        var renderer = new HyperlinkRenderer(new HyperlinkRendererOptions());
+
+        var accepted = renderer.Accepts(content);
+
+        Assert.False(accepted);
+    }
+
     [Fact]
     public void Should_CreateLink_When_PassedValidData()
     {
@@ -62,4 +78,54 @@ public class HyperlinkRendererTests
         Assert.Contains($"<a href=\"{url}\"", html);
         Assert.Contains($">{linkText}</a>", html);
     }
+
+    [Fact]
+    public void Should_NotAddLink_When_MissingURI()
+    {
+        var renderer = new HyperlinkRenderer(new HyperlinkRendererOptions());
+        var rendererCollection = new RichTextRenderer(new[] { renderer });
+
+        const string linkText = "Click Here";
+
+        var content = new RichTextContent()
+        {
+            NodeType = NODE_TYPE,
+            Value = linkText,
+        };
+
+        var result = renderer.AddHtml(content, rendererCollection, new StringBuilder());
+
+        var html = result.ToString();
+
+        Assert.Empty(html);
+    }
+
+    [Fact]
+    public void Should_AddClasses_When_OptionsContainsClasses()
+    {
+        const string testClasses = "testing-classes";
+
+        var renderer = new HyperlinkRenderer(new HyperlinkRendererOptions() { Classes = testClasses });
+        var rendererCollection = new RichTextRenderer(new[] { renderer });
+
+        const string linkText = "Click Here";
+        const string url = "https://www.test-url.com";
+
+        var content = new RichTextContent()
+        {
+            NodeType = NODE_TYPE,
+            Value = linkText,
+            Data = new RichTextData()
+            {
+                Uri = url
+            }
+        };
+
+        var result = renderer.AddHtml(content, rendererCollection, new StringBuilder());
+
+        var html = result.ToString();
+
+        Assert.Contains($"class=\"{testClasses}\"", html);
+    }
+
 }
