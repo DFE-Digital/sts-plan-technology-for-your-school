49483f8a8b5de712c4cde973d80056edec57180f
PeterShipstoneAND <peter.shipstone@and.digital>
2023-05-26 10:17:58 +0100

    ran TF fmt and updated pipeline so it fails if the format is incorrect

--- terraform/main-hosting.tf
+++ terraform/main-hosting.tf
@@ -4,7 +4,7 @@ module "main_hosting" {
   environment    = local.environment
   project_name   = local.project_name
   azure_location = local.azure_location
-  tags = local.tags
+  tags           = local.tags
 
   enable_container_registry = false
   image_name                = "my-app"
