a9d3b812c9c142fa9366e3bcbca0960e4d616a4f
PeterShipstoneAND <peter.shipstone@and.digital>
2023-05-26 11:31:29 +0100

    ran "terraform fmt" to fix format issue

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  project_name    = var.project_name
-  environment     = var.environment
-  azure_location  = var.azure_location
+  project_name   = var.project_name
+  environment    = var.environment
+  azure_location = var.azure_location
   tags = {
     "Environment"      = var.az_tag_environment,
     "Service Offering" = var.az_tag_product,
