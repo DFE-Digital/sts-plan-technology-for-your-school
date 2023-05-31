1af59009802181a35c28cd931838229c64f2db0f
Jim Washbrook <jim.washbrook@and.digital>
2023-05-31 14:33:39 +0100

    fix: Merge issue

--- src/Dfe.PlanTech.Web/Controllers/PagesController.cs
+++ src/Dfe.PlanTech.Web/Controllers/PagesController.cs
@@ -36,9 +36,3 @@ public class PagesController : Controller
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
-}
