bf8c6d621a348edd8a1710a9731b4bf1c506b27e
Jim Washbrook <jim.washbrook@and.digital>
2023-06-01 13:22:08 +0100

    feat: Improved component views factory start

--- src/Dfe.PlanTech.Web/Helpers/ComponentViewsFactory.cs
+++ src/Dfe.PlanTech.Web/Helpers/ComponentViewsFactory.cs
@@ -1,11 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Extensions.Hosting.Internal;
 
 namespace Dfe.PlanTech.Web.Helpers;
 
-public static class ComponentViewsFactory
+public class ComponentViewsFactory
 {
-    public static string GetViewForType(object model) => $"Components/{model.GetType().Name}";
+    private readonly ComponentViewsFactoryOptions _options;
+
+    public ComponentViewsFactory(ComponentViewsFactoryOptions options)
+    {
+        _options = options;
+    }
+
+    public bool TryGetViewForType(object model, out string viewPath)
+    {
+        var types = Assembly.GetExecutingAssembly().GetTypes();
+
+        viewPath = "";
+
+        return true;
+    }
+}
+
+public class ComponentViewsFactoryOptions
+{
+    public string[] ComponentViewPaths { get; init; } = Array.Empty<string>();
 }
