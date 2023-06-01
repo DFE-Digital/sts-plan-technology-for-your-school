725bdb0ae54123fa762edc3d05288e8d5f18f2b1
Martin Sanders <martin.sanders@and.digital>
2023-06-01 14:56:42 +0100

    feat: Added component for displaying drop down

---
+++ src/Dfe.PlanTech.Domain/Content/Models/ComponentDropDown.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Dfe.PlanTech.Domain.Content.Models
+{
+	public class ComponentDropDown : ContentComponent
+	{
+        public string Title { get; init; } = null!;
+        public RichTextContent Content { get; init; } = null!;
+    }
+}
+
