4a5d4dabc00f904ae7ccddefa5a99c07fc280aa4
jimwashbrook <131891854+jimwashbrook@users.noreply.github.com>
2023-06-01 11:20:32 +0100

    feat: Missing component type for non-found entity (#29)
    * feat: Missing component type for non-found entity
    
    * fix: Add logger factory -> create logger
    
    * tests: Add NullLoggerFactory

---
+++ src/Dfe.PlanTech.Domain/Content/Models/MissingComponent.cs
@@ -0,0 +1,5 @@
+namespace Dfe.PlanTech.Domain.Content.Models;
+
+public class MissingComponent : ContentComponent
+{
+}
--- src/Dfe.PlanTech.Infrastructure.Contentful/Persistence/EntityResolver.cs
+++ src/Dfe.PlanTech.Infrastructure.Contentful/Persistence/EntityResolver.cs
@@ -1,13 +1,41 @@
 using Contentful.Core.Configuration;
 using Dfe.PlanTech.Domain.Content.Interfaces;
+using Dfe.PlanTech.Domain.Content.Models;
+using Microsoft.Extensions.Logging;
 
 namespace Dfe.PlanTech.Infrastructure.Contentful;
 
+/// <summary>
+/// For mapping fields/properties of type <see chref="IContentComponent"/> to their concrete type,
+/// when serialising the returned API response from Contentful
+/// </summary>
 public class EntityResolver : IContentTypeResolver
 {
+    private readonly ILogger<IContentTypeResolver> _logger;
+
     public Dictionary<string, Type> _types = typeof(IContentComponent).Assembly.GetTypes()
                                                                         .Where(type => type.IsAssignableTo(typeof(IContentComponent)))
                                                                         .ToDictionary(type => type.Name.ToLower());
 
-    public Type Resolve(string contentTypeId) => _types.TryGetValue(contentTypeId.ToLower(), out var type) ? type : throw new Exception($"Could not find type for {contentTypeId}");
+    public EntityResolver(ILogger<IContentTypeResolver> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Returns matching type for ID, or <see chref="MissingComponent"/> if none found.
+    /// </summary>
+    /// <param name="contentTypeId"></param>
+    /// <returns></returns>
+    public Type Resolve(string contentTypeId)
+    {
+        if (_types.TryGetValue(contentTypeId.ToLower(), out var type))
+        {
+            return type;
+        }
+
+        _logger.LogWarning("Could not find content type for ID {contentTypeId}", contentTypeId);
+
+        return typeof(MissingComponent);
+    }
 }
