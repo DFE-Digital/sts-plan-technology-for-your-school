0809a0c75bd973aaa5fd522a6271e832c1571083
Martin Sanders <martin.sanders@and.digital>
2023-06-05 15:24:05 +0100

    fix: formatting style updated

--- src/Dfe.PlanTech.Domain/Content/Models/ComponentDropDown.cs
+++ src/Dfe.PlanTech.Domain/Content/Models/ComponentDropDown.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+
 namespace Dfe.PlanTech.Domain.Content.Models
 {
+    /// <summary>
+    /// Model for DropDown type.
+    /// </summary>
 	public class ComponentDropDown : ContentComponent
 	{
+        /// <summary>
+        /// The title to display.
+        /// </summary>
         public string Title { get; init; } = null!;
+
+        /// <summary>
+        /// The Content to display.
+        /// </summary>
         public RichTextContent Content { get; init; } = null!;
     }
 }
