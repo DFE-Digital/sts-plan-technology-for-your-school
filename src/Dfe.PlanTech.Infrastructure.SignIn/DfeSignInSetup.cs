0081a32443a1f2c76af06ec9ff0199ffb074a6ce
Jim Washbrook <jim.washbrook@and.digital>
2023-06-07 15:51:34 +0100

    feat: Initial project

---
+++ src/Dfe.PlanTech.Infrastructure.SignIn/DfeSignInSetup.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dfe.PlanTech.Infrastructure.SignIn;
+
+public static class DfeSignInSetup
+{
+    public static IServiceCollection AddDfeSignIn(this IServiceCollection services){
+        
+    }
+}
