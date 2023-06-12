47dcb2eb05b269ce65f00a87551e2855761af9c5
Jim Washbrook <jim.washbrook@and.digital>
2023-06-12 09:40:24 +0100

    minor: Refactor + comments

--- src/Dfe.PlanTech.Web/Middleware/UrlHistoryMiddleware.cs
+++ src/Dfe.PlanTech.Web/Middleware/UrlHistoryMiddleware.cs
@@ -2,6 +2,9 @@ using Dfe.PlanTech.Application.Caching.Interfaces;
 
 namespace Dfe.PlanTech.Web.Middleware;
 
+/// <summary>
+/// Adds user page change history to cache. 
+/// </summary>
 public class UrlHistoryMiddleware
 {
     public const string CACHE_KEY = "UrlHistory";
@@ -21,28 +24,42 @@ public class UrlHistoryMiddleware
 
         bool navigatingBackwards = pageHistory.TryPeek(out string? lastVisitedHistory) && !string.IsNullOrEmpty(lastVisitedHistory) && lastVisitedHistory.Contains(targetUrl);
 
-        if (navigatingBackwards)
+        switch (navigatingBackwards)
         {
-            pageHistory.Pop();
-        }
-
-        if (!navigatingBackwards)
-        {
-            var lastUrl = httpContext.Request.Headers["Referer"].ToString();
-
-            if (!string.IsNullOrEmpty(lastUrl))
-            {
-                bool isDuplicateUrl = pageHistory.TryPeek(out lastVisitedHistory) && !string.IsNullOrEmpty(lastVisitedHistory) && lastVisitedHistory.Equals(lastUrl);
-
-                if (!isDuplicateUrl)
+            case true:
                 {
-                    pageHistory.Push(lastUrl);
-                    cacher.Set(CACHE_KEY, TimeSpan.FromHours(1), pageHistory);
+                    pageHistory.Pop();
+                    break;
                 }
-            }
+
+            case false:
+                {
+                    TryAddHistory(httpContext, cacher, pageHistory, lastVisitedHistory);
+                    break;
+                }
         }
 
         await _next(httpContext);
     }
 
+    /// <summary>
+    /// Double check we're not adding duplicate history (i.e. refresh, submit, etc.) - if not, add to history.
+    /// </summary>
+    private static void TryAddHistory(HttpContext httpContext, ICacher cacher, Stack<string> pageHistory, string? lastVisitedHistory)
+    {
+        var lastUrl = httpContext.Request.Headers["Referer"].ToString();
+
+        if (string.IsNullOrEmpty(lastUrl))
+        {
+            return;
+        }
+
+        bool isDuplicateUrl = pageHistory.TryPeek(out lastVisitedHistory) && !string.IsNullOrEmpty(lastVisitedHistory) && lastVisitedHistory.Equals(lastUrl);
+
+        if (!isDuplicateUrl)
+        {
+            pageHistory.Push(lastUrl);
+            cacher.Set(CACHE_KEY, TimeSpan.FromHours(1), pageHistory);
+        }
+    }
 }
