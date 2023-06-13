dc5a832682ac0b34bad271dc4f26769a557259d3
Jim Washbrook <jim.washbrook@and.digital>
2023-06-13 18:28:24 +0100

    feat: Add cancellation tokens

--- src/Dfe.PlanTech.Web/Controllers/QuestionsController.cs
+++ src/Dfe.PlanTech.Web/Controllers/QuestionsController.cs
@@ -21,11 +21,11 @@ public class QuestionsController : BaseController<QuestionsController>
     /// <param name="query"></param>
     /// <exception cref="ArgumentNullException">Throws exception when Id is null or empty</exception>
     /// <returns></returns>
-    public async Task<IActionResult> GetQuestionById(string id, [FromServices] GetQuestionQuery query)
+    public async Task<IActionResult> GetQuestionById(string id, CancellationToken cancellationToken, [FromServices] GetQuestionQuery query)
     {
         if (string.IsNullOrEmpty(id)) throw new ArgumentNullException(nameof(id));
 
-        var question = await query.GetQuestionById(id) ?? throw new KeyNotFoundException($"Could not find question with id {id}");
+        var question = await query.GetQuestionById(id, cancellationToken) ?? throw new KeyNotFoundException($"Could not find question with id {id}");
         
         var viewModel = new QuestionViewModel()
         {
