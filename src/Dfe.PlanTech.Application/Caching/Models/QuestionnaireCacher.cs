9aebf26bbabdc8745f8d3beddcfcc3753b8b8b07
Jim Washbrook <jim.washbrook@and.digital>
2023-06-15 10:23:12 +0100

    Add interface

--- src/Dfe.PlanTech.Application/Caching/Models/QuestionnaireCacher.cs
+++ src/Dfe.PlanTech.Application/Caching/Models/QuestionnaireCacher.cs
@@ -3,11 +3,16 @@ using Dfe.PlanTech.Domain.Caching.Models;
 
 namespace Dfe.PlanTech.Application.Caching.Models;
 
-public class QuestionnaireCacher
+public class QuestionnaireCacher : IQuestionnaireCacher
 {
     private const string CACHE_KEY = "QuestionnaireCache";
 
     private readonly ICacher _cacher;
+
+    public QuestionnaireCacher(ICacher cacher)
+    {
+        _cacher = cacher;
+    }
 
     public QuestionnaireCache? Cached => _cacher.Get(CACHE_KEY, () => new QuestionnaireCache());
 
