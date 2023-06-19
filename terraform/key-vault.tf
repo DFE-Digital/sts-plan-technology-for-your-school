bd925cf867e69244c6febfa3db97ca24fdf9fc20
Jim Washbrook <jim.washbrook@and.digital>
2023-06-19 12:00:44 +0100

    iac: Restrict KV access

--- terraform/key-vault.tf
+++ terraform/key-vault.tf
@@ -1,11 +1,17 @@
 resource "azurerm_key_vault" "vault" {
-  name                       = "${local.environment}${local.project_name}-kv"
-  location                   = local.azure_location
-  resource_group_name        = "${local.environment}${local.project_name}"
-  tenant_id                  = data.azurerm_client_config.current.tenant_id
-  sku_name                   = "standard"
-  soft_delete_retention_days = 90
-  enable_rbac_authorization  = false
+  name                          = "${local.environment}${local.project_name}-kv"
+  location                      = local.azure_location
+  resource_group_name           = "${local.environment}${local.project_name}"
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  sku_name                      = "standard"
+  soft_delete_retention_days    = 90
+  enable_rbac_authorization     = false
+  public_network_access_enabled = false
+
+  network_acls {
+    bypass                     = "AzureServices"
+    virtual_network_subnet_ids = local.subnet_name
+  }
 }
 
 resource "azurerm_key_vault_access_policy" "vault_access_policy_tf" {
