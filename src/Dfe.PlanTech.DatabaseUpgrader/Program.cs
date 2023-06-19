8fc65e8a80f577446d87baf62db141121ff715a5
Martin Sanders <martin.sanders@and.digital>
2023-06-19 13:06:22 +0100

    Added validation and reworked fild structure.

--- src/Dfe.PlanTech.DatabaseUpgrader/Program.cs
+++ src/Dfe.PlanTech.DatabaseUpgrader/Program.cs
@@ -1,39 +1,64 @@
 ﻿using DbUp;
 using System.Reflection;
 
-
-
 /// <summary>
-/// Prototype DPUP.
+/// PlanTech Database Upgrader.
 /// </summary>
-
 internal class Program
 {
+    const int ERROR_RESULT = -1;
+    const int SUCCESS_RESULT = 0;
+
     private static int Main(string[] args)
     {
-        const string connectionString = "";
+        if (args == null || !args.Any())
+        {
+            DisplayError("Please supply a connection string.");
+        }
 
-        var upgrader =
-            DeployChanges.To
-                .SqlDatabase(connectionString)
-                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
-                .LogToConsole()
-                .Build();
+        var connectionString = args[0];
+
+        var result = MigrateDatabase(connectionString);
+
+        return result ? SUCCESS_RESULT : ERROR_RESULT;
+    }
+
+    private static bool MigrateDatabase(string connectionString)
+    {
+        var upgrader = DeployChanges.To
+            .SqlDatabase(connectionString)
+            .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+            .LogToConsole()
+            .Build();
 
         var result = upgrader.PerformUpgrade();
 
         if (!result.Successful)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(result.Error);
-            Console.ResetColor();
-#if DEBUG
-            Console.ReadLine();
-#endif
-            return -1;
+            DisplayError(result.Error.Message, result.Error);
+            return false;
         }
-        Console.WriteLine("Success!");
-        Console.WriteLine(result);
-        return 0;
+
+        DisplaySuccess("Success!");
+
+        return true;
+    }
+
+    private static void DisplaySuccess(string successMessage)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(successMessage);
+        Console.ResetColor();
+    }
+
+    private static void DisplayError(string errorMessage, Exception? exception = null)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(errorMessage);
+        if (exception != null)
+        {
+            Console.WriteLine(exception.StackTrace);
+        }
+        Console.ResetColor();
     }
 }
