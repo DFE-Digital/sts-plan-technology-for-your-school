fd15178df2cd39f3fb8368ecf39f8ef53c6a4da0
Jim Washbrook <jim.washbrook@and.digital>
2023-06-22 09:49:28 +0100

    Formatting fix

--- terraform/key-vault.tf
+++ terraform/key-vault.tf
@@ -1,12 +1,12 @@
 resource "azurerm_key_vault" "vault" {
-  name                          = local.kv_name
-  location                      = local.azure_location
-  resource_group_name           = "${local.environment}${local.project_name}"
-  tenant_id                     = data.azurerm_client_config.current.tenant_id
-  sku_name                      = "standard"
-  soft_delete_retention_days    = 90
-  enable_rbac_authorization     = false
-  tags                          = local.tags
+  name                       = local.kv_name
+  location                   = local.azure_location
+  resource_group_name        = "${local.environment}${local.project_name}"
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 90
+  enable_rbac_authorization  = false
+  tags                       = local.tags
 }
 
 resource "azurerm_key_vault_access_policy" "vault_access_policy_tf" {
