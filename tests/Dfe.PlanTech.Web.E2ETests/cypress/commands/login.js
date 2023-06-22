4dbfd98743d64c0c8996834182f2c7ed2af7ac9b
Jim Washbrook <jim.washbrook@and.digital>
2023-06-22 17:20:12 +0100

    Login before self-assessment

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/commands/login.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/commands/login.js
@@ -4,3 +4,10 @@ Cypress.Commands.add("login", (email, password, url) => {
     cy.get("input#password").type(password);
     cy.get("div.govuk-button-group button.govuk-button").first().click();
 });
+
+Cypress.Commands.add("loginWithEnv", (url) => {
+    cy.visit(url);
+    cy.get("input#username").type(Cypress.env("Username"));
+    cy.get("input#password").type(Cypress.env("Password"));
+    cy.get("div.govuk-button-group button.govuk-button").first().click();
+});
