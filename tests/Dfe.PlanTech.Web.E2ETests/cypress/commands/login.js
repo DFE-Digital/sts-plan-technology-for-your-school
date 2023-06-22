f2ffeb12c9b57c4e5478e69b2e8c407e82a52dc2
Jim Washbrook <jim.washbrook@and.digital>
2023-06-22 17:21:15 +0100

    Login with env refactor

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/commands/login.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/commands/login.js
@@ -5,9 +5,4 @@ Cypress.Commands.add("login", (email, password, url) => {
     cy.get("div.govuk-button-group button.govuk-button").first().click();
 });
 
-Cypress.Commands.add("loginWithEnv", (url) => {
-    cy.visit(url);
-    cy.get("input#username").type(Cypress.env("Username"));
-    cy.get("input#password").type(Cypress.env("Password"));
-    cy.get("div.govuk-button-group button.govuk-button").first().click();
-});
+Cypress.Commands.add("loginWithEnv", (url) => cy.login(Cypress.env("Username"), Cypress.env("Password"), url));
