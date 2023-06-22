69fb43fdf3333d6c5b207911a693681adc76ac74
Jim Washbrook <jim.washbrook@and.digital>
2023-06-22 17:47:47 +0100

    revert all changes

--- terraform/key-vault.tf
+++ terraform/key-vault.tf
@@ -1,19 +1,18 @@
 resource "azurerm_key_vault" "vault" {
-  name                       = local.kv_name
-  location                   = local.azure_location
-  resource_group_name        = "${local.environment}${local.project_name}"
-  tenant_id                  = data.azurerm_client_config.current.tenant_id
-  sku_name                   = "standard"
-  soft_delete_retention_days = 90
-  enable_rbac_authorization  = false
-  tags                       = local.tags
+  name                          = local.kv_name
+  location                      = local.azure_location
+  resource_group_name           = "${local.environment}${local.project_name}"
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  sku_name                      = "standard"
+  soft_delete_retention_days    = 90
+  enable_rbac_authorization     = false
+  tags                          = local.tags
 }
 
 resource "azurerm_key_vault_access_policy" "vault_access_policy_tf" {
   key_vault_id = azurerm_key_vault.vault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
-  depends_on   = [null_resource.keyvault-assign-identity]
+  object_id    = local.current_user_id
 
   secret_permissions = ["List", "Get", "Set"]
   key_permissions    = ["List", "Get", "Create"]
