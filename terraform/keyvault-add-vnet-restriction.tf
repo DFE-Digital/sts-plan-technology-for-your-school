51c3022c514457a7bef1d2dbd457c40a8190d4f1
Jim Washbrook <jim.washbrook@and.digital>
2023-06-26 17:03:24 +0100

    amend depends on

--- terraform/keyvault-add-vnet-restriction.tf
+++ terraform/keyvault-add-vnet-restriction.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "keyvault-add-vnet-restriction" {
-  depends_on = [azurerm_key_vault_access_policy.vault_access_policy_tf]
+  depends_on = [module.main_hosting]
 
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
