a5bdfc54fd5d391fc1731032c17a027d3efb6511
Jim Washbrook <jim.washbrook@and.digital>
2023-06-27 14:48:04 +0100

    back button test

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/interstitial-page.cy.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/interstitial-page.cy.js
@@ -2,20 +2,32 @@ describe("landing page", () => {
     const url = Cypress.env("URL") + "/broadband-connection";
     const selfassessmentUrl = Cypress.env("URL") + "/self-assessment";
 
-    beforeEach(() => {
-        cy.loginWithEnv(url);
-    });
-
     it("should have content", () => {
+        cy.loginWithEnv(url);
+
         cy.origin(Cypress.env("URL"), () => {
             cy.get("rich-text").should("exist");
         });
     });
 
     it("should have button which links to a question", () => {
+        cy.loginWithEnv(url);
+
         cy.origin(Cypress.env("URL"), () => {
             cy.get("a.govuk-button.govuk-link").should("exist");
             cy.get("a.govuk-button.govuk-link").should("have.attr", "href").and("include", "question");
+        });
+    });
+
+    it("should link back to self-assessment", () => {
+        cy.loginWithEnv(selfassessmentUrl);
+
+        cy.origin(Cypress.env("URL"), () => {
+
+            cy.get("ul.app-task-list__items  a").first().click();
+
+            cy.get("a.govuk-back-link").should("exist");
+            cy.get("a.govuk-back-link").should("have.attr", "href").and("include", "self-assessment");
         });
     });
 });
