efacd24106f95f9cb6b54880ab11478a2e39e9d7
Iain STANGER <Iain.STANGER@EDUCATION.GOV.UK>
2023-06-27 16:20:44 +0100

    code smell fixes

--- src/Dfe.PlanTech.Domain/Content/Models/Options/TextRendererOptions.cs
+++ src/Dfe.PlanTech.Domain/Content/Models/Options/TextRendererOptions.cs
@@ -25,7 +25,7 @@ public class TextRendererOptions
         return matchingOption;
     }
 
-    public static IEnumerable<string> GetOpenTagHtml(MarkOption option)
+    public IEnumerable<string> GetOpenTagHtml(MarkOption option)
     {
         yield return option.HtmlTag;
 
