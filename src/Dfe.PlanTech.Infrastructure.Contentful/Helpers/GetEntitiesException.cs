81bb706ff1d04ba6b38c16c33c053399e80346b5
Iain STANGER <Iain.STANGER@EDUCATION.GOV.UK>
2023-06-27 16:41:12 +0100

    attempt to remove security issues in the Dockerfile

--- src/Dfe.PlanTech.Infrastructure.Contentful/Helpers/GetEntitiesException.cs
+++ src/Dfe.PlanTech.Infrastructure.Contentful/Helpers/GetEntitiesException.cs
@@ -2,7 +2,6 @@
 
 namespace Dfe.PlanTech.Infrastructure.Contentful.Helpers
 {
-    [Serializable]
     public class GetEntitiesException : Exception
     {
         public GetEntitiesException(string? message) : base(message)
