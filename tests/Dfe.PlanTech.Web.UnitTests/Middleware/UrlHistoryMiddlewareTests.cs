f43beb11f868489a7e794269f4f2fc28776f3773
Jim Washbrook <jim.washbrook@and.digital>
2023-06-28 12:53:54 +0100

    UrlHistoryMiddleware Tests

--- tests/Dfe.PlanTech.Web.UnitTests/Middleware/UrlHistoryMiddlewareTests.cs
+++ tests/Dfe.PlanTech.Web.UnitTests/Middleware/UrlHistoryMiddlewareTests.cs
@@ -1,11 +1,11 @@
 using Dfe.PlanTech.Application.Caching.Interfaces;
 using Dfe.PlanTech.Application.Caching.Models;
-using Dfe.PlanTech.Application.Core;
 using Dfe.PlanTech.Domain.Caching.Models;
 using Dfe.PlanTech.Web.Helpers;
 using Dfe.PlanTech.Web.Middleware;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using Xunit;
@@ -37,7 +37,7 @@ public class UrlHistoryMiddlewareTests
     public async Task Should_PopHistory_When_Navigating_Backwards()
     {
         var requestMock = new Mock<HttpRequest>();
-        
+
         requestMock.SetupGet(request => request.Scheme).Returns("https");
         requestMock.SetupGet(request => request.Host).Returns(new HostString("www.website.com"));
         requestMock.SetupGet(request => request.Path).Returns("/three");
@@ -65,7 +65,7 @@ public class UrlHistoryMiddlewareTests
         requestMock.SetupGet(request => request.Host).Returns(new HostString(URL_FOURTH.Host));
         requestMock.SetupGet(request => request.Path).Returns(URL_FOURTH.PathAndQuery);
         requestMock.SetupGet(request => request.Headers).Returns(new HeaderDictionary(){
-            { "Referer", URL_FOURTH.ToString() }
+            { UrlHistoryMiddleware.REFERER_HEADER_KEY, URL_FOURTH.ToString() }
         });
 
         var contextMock = new Mock<HttpContext>();
@@ -82,4 +82,91 @@ public class UrlHistoryMiddlewareTests
         Assert.Equal(URL_FOURTH, historyCache.Peek());
         Assert.Equal(4, historyCache.Count);
     }
+
+    [Fact]
+    public async Task Should_Not_AddToHistory_When_Missing_Referer_Header()
+    {
+        var requestMock = new Mock<HttpRequest>();
+        requestMock.SetupGet(request => request.Scheme).Returns("notarealscheme");
+        requestMock.SetupGet(request => request.Host).Returns(new HostString("notarealhost"));
+        requestMock.SetupGet(request => request.Path).Returns("/");
+        requestMock.SetupGet(request => request.Headers).Returns(new HeaderDictionary());
+
+        var contextMock = new Mock<HttpContext>();
+        contextMock.Setup(context => context.Request).Returns(() => requestMock.Object);
+
+        var nextDelegateMock = new Mock<RequestDelegate>();
+        var urlHistory = new UrlHistoryMiddleware(new NullLogger<UrlHistoryMiddleware>(), nextDelegateMock.Object);
+
+        var history = new UrlHistory(_cacher);
+        await urlHistory.InvokeAsync(contextMock.Object, history);
+
+        var historyCache = history.History;
+
+        Assert.Equal(3, historyCache.Count);
+    }
+
+    [Fact]
+    public async Task Should_Not_AddToHistory_When_Error_Getting_Uri()
+    {
+        var requestMock = new Mock<HttpRequest>();
+        requestMock.SetupGet(request => request.Scheme).Returns(() => "");
+        requestMock.SetupGet(request => request.Host).Returns(new HostString(""));
+        requestMock.SetupGet(request => request.Path).Returns("/");
+        requestMock.SetupGet(request => request.Headers).Returns(new HeaderDictionary(){
+            { UrlHistoryMiddleware.REFERER_HEADER_KEY, URL_FOURTH.ToString() }
+        });
+
+        var contextMock = new Mock<HttpContext>();
+        contextMock.Setup(context => context.Request).Returns(() => requestMock.Object);
+
+        var nextDelegateMock = new Mock<RequestDelegate>();
+        var urlHistory = new UrlHistoryMiddleware(new NullLogger<UrlHistoryMiddleware>(), nextDelegateMock.Object);
+
+        var history = new UrlHistory(_cacher);
+        await urlHistory.InvokeAsync(contextMock.Object, history);
+
+        var historyCache = history.History;
+
+        Assert.Equal(3, historyCache.Count);
+    }
+
+    [Fact]
+    public async Task Should_Log_Error_When_Uri_Parsing_Error()
+    {
+        var requestMock = new Mock<HttpRequest>();
+        requestMock.SetupGet(request => request.Scheme).Returns(() => "");
+        requestMock.SetupGet(request => request.Host).Returns(new HostString(""));
+        requestMock.SetupGet(request => request.Path).Returns("/");
+        requestMock.SetupGet(request => request.Headers).Returns(new HeaderDictionary(){
+            { UrlHistoryMiddleware.REFERER_HEADER_KEY, URL_FOURTH.ToString() }
+        });
+
+        var contextMock = new Mock<HttpContext>();
+        contextMock.Setup(context => context.Request).Returns(() => requestMock.Object);
+
+        var nextDelegateMock = new Mock<RequestDelegate>();
+
+        var loggerMock = new Mock<ILogger<UrlHistoryMiddleware>>();
+        loggerMock.Setup(logger => logger.Log(It.IsAny<LogLevel>(),
+                                                            It.IsAny<EventId>(),
+                                                            It.IsAny<It.IsAnyType>(),
+                                                            It.IsAny<Exception?>(),
+                                                            It.IsAny<Func<It.IsAnyType, Exception?, string>>()))
+                    .Verifiable();
+
+        var urlHistory = new UrlHistoryMiddleware(loggerMock.Object, nextDelegateMock.Object);
+
+        var history = new UrlHistory(_cacher);
+        await urlHistory.InvokeAsync(contextMock.Object, history);
+
+        var historyCache = history.History;
+
+        loggerMock.Verify(logger => logger.Log(It.IsAny<LogLevel>(),
+                                                It.IsAny<EventId>(),
+                                                It.IsAny<It.IsAnyType>(),
+                                                It.IsAny<Exception?>(),
+                                                It.IsAny<Func<It.IsAnyType, Exception?, string>>()));
+    }
+
 }
