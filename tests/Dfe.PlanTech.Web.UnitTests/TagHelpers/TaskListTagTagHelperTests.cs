ec7d11c5b1b73abb5c5750a2cc69eebb36206b1e
Jim Washbrook <jim.washbrook@and.digital>
2023-06-28 11:27:11 +0100

    Remove unnecessary usings

--- tests/Dfe.PlanTech.Web.UnitTests/TagHelpers/TaskListTagTagHelperTests.cs
+++ tests/Dfe.PlanTech.Web.UnitTests/TagHelpers/TaskListTagTagHelperTests.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
 using Dfe.PlanTech.Web.TagHelpers.TaskList;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Moq;
