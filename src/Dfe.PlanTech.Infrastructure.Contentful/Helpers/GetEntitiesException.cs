b5202db40738d2e9bb40a3de542a03249f76efea
Iain STANGER <Iain.STANGER@EDUCATION.GOV.UK>
2023-06-28 13:53:03 +0100

    code smell

--- src/Dfe.PlanTech.Infrastructure.Contentful/Helpers/GetEntitiesException.cs
+++ src/Dfe.PlanTech.Infrastructure.Contentful/Helpers/GetEntitiesException.cs
@@ -5,6 +5,11 @@ namespace Dfe.PlanTech.Infrastructure.Contentful.Helpers
     [Serializable]
     public class GetEntitiesException : Exception
     {
+        protected GetEntitiesException(SerializationInfo serializationInfo, StreamingContext streamingContext)
+        {
+            if (serializationInfo == null) throw new ArgumentNullException();
+            
+        }
         public GetEntitiesException(string? message) : base(message)
         {
         }
