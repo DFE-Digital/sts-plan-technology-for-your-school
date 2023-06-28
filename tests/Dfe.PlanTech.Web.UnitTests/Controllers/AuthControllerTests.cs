d19c9d1c28b6df9cff500312d75cfc43fc561922
Jim Washbrook <jim.washbrook@and.digital>
2023-06-28 12:33:23 +0100

    Remove moq

--- tests/Dfe.PlanTech.Web.UnitTests/Controllers/AuthControllerTests.cs
+++ tests/Dfe.PlanTech.Web.UnitTests/Controllers/AuthControllerTests.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Moq;
 using Xunit;
 
 namespace Dfe.PlanTech.Web.UnitTests.Controllers
