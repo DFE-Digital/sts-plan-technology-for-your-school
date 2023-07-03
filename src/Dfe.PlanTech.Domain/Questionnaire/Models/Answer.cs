1a78ef509df21c2a0be853bed075c42eb59cf25c
Zac King <zac.king@and.digital>
2023-07-03 17:06:46 +0100

    refactor: Revert Answer

--- src/Dfe.PlanTech.Domain/Questionnaire/Models/Answer.cs
+++ src/Dfe.PlanTech.Domain/Questionnaire/Models/Answer.cs
@@ -1,22 +1,14 @@
-using System.ComponentModel.DataAnnotations;
 using Dfe.PlanTech.Domain.Content.Models;
 
 namespace Dfe.PlanTech.Domain.Questionnaire.Models;
 
 public class Answer : ContentComponent
 {
-    [Required]
-    public int Id { get; set; }
-
     /// <summary>
     /// Actual answer
     /// </summary>
     /// <value></value>
-    [Required]
     public string Text { get; init; } = null!;
 
     public Question? NextQuestion { get; init; }
-
-    [Required]
-    public DateTime DateCreated { get; } = DateTime.UtcNow;
 }
