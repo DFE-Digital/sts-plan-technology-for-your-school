017da345ca471b0934155d497492ca9bb17b2090
snipered2003 <snipered@hotmail.com>
2023-07-05 12:37:05 +0100

    Added logging

--- src/Dfe.PlanTech.Application/Users/Commands/RecordUserSigninCommand.cs
+++ src/Dfe.PlanTech.Application/Users/Commands/RecordUserSigninCommand.cs
@@ -3,6 +3,7 @@ using Dfe.PlanTech.Application.Users.Interfaces;
 using Dfe.PlanTech.Application.Users.Queries;
 using Dfe.PlanTech.Domain.SignIn.Models;
 using Dfe.PlanTech.Domain.Users.Models;
+using Microsoft.Extensions.Logging;
 
 namespace Dfe.PlanTech.Application.Users.Commands;
 
@@ -10,19 +11,29 @@ public class RecordUserSignInCommand : IRecordUserSignInCommand
 {
     private readonly IUsersDbContext _db;
     private readonly ICreateUserCommand _createUserCommand;
+    private readonly ILogger<RecordUserSignInCommand> _logger;
 
-    public RecordUserSignInCommand(IUsersDbContext db, ICreateUserCommand createUserCommand)
+    public RecordUserSignInCommand(IUsersDbContext db, ICreateUserCommand createUserCommand, ILogger<RecordUserSignInCommand> logger = null)
     {
         _db = db;
         _createUserCommand = createUserCommand;
+        _logger = logger;
     }
 
     public async Task<int> RecordSignIn(RecordUserSignInDto recordUserSignInDto)
     {
+        if (recordUserSignInDto is null)
+        {
+            _logger.LogWarning($"dto object is null");
+            return 0;
+        }
+
+        _logger?.LogWarning($"Dfe signin ref is {recordUserSignInDto.DfeSignInRef}");
         //Check user exists already
         var getUserIdQuery = new GetUserIdQuery(_db);
         var existingUserId = await getUserIdQuery.GetUserId(recordUserSignInDto.DfeSignInRef);
 
+        _logger?.LogWarning($"existing user id is {existingUserId}");
         if (existingUserId == null)
         {
             existingUserId = await _createUserCommand.CreateUser(recordUserSignInDto);
