21cfe8bf0bb7814bd42be69b1caf01b8f817e0f8
Zac King <zac.king@and.digital>
2023-07-11 14:02:53 +0100

    test: Add tests for UserHelper

---
+++ tests/Dfe.PlanTech.Web.UnitTests/Helpers/UserHelperTests.cs
@@ -0,0 +1,52 @@
+using System.Security.Claims;
+using Dfe.PlanTech.Application.Persistence.Interfaces;
+using Dfe.PlanTech.Application.Users.Helper;
+using Dfe.PlanTech.Domain.Users.Models;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Xunit;
+
+public class UserHelperTests
+{
+    private readonly UserHelper _userHelper;
+    private readonly Mock<IHttpContextAccessor> _httpContextAccessorMock;
+    private readonly Mock<IPlanTechDbContext> _planTechDbContextMock;
+
+    public UserHelperTests()
+    {
+        _httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+        _planTechDbContextMock = new Mock<IPlanTechDbContext>();
+
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+        {
+            new Claim(ClaimTypes.Name, "test"),
+            new Claim(ClaimTypes.NameIdentifier, "1"),
+            }, "mock"));
+
+        _httpContextAccessorMock.Setup(m => m.HttpContext).Returns(new DefaultHttpContext() { User = user });
+
+        _userHelper = new UserHelper(_httpContextAccessorMock.Object, _planTechDbContextMock.Object);
+    }
+
+    [Fact]
+    public async Task GetCurrentUserId_Returns_Correct_Id_When_UserExists_InDatabase() // TODO: More descriptive name
+    {
+        _planTechDbContextMock.Setup(m => m.GetUserBy(userModel => userModel.DfeSignInRef == "1")).ReturnsAsync(new User() { Id = 1 });
+
+        var result = await _userHelper.GetCurrentUserId();
+
+        Assert.IsType<int>(result);
+
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void GetEstablishmentId_Returns_Correct_Id()
+    {
+        var result = _userHelper.GetEstablishmentId();
+
+        Assert.IsType<int>(result);
+
+        Assert.Equal(1, result);
+    }
+}
