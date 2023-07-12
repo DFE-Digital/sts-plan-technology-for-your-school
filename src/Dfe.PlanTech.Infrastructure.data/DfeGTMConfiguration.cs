d6bc34912a12b6f8aab25467b67f5eb730d60ffa
Iain STANGER <Iain.STANGER@EDUCATION.GOV.UK>
2023-07-12 08:59:49 +0100

    update

--- src/Dfe.PlanTech.Infrastructure.data/DfeGTMConfiguration.cs
+++ src/Dfe.PlanTech.Infrastructure.data/DfeGTMConfiguration.cs
@@ -2,7 +2,7 @@
 {
     public class DfeGtmConfiguration : IDfeGtmConfiguration
     {
-        public string Head { get; set; } = null!;
         public string Body { get; set; } = null!;
+        public string Head { get; set; } = null!;
     }
 }
--- src/Dfe.PlanTech.Infrastructure.data/IDfeGTMConfiguration.cs
+++ src/Dfe.PlanTech.Infrastructure.data/IDfeGTMConfiguration.cs
@@ -1,8 +1,6 @@
 ﻿namespace Dfe.PlanTech.Infrastructure.Data
 {
-    public interface IDfeGtmConfiguration
+    internal interface IDfeGtmConfiguration
     {
-        public string Head { get; set; }
-        public string Body { get; set; }
     }
 }
