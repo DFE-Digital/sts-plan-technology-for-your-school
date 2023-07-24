ec273b8906c8fb2feedc82bfc0a001e32abb8f27
Jim Washbrook <jim.washbrook@and.digital>
2023-07-24 15:23:40 +0100

    fix: Use correct local name

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -19,7 +19,7 @@ locals {
   #################
   container_app_image_name = "plan-tech-app"
   container_app_name       = "${local.resource_prefix}-${local.container_app_image_name}"
-  kestrel_endpoint = var.kestrel_endpoint
+  kestrel_endpoint         = var.az_app_kestrel_endpoint
 
   ##############
   # Front Door #
