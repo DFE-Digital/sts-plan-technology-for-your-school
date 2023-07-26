9c369d6596fce450ec46808906af3a9b83fef4ca
Jim Washbrook <jim.washbrook@and.digital>
2023-07-26 14:08:32 +0100

    fix: Remove trigger

--- terraform/keyvault-add-vnet-restriction.tf
+++ terraform/keyvault-add-vnet-restriction.tf
@@ -1,10 +1,6 @@
 resource "null_resource" "keyvault-add-vnet-restriction" {
   depends_on = [module.main_hosting]
 
-  triggers = {
-    managed_identity_id = azurerm_user_assigned_identity.user_assigned_identity.id
-  }
-
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = local.keyvault-add-vnet-restriction_command
