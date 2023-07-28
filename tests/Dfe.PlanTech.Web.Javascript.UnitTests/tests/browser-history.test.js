ada55b91e7c9d69411f4604dbb5a15039872005d
Jim Washbrook <jim.washbrook@and.digital>
2023-07-28 10:02:57 +0100

    chore: Add describe

--- tests/Dfe.PlanTech.Web.Javascript.UnitTests/tests/browser-history.test.js
+++ tests/Dfe.PlanTech.Web.Javascript.UnitTests/tests/browser-history.test.js
@@ -2,116 +2,115 @@ import { BrowserHistory } from "../../../src/Dfe.PlanTech.Web/wwwroot/js/browser
 
 let windowSpy;
 
-beforeEach(() => {
-    windowSpy = jest.spyOn(window, "window", "get");
-});
-
-afterEach(() => {
-    windowSpy.mockRestore();
-    localStorage.clear();
-});
-
-test("constructs", () => {
-    setWindowSpyLocation({
-        href: "http://www.plan-tech.com/",
-        pathname: "/",
-        hostname: "www.plan-tech.com",
-        protocol: "http"
+describe("BrowserHistory tests", () => {
+    beforeEach(() => {
+        windowSpy = jest.spyOn(window, "window", "get");
     });
 
-    const history = new BrowserHistory();
-
-    expect(history).toBeTruthy();
-});
-
-test("saves url to history", () => {
-    const href = 'http://www.plan-tech.com/';
-
-    setWindowSpyLocation({
-        href,
-        pathname: "/",
-        hostname: "www.plan-tech.com",
-        protocol: "http"
+    afterEach(() => {
+        windowSpy.mockRestore();
+        localStorage.clear();
     });
 
-    const browserHistory = new BrowserHistory();
-
-    const history = browserHistory.history;
-
-    expect(history).toHaveLength(1);
-    expect(history[0]).toEqual(href);
-});
-
-test("saves multiple to history", () => {
-    const pathNames = ['first-url', 'second-url', 'third-url'];
-    const protocol = "http";
-    const hostName = "www.plan-tech.com";
-
-    for (let x = 0; x < pathNames.length; x++) {
-        const path = pathNames[x];
-
-        const href = `${protocol}://${hostName}/${path}`;
+    test("constructs", () => {
         setWindowSpyLocation({
-            href: href,
-            pathname: path,
-            hostname: hostName,
-            protocol
+            href: "http://www.plan-tech.com/",
+            pathname: "/",
+            hostname: "www.plan-tech.com",
+            protocol: "http"
+        });
+
+        const history = new BrowserHistory();
+
+        expect(history).toBeTruthy();
+    });
+
+    test("saves url to history", () => {
+        const href = 'http://www.plan-tech.com/';
+
+        setWindowSpyLocation({
+            href,
+            pathname: "/",
+            hostname: "www.plan-tech.com",
+            protocol: "http"
         });
 
         const browserHistory = new BrowserHistory();
 
         const history = browserHistory.history;
 
-        expect(history).toHaveLength(x + 1);
-        expect(history[x]).toEqual(href);    
-    }
-});
+        expect(history).toHaveLength(1);
+        expect(history[0]).toEqual(href);
+    });
 
-test("removes from history if navigating backwards", () => {
-    const pathNames = ['first-url', 'second-url', 'third-url'];
-    const protocol = "http";
-    const hostName = "www.plan-tech.com";
+    test("saves multiple to history", () => {
+        const pathNames = ['first-url', 'second-url', 'third-url'];
+        const protocol = "http";
+        const hostName = "www.plan-tech.com";
 
-    for (const path of pathNames){
-        const href = `${protocol}://${hostName}/${path}`;
+        for (let x = 0; x < pathNames.length; x++) {
+            const path = pathNames[x];
+
+            const href = `${protocol}://${hostName}/${path}`;
+            setWindowSpyLocation({
+                href: href,
+                pathname: path,
+                hostname: hostName,
+                protocol
+            });
+
+            const browserHistory = new BrowserHistory();
+
+            const history = browserHistory.history;
+
+            expect(history).toHaveLength(x + 1);
+            expect(history[x]).toEqual(href);
+        }
+    });
+
+    test("removes from history if navigating backwards", () => {
+        const pathNames = ['first-url', 'second-url', 'third-url'];
+        const protocol = "http";
+        const hostName = "www.plan-tech.com";
+
+        for (const path of pathNames) {
+            const href = `${protocol}://${hostName}/${path}`;
+            setWindowSpyLocation({
+                href: href,
+                pathname: path,
+                hostname: hostName,
+                protocol
+            });
+
+            const history = new BrowserHistory();
+            expect(history).toBeTruthy();
+        }
+
+        const href = `${protocol}://${hostName}/${pathNames[1]}`;
         setWindowSpyLocation({
             href: href,
-            pathname: path,
+            pathname: pathNames[1],
             hostname: hostName,
             protocol
         });
-        
-        const history = new BrowserHistory();
-        expect(history).toBeTruthy();
-    }
 
-    const href = `${protocol}://${hostName}/${pathNames[1]}`;
-    setWindowSpyLocation({
-        href: href,
-        pathname: pathNames[1],
-        hostname: hostName,
-        protocol
+        const browserHistory = new BrowserHistory();
+        const history = browserHistory.history;
+
+        expect(history).toHaveLength(1);
+        expect(history[0]).toEqual(`${protocol}://${hostName}/${pathNames[0]}`);
     });
 
-    const browserHistory = new BrowserHistory();
-    const history = browserHistory.history;
-
-    expect(history).toHaveLength(1);
-    expect(history[0]).toEqual(`${protocol}://${hostName}/${pathNames[0]}`);
+    /**
+     * Set the window.locaton value in the window spy
+     * @param {WindowLocation} location 
+     */
+    const setWindowSpyLocation = (location) => {
+        windowSpy.mockImplementation(() => ({
+            location
+        }));
+    }
 });
-
-
-
-/**
- * Set the window.locaton value in the window spy
- * @param {WindowLocation} location 
- */
-const setWindowSpyLocation = (location) => {
-    windowSpy.mockImplementation(() => ({
-        location
-    }));
-}
-
 /**
  * window.location
  * @typedef {Object} WindowLocation
