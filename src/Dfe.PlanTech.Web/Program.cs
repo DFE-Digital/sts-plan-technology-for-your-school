768ddc5c9ea2ca6226301f91586ba7c9eaa0356b
snipered2003 <snipered@hotmail.com>
2023-07-31 09:17:49 +0100

    removed debugging

--- src/Dfe.PlanTech.Web/Program.cs
+++ src/Dfe.PlanTech.Web/Program.cs
@@ -53,10 +53,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-else 
-{
-    app.UseDeveloperExceptionPage();
-}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
