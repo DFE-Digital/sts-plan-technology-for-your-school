9f13f090baf4bcbd2527759ac7833f62224ea945
snipered2003 <snipered@hotmail.com>
2023-08-04 11:16:33 +0100

    wip

---
+++ src/Dfe.PlanTech.Web/Controllers/CookiesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dfe.PlanTech.Web.Controllers
+{
+    [Route("/cookies")]
+    public class CookiesController : Controller
+    {
+        [HttpPost("accept")]
+        public IActionResult Accept()
+        {
+            CreateCookie("PlanTech-CookieAccepted", "Accepted");
+            var host = HttpContext.Request.Host.Value;
+            //return Redirect(host);
+            return RedirectToAction("/", "Pages");
+        }
+
+        [HttpPost("hidebanner")]
+        public IActionResult HideBanner()
+        {
+            CreateCookie("PlanTech-HideCookieBanner", "Hidden");
+            var host = HttpContext.Request.Host.Value;
+            return Redirect(host);
+        }
+
+        private void CreateCookie(string key, string value)
+        {
+            CookieOptions cookieOptions = new CookieOptions();
+            cookieOptions.Expires = new DateTimeOffset(DateTime.Now.AddYears(1));
+            HttpContext.Response.Cookies.Append(key, value, cookieOptions);
+        }
+    }
+}
---
+++ src/Dfe.PlanTech.Web/Models/CookiesModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Dfe.PlanTech.Web.Models
+{
+    public class CookiesModel
+    {
+        public bool Accepted { get; set; }
+
+        public bool Visible { get; set; }
+    }
+}
