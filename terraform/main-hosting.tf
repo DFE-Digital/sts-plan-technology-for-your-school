426189c5b6a38cdfbc34814528145234d5ce17e7
Jim Washbrook <jim.washbrook@and.digital>
2023-08-09 13:26:30 +0100

    chore: Upgrade to latest version (with fix)

--- terraform/main-hosting.tf
+++ terraform/main-hosting.tf
@@ -1,5 +1,5 @@
 module "main_hosting" {
-  source = "github.com/DFE-Digital/terraform-azurerm-container-apps-hosting?ref=v0.19.3"
+  source = "github.com/DFE-Digital/terraform-azurerm-container-apps-hosting?ref=v0.19.4"
 
   ###########
   # General #
