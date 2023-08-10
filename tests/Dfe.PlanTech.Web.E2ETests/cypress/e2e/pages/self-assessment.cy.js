f1ef124f8218d8d19ea0628a574141894a7e2353
Jim Washbrook <jim.washbrook@and.digital>
2023-08-10 10:48:28 +0100

    fix: URL

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/self-assessment.cy.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/self-assessment.cy.js
@@ -1,39 +1,31 @@
 describe("landing page", () => {
-  const url = Cypress.env("/") + "/self-assessment";
+  const url = "/self-assessment";
 
   beforeEach(() => {
     cy.loginWithEnv(url);
   });
 
   it("should have heading", () => {
-    cy.origin(Cypress.env("/"), () => {
-      cy.get("h1.govuk-heading-xl")
-        .should("exist")
-        .and("have.text", "Technology self-assessment");
-    });
+    cy.get("h1.govuk-heading-xl")
+      .should("exist")
+      .and("have.text", "Technology self-assessment");
   });
 
   it("should contain categories", () => {
-    cy.origin(Cypress.env("/"), () => {
-      cy.get("h2.govuk-heading-m").should("exist");
-    });
+    cy.get("h2.govuk-heading-m").should("exist");
   });
 
   it("should contain sections", () => {
-    cy.origin(Cypress.env("/"), () => {
-      cy.get("ul.app-task-list__items > li")
-        .should("exist")
-        .and("have.length.greaterThan", 1);
-    });
+    cy.get("ul.app-task-list__items > li")
+      .should("exist")
+      .and("have.length.greaterThan", 1);
   });
 
   it("each section should link to a page", () => {
-    cy.origin(Cypress.env("/"), () => {
-      cy.get("ul.app-task-list__items > li a").each((link) => {
-        cy.wrap(link)
-          .should("have.attr", "href")
-          .and("not.be.null");
-      });
+    cy.get("ul.app-task-list__items > li a").each((link) => {
+      cy.wrap(link)
+        .should("have.attr", "href")
+        .and("not.be.null");
     });
   });
 });
