c9035d6b1fb3eca74e3695d806f859818ee0a234
Jim Washbrook <jim.washbrook@and.digital>
2023-08-15 15:08:16 +0100

    fix: Depend on specific output to prevent cycle

--- terraform/user-assigned-identity.tf
+++ terraform/user-assigned-identity.tf
@@ -1,5 +1,5 @@
 resource "azurerm_user_assigned_identity" "user_assigned_identity" {
-  depends_on          = [module.main_hosting]
+  depends_on          = [module.main_hosting.azurerm_resource_group_default]
   
   name                = local.user_identity_name
   location            = local.azure_location
