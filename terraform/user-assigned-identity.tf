b49551e9ad26f2dd834f462bf36357069605e807
Jim Washbrook <jim.washbrook@and.digital>
2023-08-15 15:12:02 +0100

    fix: identity

--- terraform/user-assigned-identity.tf
+++ terraform/user-assigned-identity.tf
@@ -1,6 +1,6 @@
 resource "azurerm_user_assigned_identity" "user_assigned_identity" {
-  depends_on          = [module.main_hosting.azurerm_resource_group_default]
-  
+  depends_on = [module.main_hosting.azurerm_resource_group_default]
+
   name                = local.user_identity_name
   location            = local.azure_location
   resource_group_name = local.resource_group_name
