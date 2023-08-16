5f5ce4ca03dca31cd3a8bd358cb3ba6877c14133
Jim Washbrook <jim.washbrook@and.digital>
2023-08-16 14:01:39 +0100

    feat: Add necessary user permissions

--- src/Dfe.PlanTech.DatabaseUpgrader/EnvironmentSpecificScripts/tst/2023/20230816_132200_CreateAnalyticsUser.sql
+++ src/Dfe.PlanTech.DatabaseUpgrader/EnvironmentSpecificScripts/tst/2023/20230816_132200_CreateAnalyticsUser.sql
@@ -1,3 +1,5 @@
 -- $Analytics_Username$ $Analytics_Password$
 
-CREATE USER $Analytics_Username$ WITH PASSWORD = '$Analytics_Password$';
+CREATE USER [$Analytics_Username$] WITH PASSWORD = '$Analytics_Password$'
+ALTER ROLE [db_datareader] ADD MEMBER [$Analytics_Username$]
+GRANT SELECT ON SCHEMA :: [dbo] to [AnalyticsUser];
