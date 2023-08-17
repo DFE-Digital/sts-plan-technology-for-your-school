0685ccc55a5bfd8bdc8c1fa91af457605fe1f38f
Jim Washbrook <jim.washbrook@and.digital>
2023-08-17 14:37:20 +0100

    chore: Delete unused file

--- src/Dfe.PlanTech.Web/Models/BasePageViewModel.cs
+++
@@ -1,8 +0,0 @@
-using Dfe.PlanTech.Domain.Content.Models;
-
-namespace Dfe.PlanTech.Web.Models;
-
-public abstract class BasePageViewModel
-{
-  public List<NavigationLink> Links { get; set; } = new List<NavigationLink>();
-}
