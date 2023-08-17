ad2012b555a6021a72c0839de47d4b1ecd8d6b30
Jim Washbrook <jim.washbrook@and.digital>
2023-08-17 15:01:57 +0100

    fix: Use nsubstitute for tests - Mock queries instead of DB - Query actual data

--- tests/Dfe.PlanTech.Web.UnitTests/Helpers/UserHelperTests.cs
+++ tests/Dfe.PlanTech.Web.UnitTests/Helpers/UserHelperTests.cs
@@ -1,27 +1,59 @@
 using Dfe.PlanTech.Application.Persistence.Interfaces;
 using Dfe.PlanTech.Application.Users.Helper;
+using Dfe.PlanTech.Application.Users.Interfaces;
 using Dfe.PlanTech.Domain.Establishments.Models;
 using Dfe.PlanTech.Domain.Users.Models;
 using Microsoft.AspNetCore.Http;
 using NSubstitute;
-using NSubstitute.Core;
-using NSubstitute.ReturnsExtensions;
 using System.Security.Claims;
 using Xunit;
 
 namespace Dfe.PlanTech.Web.UnitTests.Helpers;
 public class UserHelperTests
 {
+    private const string FIRST_USER_REF = "1";
+    private const string SECOND_USER_REF = "OtherReference";
+
+    private const string FIRST_ESTABLISHMENT_REF = "131";
+
     private readonly UserHelper _userHelper;
     private IHttpContextAccessor _httpContextAccessorMock;
     private IPlanTechDbContext _planTechDbContextMock;
     private ICreateEstablishmentCommand _createEstablishmentCommandMock;
+    private IGetUserIdQuery _getUserIdQueryMock;
+    private IGetEstablishmentIdQuery _getEstablishmentIdQueryMock;
+
+    private List<User> _users = new(){
+        new User(){
+            DfeSignInRef = FIRST_USER_REF,
+            Id = 1
+        },
+        new User(){
+            DfeSignInRef = SECOND_USER_REF,
+            Id = 2
+        }
+    };
+
+    private List<Establishment> _establishments = new(){
+       new Establishment(){
+        EstablishmentRef = FIRST_ESTABLISHMENT_REF,
+        Id = 1
+       },
+       new Establishment(){
+        EstablishmentRef = "Other reference",
+        Id = 2
+       }
+    };
+
 
     public UserHelperTests()
     {
         _httpContextAccessorMock = Substitute.For<IHttpContextAccessor>();
         _planTechDbContextMock = Substitute.For<IPlanTechDbContext>();
         _createEstablishmentCommandMock = Substitute.For<ICreateEstablishmentCommand>();
+
+        MockGetEstablishmentIdQuery();
+        MockGetUserIdQuery();
 
         var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
         {
@@ -32,14 +64,39 @@ public class UserHelperTests
 
         _httpContextAccessorMock.HttpContext.Returns(new DefaultHttpContext() { User = user });
 
-        _userHelper = new UserHelper(_httpContextAccessorMock, _planTechDbContextMock, _createEstablishmentCommandMock);
+        _userHelper = new UserHelper(_httpContextAccessorMock, _planTechDbContextMock, _createEstablishmentCommandMock, _getUserIdQueryMock, _getEstablishmentIdQueryMock);
+    }
+
+    private void MockGetEstablishmentIdQuery()
+    {
+        _getEstablishmentIdQueryMock = Substitute.For<IGetEstablishmentIdQuery>();
+        _getEstablishmentIdQueryMock.GetEstablishmentId(Arg.Any<string>())
+                                    .Returns((callInfo) =>
+                                    {
+                                        var establishmentRef = callInfo.ArgAt<string>(0);
+
+                                        return _establishments.Where(establishment => establishment.EstablishmentRef == establishmentRef)
+                                                    .Select(establishment => establishment.Id)
+                                                    .FirstOrDefault();
+                                    });
+    }
+
+    private void MockGetUserIdQuery()
+    {
+        _getUserIdQueryMock = Substitute.For<IGetUserIdQuery>();
+        _getUserIdQueryMock.GetUserId(Arg.Any<string>()).Returns((callInfo) =>
+        {
+            var userRef = callInfo.ArgAt<string>(0);
+
+            return _users.Where(user => user.DfeSignInRef == userRef)
+                        .Select(user => user.Id)
+                        .FirstOrDefault();
+        });
     }
 
     [Fact]
     public async Task GetCurrentUserId_Returns_Correct_Id_When_UserExists_InDatabase()
     {
-        _planTechDbContextMock.GetUserBy(userModel => userModel.DfeSignInRef == "1").Returns(new User() { Id = 1 });
-
         var result = await _userHelper.GetCurrentUserId();
 
         Assert.IsType<int>(result);
@@ -50,8 +107,6 @@ public class UserHelperTests
     [Fact]
     public async Task GetEstablishmentId_Returns_Correct_Id_When_Establishment_Exists_In_DB()
     {
-        _planTechDbContextMock.GetEstablishmentBy(establishment => establishment.EstablishmentRef == "131").Returns(Task.FromResult(new Establishment() { Id = 1 }));
-
         var result = await _userHelper.GetEstablishmentId();
 
         await _createEstablishmentCommandMock.Received(0).CreateEstablishment(Arg.Any<EstablishmentDto>());
@@ -90,7 +145,7 @@ public class UserHelperTests
 
         _httpContextAccessorMock.HttpContext.Returns(new DefaultHttpContext() { User = user });
 
-        var userHelperWithMissingOrgData = new UserHelper(_httpContextAccessorMock, _planTechDbContextMock, _createEstablishmentCommandMock);
+        var userHelperWithMissingOrgData = new UserHelper(_httpContextAccessorMock, _planTechDbContextMock, _createEstablishmentCommandMock, _getUserIdQueryMock, _getEstablishmentIdQueryMock);
 
         var result = await userHelperWithMissingOrgData.GetEstablishmentId();
 
