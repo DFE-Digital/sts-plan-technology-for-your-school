af5b0b5371c9d00c5793b147fdf84e9dd426c22e
“Umar <umar.hussain@and.digital>
2023-08-22 14:30:10 +0100

    Add test for contract resolver

---
+++ tests/Dfe.PlanTech.Infrastructure.Contentful.UnitTests/Serializers/DependencyInjectionContractResolverTests.cs
@@ -0,0 +1,61 @@
+using NSubstitute;
+using Newtonsoft.Json.Serialization;
+using System.Reflection;
+
+namespace Dfe.PlanTech.Infrastructure.Contentful.Serializers
+{
+    public class DependencyInjectionContractResolverTests
+    {
+        [Fact]
+        public void CreateObjectContract_ModelFound()
+        {
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            var objectType = typeof(DummyModel);
+
+            serviceProvider.GetService(objectType).Returns(new DummyModel());
+
+            var resolver = new DependencyInjectionContractResolver(serviceProvider);
+
+            var contract = InvokeProtectedCreateObjectContract(resolver, objectType);
+
+            Assert.NotNull(contract);
+        }
+        
+        [Fact]
+        public void CreateObjectContract_ModelNotFound()
+        {
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            var objectType = typeof(DummyModel);
+
+            serviceProvider.GetService(objectType).Returns(null);
+
+            var resolver = new DependencyInjectionContractResolver(serviceProvider);
+
+            var contract = InvokeProtectedCreateObjectContract(resolver, objectType);
+
+            Assert.NotNull(contract);
+        }
+        
+
+        private JsonObjectContract InvokeProtectedCreateObjectContract(DependencyInjectionContractResolver resolver, Type objectType)
+        {
+            var methodInfo = typeof(DependencyInjectionContractResolver).GetMethod(
+                "CreateObjectContract",
+                BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public,
+                null,
+                new[] { typeof(Type) },
+                null
+            );
+
+            if (methodInfo != null)
+            {
+                return (JsonObjectContract)methodInfo.Invoke(resolver, new object[] { objectType });
+            }
+
+            throw new MissingMethodException("CreateObjectContract method not found.");
+        }
+    }
+
+    public class DummyModel
+    { }
+}
