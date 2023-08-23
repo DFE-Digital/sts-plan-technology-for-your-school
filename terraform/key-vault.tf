b94a627d1866ff1a394fd70a7f159376262b6129
“Umar <umar.hussain@and.digital>
2023-08-23 16:26:00 +0100

    enable to run manually

--- terraform/key-vault.tf
+++ terraform/key-vault.tf
@@ -7,7 +7,7 @@ resource "azurerm_key_vault" "vault" {
   soft_delete_retention_days = 90
   enable_rbac_authorization  = false
   tags                       = local.tags
-  purge_protection_enabled   = true
+  purge_protection_enabled   = false
 }
 
 resource "azurerm_key_vault_access_policy" "vault_access_policy_tf" {
