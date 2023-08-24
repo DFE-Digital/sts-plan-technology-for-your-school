68f31d990f073f9028152ef8832a2157dbd0f06a
Zac King <zac.king@and.digital>
2023-08-24 11:50:43 +0100

    test: implement tests

--- tests/Dfe.PlanTech.Web.UnitTests/ViewComponents/CategorySectionViewComponentTests.cs
+++ tests/Dfe.PlanTech.Web.UnitTests/ViewComponents/CategorySectionViewComponentTests.cs
@@ -99,13 +99,139 @@ namespace Dfe.PlanTech.Web.UnitTests.ViewComponents
         [Fact]
         public void Returns_CategorySelectionInfo_If_Slug_Exists_And_SectionIsNotCompleted()
         {
-            // TODO: Implement Test
+            _category.Completed = 0;
+
+            _category.SectionStatuses.Add(new Domain.Submissions.Models.SectionStatuses()
+            {
+                SectionId = "Section1",
+                Completed = 0,
+            });
+
+            _getSubmissionStatusesQuery.GetCategoryWithCompletedSectionStatuses(_category).Returns(_category);
+
+            var result = _categorySectionViewComponent.Invoke(_category) as ViewViewComponentResult;
+
+            Assert.NotNull(result);
+            Assert.NotNull(result.ViewData);
+
+            var model = result.ViewData.Model;
+            Assert.NotNull(model);
+
+            var unboxed = model as CategorySectionViewComponentViewModel;
+            Assert.NotNull(unboxed);
+
+            Assert.Equal(0, unboxed.CompletedSectionCount);
+            Assert.Equal(1, unboxed.TotalSectionCount);
+
+            var categorySectionDtoList = unboxed.CategorySectionDto as IEnumerable<CategorySectionDto>;
+
+            Assert.NotNull(categorySectionDtoList);
+
+            categorySectionDtoList = categorySectionDtoList.ToList();
+            Assert.NotEmpty(categorySectionDtoList);
+
+            var categorySectionDto = categorySectionDtoList.FirstOrDefault();
+
+            Assert.NotNull(categorySectionDto);
+
+            Assert.Equal("section-1", categorySectionDto.Slug);
+            Assert.Equal("Test Section 1", categorySectionDto.Name);
+            Assert.Equal("Green", categorySectionDto.TagColour);
+            Assert.Equal("STARTED", categorySectionDto.TagText);
+            Assert.Null(categorySectionDto.NoSlugForSubtopicErrorMessage);
+        }
+
+        [Fact]
+        public void Returns_CategorySelectionInfo_If_Section_IsNotStarted()
+        {
+            _category.Completed = 0;
+
+            _getSubmissionStatusesQuery.GetCategoryWithCompletedSectionStatuses(_category).Returns(_category);
+
+            var result = _categorySectionViewComponent.Invoke(_category) as ViewViewComponentResult;
+
+            Assert.NotNull(result);
+            Assert.NotNull(result.ViewData);
+
+            var model = result.ViewData.Model;
+            Assert.NotNull(model);
+
+            var unboxed = model as CategorySectionViewComponentViewModel;
+            Assert.NotNull(unboxed);
+
+            Assert.Equal(0, unboxed.CompletedSectionCount);
+            Assert.Equal(1, unboxed.TotalSectionCount);
+
+            var categorySectionDtoList = unboxed.CategorySectionDto as IEnumerable<CategorySectionDto>;
+
+            Assert.NotNull(categorySectionDtoList);
+
+            categorySectionDtoList = categorySectionDtoList.ToList();
+            Assert.NotEmpty(categorySectionDtoList);
+
+            var categorySectionDto = categorySectionDtoList.FirstOrDefault();
+
+            Assert.NotNull(categorySectionDto);
+
+            Assert.Equal("section-1", categorySectionDto.Slug);
+            Assert.Equal("Test Section 1", categorySectionDto.Name);
+            Assert.Equal("Grey", categorySectionDto.TagColour);
+            Assert.Equal("NOT STARTED", categorySectionDto.TagText);
+            Assert.Null(categorySectionDto.NoSlugForSubtopicErrorMessage);
         }
 
         [Fact]
         public void Returns_NullSlug_And_ErrorMessage_In_CategorySectionInfo_If_SlugDoesNotExist()
         {
-            // TODO: Implement Test
+            _category.Sections[0] = new Section()
+            {
+                Sys = new Sys() { Id = "Section1" },
+                Name = "Test Section 1",
+                InterstitialPage = new Domain.Content.Models.Page()
+                {
+                    Slug = null!,
+                }
+            };
+
+            _category.SectionStatuses.Add(new Domain.Submissions.Models.SectionStatuses()
+            {
+                SectionId = "Section1",
+                Completed = 1,
+            });
+
+            _getSubmissionStatusesQuery.GetCategoryWithCompletedSectionStatuses(_category).Returns(_category);
+
+            var result = _categorySectionViewComponent.Invoke(_category) as ViewViewComponentResult;
+
+            Assert.NotNull(result);
+            Assert.NotNull(result.ViewData);
+
+            var model = result.ViewData.Model;
+            Assert.NotNull(model);
+
+            var unboxed = model as CategorySectionViewComponentViewModel;
+            Assert.NotNull(unboxed);
+
+            Assert.Equal(1, unboxed.CompletedSectionCount);
+            Assert.Equal(1, unboxed.TotalSectionCount);
+
+            var categorySectionDtoList = unboxed.CategorySectionDto as IEnumerable<CategorySectionDto>;
+
+            Assert.NotNull(categorySectionDtoList);
+
+            categorySectionDtoList = categorySectionDtoList.ToList();
+            Assert.NotEmpty(categorySectionDtoList);
+
+            var categorySectionDto = categorySectionDtoList.FirstOrDefault();
+
+            Assert.NotNull(categorySectionDto);
+
+            Assert.Null(categorySectionDto.Slug);
+            Assert.Equal("Test Section 1", categorySectionDto.Name);
+            Assert.Null(categorySectionDto.TagColour);
+            Assert.Null(categorySectionDto.TagText);
+            Assert.NotNull(categorySectionDto.NoSlugForSubtopicErrorMessage);
+            Assert.Equal("Test Section 1 unavailable", categorySectionDto.NoSlugForSubtopicErrorMessage);
         }
     }
 }
