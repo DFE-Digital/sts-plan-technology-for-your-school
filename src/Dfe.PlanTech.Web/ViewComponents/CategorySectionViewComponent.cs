65ee46e0b22e384f306e423ffc5cc4a80a701ad3
Zac King <zac.king@and.digital>
2023-08-24 17:06:34 +0100

    refactor: resolve code smell

--- src/Dfe.PlanTech.Web/ViewComponents/CategorySectionViewComponent.cs
+++ src/Dfe.PlanTech.Web/ViewComponents/CategorySectionViewComponent.cs
@@ -30,6 +30,35 @@ namespace Dfe.PlanTech.Web.ViewComponents
             return View(categorySectionViewModel);
         }
 
+        private void _LogErrorWithUserFeedback(ISection categorySection, ref CategorySectionDto categorySectionDto)
+        {
+            categorySectionDto.Slug = null;
+            _logger.LogError("No Slug found for Subtopic with ID: {categorySectionId}", categorySection.Sys.Id);
+            categorySectionDto.NoSlugForSubtopicErrorMessage = String.Format("{0} unavailable", categorySection.Name);
+        }
+
+        private void _SetCategorySectionDtoTagWithRetrievalError(ref CategorySectionDto categorySectionDto)
+        {
+            categorySectionDto.TagColour = TagColour.Red.ToString();
+            categorySectionDto.TagText = "UNABLE TO RETRIEVE STATUS";
+        }
+
+        private void _SetCategorySectionDtoTagWithCurrentStatus(ICategory category, ISection categorySection, ref CategorySectionDto categorySectionDto)
+        {
+            var sectionStatusCompleted = category.SectionStatuses.FirstOrDefault(sectionStatus => sectionStatus.SectionId == categorySection.Sys.Id)?.Completed;
+
+            if (sectionStatusCompleted != null)
+            {
+                categorySectionDto.TagColour = sectionStatusCompleted == 1 ? TagColour.DarkBlue.ToString() : TagColour.Green.ToString();
+                categorySectionDto.TagText = sectionStatusCompleted == 1 ? "COMPLETE" : "STARTED";
+            }
+            else
+            {
+                categorySectionDto.TagColour = TagColour.Grey.ToString();
+                categorySectionDto.TagText = "NOT STARTED";
+            }
+        }
+
         private IEnumerable<CategorySectionDto> _GetCategorySectionViewComponentViewModel(ICategory category)
         {
             foreach (var categorySection in category.Sections)
@@ -40,38 +69,11 @@ namespace Dfe.PlanTech.Web.ViewComponents
                     Name = categorySection.Name
                 };
 
-                if (string.IsNullOrWhiteSpace(categorySectionDto.Slug)) categorySectionDto.Slug = null;
+                if (string.IsNullOrWhiteSpace(categorySectionDto.Slug)) _LogErrorWithUserFeedback(categorySection, ref categorySectionDto);
+                else if (category.RetrievalError) _SetCategorySectionDtoTagWithRetrievalError(ref categorySectionDto);
+                else _SetCategorySectionDtoTagWithCurrentStatus(category, categorySection, ref categorySectionDto);
 
-                if (categorySectionDto.Slug != null)
-                {
-                    if (!category.RetrievalError)
-                    {
-                        var sectionStatusCompleted = category.SectionStatuses.FirstOrDefault(sectionStatus => sectionStatus.SectionId == categorySection.Sys.Id)?.Completed;
-
-                        if (sectionStatusCompleted != null)
-                        {
-                            categorySectionDto.TagColour = (sectionStatusCompleted == 1 ? TagColour.DarkBlue : TagColour.Green).ToString();
-                            categorySectionDto.TagText = sectionStatusCompleted == 1 ? "COMPLETE" : "STARTED";
-                        }
-                        else
-                        {
-                            categorySectionDto.TagColour = TagColour.Grey.ToString();
-                            categorySectionDto.TagText = "NOT STARTED";
-                        }
-                    }
-                    else
-                    {
-                        categorySectionDto.TagColour = TagColour.Red.ToString();
-                        categorySectionDto.TagText = "UNABLE TO RETRIEVE STATUS";
-                    }
-
-                    TempData[categorySectionDto.Slug] = categorySectionDto.Name + "+" + categorySection.Sys.Id;
-                }
-                else
-                {
-                    _logger.LogError("No Slug found for Subtopic with ID: {categorySectionId}", categorySection.Sys.Id);
-                    categorySectionDto.NoSlugForSubtopicErrorMessage = String.Format("{0} unavailable", categorySection.Name);
-                }
+                if (categorySectionDto.Slug != null) TempData[categorySectionDto.Slug] = categorySectionDto.Name + "+" + categorySection.Sys.Id;
 
                 yield return categorySectionDto;
             }
