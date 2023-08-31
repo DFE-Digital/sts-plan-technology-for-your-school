d6ad37e724456dff0d1268b55e85538222520003
“Umar <umar.hussain@and.digital>
2023-08-31 11:19:32 +0100

    Add home button functionality to Accessibility page

---
+++ src/Dfe.PlanTech.Web/Controllers/AccessibilityController.cs
@@ -0,0 +1,30 @@
+using Dfe.PlanTech.Application.Content.Queries;
+using Dfe.PlanTech.Domain.Content.Models;
+using Dfe.PlanTech.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfe.PlanTech.Web.Controllers;
+
+[Route("/accessibility")]
+public class AccessibilityController : BaseController<AccessibilityController>
+{
+    
+    public AccessibilityController(ILogger<AccessibilityController> logger) : base(logger)
+    {
+        
+    }
+    public async Task<IActionResult> GetAccessibilityPage([FromServices] GetPageQuery getPageQuery)
+    {
+        Page accessibilityPageContent = await getPageQuery.GetPageBySlug("accessibility", CancellationToken.None);
+
+        AccessibilityViewModel accessibilityViewModel = new AccessibilityViewModel()
+        {
+            Title = accessibilityPageContent.Title,
+            Content = accessibilityPageContent.Content,
+            UserIsAuthenticated = User.Identity.IsAuthenticated
+        };
+        
+        return View("Accessibility", accessibilityViewModel);
+    }
+
+}
