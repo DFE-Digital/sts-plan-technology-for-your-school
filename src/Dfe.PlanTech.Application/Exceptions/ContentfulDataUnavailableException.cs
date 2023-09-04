b0b4b295a8713aeaf196568f396deaebaa88a7dc
“Umar <umar.hussain@and.digital>
2023-09-04 11:53:28 +0100

    Refactor

--- src/Dfe.PlanTech.Application/Exceptions/ContentfulDataUnavailableException.cs
+++ src/Dfe.PlanTech.Application/Exceptions/ContentfulDataUnavailableException.cs
@@ -4,15 +4,6 @@ using System;
 
 public class ContentfulDataUnavailableException : Exception
 {
-    public ContentfulDataUnavailableException()
-    {
-    }
-
-    public ContentfulDataUnavailableException(string message)
-        : base(message)
-    {
-    }
-
     public ContentfulDataUnavailableException(string message, Exception inner)
         : base(message, inner)
     {
