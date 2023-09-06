6f0c2f2034398925c22a3509b32006816011e444
Jim Washbrook <jim.washbrook@and.digital>
2023-09-06 09:58:47 +0100

    refactor: Cleanup page + home link duplications

--- src/Dfe.PlanTech.Web/ViewComponents/HomeLinkViewComponent.cs
+++ src/Dfe.PlanTech.Web/ViewComponents/HomeLinkViewComponent.cs
@@ -1,20 +1,11 @@
-using Dfe.PlanTech.Web.Models;
-
 namespace Dfe.PlanTech.Web.ViewComponents;
 
 using Microsoft.AspNetCore.Mvc;
 
 public class HomeLinkViewComponent : ViewComponent
-{
-    public IViewComponentResult Invoke(AccessibilityViewModel model)
-    {
-        if (model.UserIsAuthenticated)
-        {
-            return View("AuthenticatedLink");
-        }
-        else
-        {
-            return View("UnauthenticatedLink");
-        }
-    }
+{  
+    public bool IsAuthenticated => User.Identity?.IsAuthenticated == true;
+    public string Href => IsAuthenticated ? "/self-assessment" : "/";
+
+    public IViewComponentResult Invoke() => View("Default", Href);
 }
