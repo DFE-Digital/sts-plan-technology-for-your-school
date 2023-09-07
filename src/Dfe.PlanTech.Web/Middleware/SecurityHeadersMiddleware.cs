f8be5ca2e4f6256c780ce6d405416e94171dcf83
Jim Washbrook <jim.washbrook@and.digital>
2023-09-07 11:39:47 +0100

    minor refactor

--- src/Dfe.PlanTech.Web/Middleware/SecurityHeadersMiddleware.cs
+++ src/Dfe.PlanTech.Web/Middleware/SecurityHeadersMiddleware.cs
@@ -1,5 +1,8 @@
 namespace Dfe.PlanTech.Web.Middleware;
 
+/// <summary>
+/// Middleware that sets various response headers for security reasons.
+/// </summary>
 public class SecurityHeadersMiddleware
 {
   private readonly ILogger<SecurityHeadersMiddleware> _logger;
@@ -13,17 +16,29 @@ public class SecurityHeadersMiddleware
 
   public async Task InvokeAsync(HttpContext context)
   {
-    context.Response.Headers["X-Frame-Options"] = "Deny";
-    context.Response.Headers["Content-Security-Policy"] = "frame-ancestors 'none'";
+    AddFramejackingPreventHeaders(context);
 
     await _next(context);
   }
+
+  /// <summary>
+  /// Adds headers that prevent framejacking vulnerability
+  /// </summary>
+  /// <param name="context"></param>
+  private static void AddFramejackingPreventHeaders(HttpContext context)
+  {
+    context.Response.Headers["X-Frame-Options"] = "Deny";
+    context.Response.Headers["Content-Security-Policy"] = "frame-ancestors 'none'";
+  }
 }
-
-
 
 public static class SecurityHeadersMiddlewareExtensions
 {
+  /// <summary>
+  /// Extension method for adding the above middleware in a cleaner way.
+  /// </summary>
+  /// <param name="app"></param>
+  /// <returns></returns>
   public static IApplicationBuilder UseSecurityHeaders(
       this IApplicationBuilder app) => app.UseMiddleware<SecurityHeadersMiddleware>();
 }
