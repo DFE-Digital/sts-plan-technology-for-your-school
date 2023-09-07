34ddab13310b4801f9713294fcbff7ddb62df197
Jim Washbrook <jim.washbrook@and.digital>
2023-09-07 11:50:47 +0100

    tests: Add security header tests

---
+++ tests/Dfe.PlanTech.Web.UnitTests/Middleware/SecurityHeadersMiddlewareTests.cs
@@ -0,0 +1,34 @@
+using Dfe.PlanTech.Web.Middleware;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using NSubstitute;
+using Xunit;
+
+namespace Dfe.PlanTech.Web.UnitTests.Middleware;
+
+public class SecurityHeadersMiddlewareTests
+{
+
+  [Fact]
+  public async Task Should_Set_Headers()
+  {
+    var context = new DefaultHttpContext();
+
+    var next = (HttpContext hc) => Task.CompletedTask;
+
+    var logger = new Logger<SecurityHeadersMiddleware>(new NullLoggerFactory());
+
+    var middleware = new SecurityHeadersMiddleware(logger, new RequestDelegate(next));
+
+    await middleware.InvokeAsync(context);
+
+    var response = context.Response;
+
+    Assert.NotEmpty(context.Response.Headers.XFrameOptions);
+    Assert.Equal("Deny", context.Response.Headers.XFrameOptions);
+
+    Assert.NotEmpty(context.Response.Headers.ContentSecurityPolicy);
+    Assert.Equal("frame-ancestors 'none'", context.Response.Headers.ContentSecurityPolicy);
+  }
+}
