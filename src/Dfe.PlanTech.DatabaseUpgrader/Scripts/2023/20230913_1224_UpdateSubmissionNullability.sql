f66a6f5019ebd2f16498972314ad29feb56d520e
jimwashbrook <131891854+jimwashbrook@users.noreply.github.com>
2023-09-13 12:58:32 +0100

    feat: Fix nullability in schema (#283)

---
+++ src/Dfe.PlanTech.DatabaseUpgrader/Scripts/2023/20230913_1224_UpdateSubmissionNullability.sql
@@ -0,0 +1,19 @@
+BEGIN TRY
+  BEGIN TRAN
+    SELECT Id into #submissionIds FROM [dbo].[submission] WHERE sectionId IS NULL OR sectionName IS NULL
+
+    DELETE FROM [dbo].[response] WHERE EXISTS (SELECT *
+                                FROM #submissionIds
+                                where submissionId = #submissionIds.id) 
+
+    DELETE FROM [dbo].[submission] WHERE sectionId IS NULL OR sectionName IS NULL
+
+    ALTER TABLE [dbo].[submission] ALTER COLUMN [sectionId] [NVARCHAR](50) NOT NULL
+    ALTER TABLE [dbo].[submission] ALTER COLUMN [sectionName] [NVARCHAR](50) NOT NULL
+    ALTER TABLE [dbo].[submission] DROP COLUMN [recommendationId]
+    
+  COMMIT TRAN
+END TRY
+BEGIN CATCH
+	ROLLBACK
+END CATCH
