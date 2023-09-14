ea7b41b109817ebb5218447ccc05dc0570e5501a
Jim Washbrook <jim.washbrook@and.digital>
2023-09-14 16:26:25 +0100

    tests: Test for question + answers

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/check-answer-page.cy.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/check-answer-page.cy.js
@@ -1,24 +1,63 @@
+let selectedQuestionsWithAnswers = [];
+
 describe("Check answers page", () => {
   const url = "/self-assessment";
 
   beforeEach(() => {
+    selectedQuestionsWithAnswers = [];
     cy.loginWithEnv(url);
 
     navigateToCheckAnswersPage();
+
+    cy.log(selectedQuestionsWithAnswers);
 
     cy.url().should("contain", "check-answers");
 
     cy.injectAxe();
   });
 
-  it("passes accessibility tests", () => {
-    cy.runAxe();
-  });
-
   it("submits answers", () => {
     submitAnswers();
 
     cy.url().should("contain", "self-assessment");
+  });
+
+  it("should show each selected question with answer", () => {
+    cy.get("div.govuk-summary-list__row")
+      .should("exist")
+      .and("have.length", selectedQuestionsWithAnswers.length)
+      .each((row) => {
+        const questionWithAnswer = {
+          question: null,
+          answer: null,
+        };
+
+        cy.wrap(row)
+          .find("dt.govuk-summary-list__key")
+          .should("exist")
+          .invoke("text")
+          .then((question) => (questionWithAnswer.question = question.trim()));
+
+        cy.wrap(row)
+          .find("dd.govuk-summary-list__value")
+          .invoke("text")
+          .then((answer) => (questionWithAnswer.answer = answer.trim()));
+
+        cy.wrap(questionWithAnswer).then(() => {
+          cy.log(JSON.stringify(selectedQuestionsWithAnswers));
+
+          const matchingQuestionWithAnswer = selectedQuestionsWithAnswers.find(
+            (qwa) => {
+              cy.log("looking for ", qwa);
+              return qwa.question == questionWithAnswer.question.trim();
+            }
+          );
+
+          expect(matchingQuestionWithAnswer.answer).to.equal(
+            questionWithAnswer.answer
+          );
+        });
+      });
   });
 
   it("shows notification banner after submitting answers", () => {
@@ -35,33 +74,38 @@ describe("Check answers page", () => {
         cy.wrap(link).should("have.attr", "title");
       });
   });
+
+  it("passes accessibility tests", () => {
+    cy.runAxe();
+  });
 });
 
 const navigateToCheckAnswersPage = () => {
   cy.clickFirstSection();
   cy.clickContinueButton();
 
-  navigateThroughQuestions();
+  return navigateThroughQuestions();
 };
 
-const navigateThroughQuestions = () =>
-  cy
-    .get("main")
+const navigateThroughQuestions = () => {
+  cy.get("main")
     .then(($main) => $main.find("form div.govuk-radios").length > 0)
     .then((onQuestionPage) => {
       if (!onQuestionPage) {
-        cy.log("no longer on question page");
         return Promise.resolve();
       }
 
-      cy.log("On question page");
-
-      cy.selectFirstRadioButton();
+      cy.selectFirstRadioButton().then((questionWithAnswer) =>
+        selectedQuestionsWithAnswers.push(questionWithAnswer)
+      );
       cy.saveAndContinue();
 
       return navigateThroughQuestions();
-    });
+    })
+    .then(() => {
 
-function submitAnswers() {
+    });
+};
+
+const submitAnswers = () =>
   cy.get("button.govuk-button").contains("Save and Submit").click();
-}
