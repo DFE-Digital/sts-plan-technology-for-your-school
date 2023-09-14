d583d1286c2d94c41910ad6394dd6f9681d26079
snipered2003 <snipered@hotmail.com>
2023-09-14 16:55:52 +0100

    wip

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/check-answer-page.cy.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/check-answer-page.cy.js
@@ -27,10 +27,10 @@ describe("Check answers page", () => {
     cy.get("div.govuk-notification-banner__header").should("exist");
   });
 
-  it("navigates to correct page when clicking change", () => {
-    cy.get("/html/body/div/main/div/div/dl/div[2]/dd[2]/a").url.click()
-    .should("match", url);
-  });
+  // it("navigates to correct page when clicking change", () => {
+  //   cy.get("/html/body/div/main/div/div/dl/div[2]/dd[2]/a").url.click()
+  //   .should("match", url);
+  // });
 });
 
 const navigateToCheckAnswersPage = () => {
