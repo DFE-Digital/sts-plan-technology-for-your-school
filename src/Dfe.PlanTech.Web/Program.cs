46a03f631836175e914640e44abfe8b03e5eea11
“Umar <umar.hussain@and.digital>
2023-09-15 10:51:51 +0100

    Remove old exception handling logic as it's been updated

--- src/Dfe.PlanTech.Web/Program.cs
+++ src/Dfe.PlanTech.Web/Program.cs
@@ -65,8 +65,7 @@ app.UseForwardedHeaders();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
+{ 
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
