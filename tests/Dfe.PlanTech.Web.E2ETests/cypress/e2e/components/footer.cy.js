89a8fc0f7b547cf1c4686d52b9b827de1c69a454
Jim Washbrook <jim.washbrook@and.digital>
2023-09-19 13:34:29 +0100

    feat: E2E test for opening in new tab

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/components/footer.cy.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/components/footer.cy.js
@@ -1,6 +1,6 @@
 describe("Footer", () => {
   const FOOTER_SELECTOR = "footer.govuk-footer";
-
+  const FOOTER_LINK_SELECTOR = `${FOOTER_SELECTOR} ul.govuk-footer__inline-list a.govuk-footer__link`;
   beforeEach(() => {
     cy.visit("/");
   });
@@ -10,10 +10,13 @@ describe("Footer", () => {
   });
 
   it("has footer links", () => {
-    const footerLinks = cy.get(`${FOOTER_SELECTOR} ul.govuk-footer__inline-list a.govuk-footer__link`);
-
-    footerLinks.should("exist")
+    cy.get(FOOTER_LINK_SELECTOR)
+      .should("exist")
       .and("be.visible")
       .and("contain", "Cookies");
+  });
+
+  it("should have a link that opens in new tab", () => {
+    cy.get(`${FOOTER_LINK_SELECTOR}[target]`).should("exist");
   });
 });
