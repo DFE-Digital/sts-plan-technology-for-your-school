ab4988739705c13e111b06d46c5b998826419a9d
snipered2003 <snipered@hotmail.com>
2023-09-20 11:55:28 +0100

    fixed broken end to end test

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/self-assessment.cy.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/self-assessment.cy.js
@@ -10,7 +10,7 @@ describe("Self-assessment page", () => {
   it("should have heading", () => {
     cy.get("h1.govuk-heading-xl")
       .should("exist")
-      .and("have.text", "Technology \u00ADself-assessment");
+      .and("have.text", "Technology\u00ADself-assessment");
     
   });
 
