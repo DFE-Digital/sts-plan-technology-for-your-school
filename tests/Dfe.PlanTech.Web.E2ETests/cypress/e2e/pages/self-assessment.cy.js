e56d2d434c48f7f0c24e2b6d1332158a3cc4f3d5
Jim Washbrook <jim.washbrook@and.digital>
2023-09-20 12:41:09 +0100

    fix: Check for nonbreaking hyphen

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/self-assessment.cy.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/self-assessment.cy.js
@@ -10,8 +10,7 @@ describe("Self-assessment page", () => {
   it("should have heading", () => {
     cy.get("h1.govuk-heading-xl")
       .should("exist")
-      .and("have.text", "Technology self\u00AD-assessment");
-    
+      .and("have.text", "Technology self‑assessment");
   });
 
   it("should contain categories", () => {
