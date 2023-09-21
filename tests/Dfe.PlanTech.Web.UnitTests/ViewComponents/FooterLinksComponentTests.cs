1d1264c12eed632e6d8a53f39d5a62b850c054b3
jimwashbrook <131891854+jimwashbrook@users.noreply.github.com>
2023-09-22 00:11:48 +0100

    feat: Add basic error handling for exception (#322)
    * feat: Add basic error handling for exception
    
    * fix: tests

--- tests/Dfe.PlanTech.Web.UnitTests/ViewComponents/FooterLinksComponentTests.cs
+++ tests/Dfe.PlanTech.Web.UnitTests/ViewComponents/FooterLinksComponentTests.cs
@@ -1,9 +1,13 @@
+using Castle.Core.Logging;
 using Dfe.PlanTech.Domain.Content.Interfaces;
 using Dfe.PlanTech.Domain.Content.Models;
 using Dfe.PlanTech.Web.ViewComponents;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using Xunit;
 
 namespace Dfe.PlanTech.Web.UnitTests.ViewComponents;
@@ -17,6 +21,8 @@ public class FooterLinksComponentTests
     }
   };
 
+  private readonly ILogger<FooterLinks> _logger = new NullLoggerFactory().CreateLogger<FooterLinks>();
+
   [Fact]
   public async Task It_Should_Retrieve_NavigationLinks()
   {
@@ -25,7 +31,7 @@ public class FooterLinksComponentTests
     getNavQuery.GetNavigationLinks(Arg.Any<CancellationToken>())
               .Returns(_navigationLinks);
 
-    var footerLinks = new FooterLinks(getNavQuery);
+    var footerLinks = new FooterLinks(getNavQuery, _logger);
 
     var result = await footerLinks.InvokeAsync();
 
@@ -37,4 +43,29 @@ public class FooterLinksComponentTests
 
     Assert.Equal(_navigationLinks, unboxed);
   }
+
+  [Fact]
+  public async Task It_Should_Return_EmptyArray_When_Exception()
+  {
+    Func<IEnumerable<NavigationLink>> getNavLinksWithError = () => throw new Exception("Error getting Contentful data");
+    
+    var getNavQuery = Substitute.For<IGetNavigationQuery>();
+
+    getNavQuery.GetNavigationLinks(Arg.Any<CancellationToken>())
+              .Returns((callinfo) => getNavLinksWithError());
+
+    var footerLinks = new FooterLinks(getNavQuery, _logger);
+
+    var result = await footerLinks.InvokeAsync();
+
+    var model = (result as ViewViewComponentResult)?.ViewData?.Model;
+
+    Assert.NotNull(model);
+
+    var unboxed = model as NavigationLink[];
+
+    Assert.NotNull(unboxed);
+    Assert.Empty(unboxed);
+  }
+
 }
