9ab2fa832034c93be898a36844dab4972d618007
Jim Washbrook <jim.washbrook@and.digital>
2023-09-21 15:12:39 +0100

    feat: Add basic error handling for exception

--- src/Dfe.PlanTech.Web/ViewComponents/FooterLinks.cs
+++ src/Dfe.PlanTech.Web/ViewComponents/FooterLinks.cs
@@ -1,4 +1,5 @@
 using Dfe.PlanTech.Domain.Content.Interfaces;
+using Dfe.PlanTech.Domain.Content.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dfe.PlanTech.Web.ViewComponents;
@@ -9,8 +10,13 @@ namespace Dfe.PlanTech.Web.ViewComponents;
 public class FooterLinks : ViewComponent
 {
   private readonly IGetNavigationQuery _getNavQuery;
+  private readonly ILogger<FooterLinks> _logger;
 
-  public FooterLinks(IGetNavigationQuery getNavQuery) => _getNavQuery = getNavQuery;
+  public FooterLinks(IGetNavigationQuery getNavQuery, ILogger<FooterLinks> logger)
+  {
+    _getNavQuery = getNavQuery;
+    _logger = logger;
+  }
 
   /// <summary>
   /// Retrieve the navigation links using <see cref="IGetNavigationQuery"/> then return the view
@@ -18,8 +24,26 @@ public class FooterLinks : ViewComponent
   /// <returns>The FooterLinks ViewViewComponentResult</returns>
   public async Task<IViewComponentResult> InvokeAsync()
   {
-    var items = await _getNavQuery.GetNavigationLinks();
+    var items = await GetLinks();
 
     return View(items);
+  }
+
+  /// <summary>
+  /// Gets links from Contentful using <see cref="IGetNavigationQuery"/> with basic error handling 
+  /// </summary>
+  /// <returns></returns>
+  private Task<IEnumerable<NavigationLink>> GetLinks()
+  {
+    try
+    {
+      return _getNavQuery.GetNavigationLinks();
+    }
+    catch (Exception ex)
+    {
+      _logger.LogCritical(ex, "Error retrieving navigation links for footer");
+
+      return Task.FromResult(Array.Empty<NavigationLink>().AsEnumerable());
+    }
   }
 }
