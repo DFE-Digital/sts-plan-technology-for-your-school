1fa2753cf94e6ca8a6e7d6936c912103108ae0d2
Jim Washbrook <jim.washbrook@and.digital>
2023-10-02 09:39:09 +0100

    tests: Start of checkanswersrouter tests

---
+++ tests/Dfe.PlanTech.Web.UnitTests/Routing/CheckAnswersRouterTests.cs
@@ -0,0 +1,13 @@
+using Dfe.PlanTech.Application.Content.Queries;
+using Xunit;
+
+namespace Dfe.PlanTech.Web.UnitTests.Routing;
+
+public class CheckAnswersRouterTests{
+  private readonly IGetPageQuery _getPageQuery;
+  [Fact]
+  public async Task Should_Redirect_To_InterstitialPage_When_Status_Is_InProgress_Or_NotStarted()
+  {
+
+  }
+}
