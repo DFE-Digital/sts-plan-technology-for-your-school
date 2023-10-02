cb0960bbd9c8f8eec83e7d0d75941a69b6040368
Zac King <zac.king@and.digital>
2023-10-02 11:19:07 +0100

    refactor: remove unused method

--- tests/Dfe.PlanTech.Web.UnitTests/Models/ComponentBuilder.cs
+++ tests/Dfe.PlanTech.Web.UnitTests/Models/ComponentBuilder.cs
@@ -128,11 +128,6 @@ namespace Dfe.PlanTech.Web.UnitTests.Models
             };
         }
 
-        private static IDictionary<string, string> GetSectionStatuses()
-        {
-            return new Dictionary<string, string> { { "3XQEHYfvEQkQwdrihDGagJ", "Completed" } };
-        }
-
         private static Page BuildPage(string? param = null)
         => new()
         {
