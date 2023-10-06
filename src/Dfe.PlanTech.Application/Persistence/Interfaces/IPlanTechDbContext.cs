0c61c83c425405792f6d4bce3ad0b34a43ad6b3f
jimwashbrook <131891854+jimwashbrook@users.noreply.github.com>
2023-10-06 16:46:57 +0100

    chore: Remove unused DB properties (#343)

--- src/Dfe.PlanTech.Application/Persistence/Interfaces/IPlanTechDbContext.cs
+++ src/Dfe.PlanTech.Application/Persistence/Interfaces/IPlanTechDbContext.cs
@@ -1,7 +1,4 @@
-﻿using Dfe.PlanTech.Domain.Answers.Models;
-using Dfe.PlanTech.Domain.Establishments.Models;
-using Dfe.PlanTech.Domain.Questions.Models;
-using Dfe.PlanTech.Domain.Responses.Models;
+﻿using Dfe.PlanTech.Domain.Establishments.Models;
 using Dfe.PlanTech.Domain.SignIns.Models;
 using Dfe.PlanTech.Domain.Submissions.Models;
 using Dfe.PlanTech.Domain.Users.Models;
@@ -13,29 +10,13 @@ namespace Dfe.PlanTech.Application.Persistence.Interfaces;
 public interface IPlanTechDbContext
 {
     // User Table & SignIn Table
-    IQueryable<User> GetUsers { get; }
-    IQueryable<SignIn> SignIns { get; }
     public void AddUser(User user);
     public void AddSignIn(SignIn signIn);
 
-    // Question Table
-    public void AddQuestion(Question question);
-    public Task<Question?> GetQuestion(Expression<Func<Question, bool>> predicate);
-
-    // Answer Table
-    public void AddAnswer(Answer answer);
-    public Task<Answer?> GetAnswer(Expression<Func<Answer, bool>> predicate);
-
     // Submission Table
     public IQueryable<Submission> GetSubmissions { get; }
-    public void AddSubmission(Submission submission);
 
     public void AddEstablishment(Establishment establishment);
-
-    // Response Table
-    public IQueryable<Response> GetResponses { get; }
-    public void AddResponse(Response response);
-    public Task<Response[]?> GetResponseList(Expression<Func<Response, bool>> predicate);
 
     public Task<int> SaveChangesAsync();
 
