89e1e7fd9849e8183f24f97a80fff27aa7b50f07
Jim Washbrook <jim.washbrook@and.digital>
2023-10-25 13:02:23 +0100

    feat: Tests

--- src/Dfe.PlanTech.Web/TagHelpers/WarningComponentTagHelper.cs
+++ src/Dfe.PlanTech.Web/TagHelpers/WarningComponentTagHelper.cs
@@ -1,18 +1,19 @@
 using Dfe.PlanTech.Domain.Content.Models;
-using Dfe.PlanTech.Web.TagHelpers.RichText;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using System.Text;
 
 namespace Dfe.PlanTech.Web.TagHelpers;
 public class WarningComponentTagHelper : TagHelper
 {
-  private readonly ILogger<WarningComponentTagHelper> _logger;
+  public const string OpeningDiv = """<div class="govuk-warning-text">""";
+  public const string WarningIcon = """<span class="govuk-warning-text__icon" aria-hidden="true">!</span>""";
+  public const string OpeningSpan = """<strong class="govuk-warning-text__text">""";
+  public const string AssistiveText = """<span class="govuk-warning-text__assistive">Warning</span>""";
 
   public WarningComponent? Warning { get; set; }
 
-  public WarningComponentTagHelper(ILogger<WarningComponentTagHelper> logger)
+  public WarningComponentTagHelper()
   {
-    _logger = logger;
   }
 
   public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
@@ -37,17 +38,17 @@ public class WarningComponentTagHelper : TagHelper
     return stringBuilder.ToString();
   }
 
+  private static void AppendOpenTag(StringBuilder stringBuilder)
+  {
+    stringBuilder.AppendLine(OpeningDiv);
+    stringBuilder.AppendLine(WarningIcon);
+    stringBuilder.AppendLine(OpeningSpan);
+    stringBuilder.AppendLine(AssistiveText);
+  }
+
   private static void AppendCloseTag(StringBuilder stringBuilder)
   {
     stringBuilder.AppendLine("</strong>");
     stringBuilder.AppendLine("</div>");
-  }
-
-  private void AppendOpenTag(StringBuilder stringBuilder)
-  {
-    stringBuilder.AppendLine("""<div class="govuk-warning-text">""");
-    stringBuilder.AppendLine("""<span class="govuk-warning-text__icon" aria-hidden="true">!</span>""");
-    stringBuilder.AppendLine("""<strong class="govuk-warning-text__text">""");
-    stringBuilder.AppendLine("""<span class="govuk-warning-text__assistive">Warning</span>""");
   }
 }
