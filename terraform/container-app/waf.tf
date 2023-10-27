77c5ae5b94fe16d7295fbaaba34fdebd2fb041fc
“Umar <umar.hussain@and.digital>
2023-10-27 15:29:03 +0100

    chore: Add specific versions to the main hosting and WAF modules

--- terraform/container-app/waf.tf
+++ terraform/container-app/waf.tf
@@ -1,5 +1,5 @@
 module "waf" {
-  source = "github.com/dfe-digital/terraform-azurerm-front-door-app-gateway-waf"
+  source = "github.com/dfe-digital/terraform-azurerm-front-door-app-gateway-waf?ref=v0.3.2"
 
   depends_on = [module.main_hosting]
 
