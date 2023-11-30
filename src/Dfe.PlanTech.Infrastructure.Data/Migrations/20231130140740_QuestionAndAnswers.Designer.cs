27d1d89b89eb4da2206b349020e8c07a9922e8a9
Jim Washbrook <jim.washbrook@and.digital>
2023-11-30 14:14:16 +0000

    feat: initial migration

--- src/Dfe.PlanTech.Infrastructure.Data/Migrations/20231130140740_QuestionAndAnswers.Designer.cs
+++ src/Dfe.PlanTech.Infrastructure.Data/Migrations/20231130140740_QuestionAndAnswers.Designer.cs
@@ -4,6 +4,7 @@ using Dfe.PlanTech.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Dfe.PlanTech.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(CmsDbContext))]
-    partial class CmsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231130140740_QuestionAndAnswers")]
+    partial class QuestionAndAnswers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
