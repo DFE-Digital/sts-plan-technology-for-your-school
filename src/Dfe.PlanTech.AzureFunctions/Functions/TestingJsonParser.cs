ea07e8e813261ec148fd61571bc5ceb39e50e6be
Jim Washbrook <jim.washbrook@and.digital>
2023-12-04 12:44:12 +0000

    wip: serialise

--- src/Dfe.PlanTech.AzureFunctions/Functions/TestingJsonParser.cs
+++ src/Dfe.PlanTech.AzureFunctions/Functions/TestingJsonParser.cs
@@ -1,8 +1,11 @@
 using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 using AutoMapper;
+using Dfe.PlanTech.Domain.Caching.Models;
 using Dfe.PlanTech.Domain.Content.Models;
+using Dfe.PlanTech.Domain.Questionnaire.Models;
 using Dfe.PlanTech.Infrastructure.Data;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -31,24 +34,22 @@ namespace Dfe.PlanTech.AzureFunctions
 
     private static List<Type> GetAllTypes() => AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembley => assembley.GetTypes()).ToList();
 
-    [Function("TestingJsonParser")]
-    public async Task<HttpResponseData> WebhookReceiver([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+    [Function("TestingSerialisation")]
+    public async Task<HttpResponseData> TestingSerialisation([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
     {
       var stream = new StreamReader(req.Body);
       var body = stream.ReadToEnd();
 
       try
       {
-        var processed = TryNormaliseJson(body, out JsonNode? node);
+        var processed = JsonSerializer.Deserialize<CmsWebHookPayload>(body, new JsonSerializerOptions()
+        {
+          PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        });
 
-        var responseType = processed ? HttpStatusCode.OK : HttpStatusCode.InternalServerError;
+        var responseType = HttpStatusCode.OK;
 
         var response = req.CreateResponse(responseType);
-
-        if (node != null)
-        {
-          await response.WriteAsJsonAsync(node);
-        }
 
         return response;
       }
@@ -72,38 +73,51 @@ namespace Dfe.PlanTech.AzureFunctions
       }
     }
 
-    private bool TryNormaliseJson(string text, out JsonNode? jsonNode)
+    [Function("TestingJsonParser")]
+    public async Task<HttpResponseData> WebhookReceiver([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
     {
-      var asJson = JsonNode.Parse(text);
+      var stream = new StreamReader(req.Body);
+      var body = stream.ReadToEnd();
 
-      if (asJson == null)
+      try
       {
-        _logger.LogError("Invalid JSON");
-        jsonNode = null;
-        return false;
+        var processed = TryNormaliseJson(body);
+
+        var responseType = processed ? HttpStatusCode.OK : HttpStatusCode.InternalServerError;
+
+        var response = req.CreateResponse(responseType);
+
+        return response;
       }
-
-      var fieldsNode = asJson["fields"];
-
-      if (fieldsNode == null)
+      catch (Exception ex)
       {
-        _logger.LogError("Missing fields");
-        jsonNode = null;
-        return false;
+        var response = req.CreateResponse(HttpStatusCode.InternalServerError);
+
+        var sb = new StringBuilder();
+
+        var currentException = ex;
+
+        while (currentException != null)
+        {
+          sb.AppendLine(currentException.Message);
+          sb.AppendLine(currentException.StackTrace);
+          currentException = ex.InnerException;
+        }
+        await response.WriteStringAsync(sb.ToString());
+
+        return response;
       }
+    }
 
-      var sys = asJson["sys"];
+    private bool TryNormaliseJson(string body)
+    {
+      var success = SerialiseBody(body, out CmsWebHookPayload? payload);
 
-      if (sys == null)
-      {
-        _logger.LogError("Missing sys");
-        jsonNode = null;
-        return false;
-      }
+      if (!success) return false;
 
-      var fields = new JsonObject();
+      var fields = new Dictionary<string, object>();
 
-      foreach (var field in fieldsNode.AsObject())
+      foreach (var field in payload!.Fields)
       {
         if (field.Value == null)
         {
@@ -121,43 +135,62 @@ namespace Dfe.PlanTech.AzureFunctions
 
         foreach (var child in fieldChildren)
         {
-          fields[FirstCharToUpperAsSpan(field.Key)] = JsonNode.Parse(child.Value!.ToJsonString());
+          fields[FirstCharToUpperAsSpan(field.Key)] = child.Value;
         }
       }
 
-      fields["Id"] = JsonNode.Parse(sys["id"].ToJsonString());
+      fields["Id"] = payload.Sys.Id;
 
-      var contentType = $"{FirstCharToUpperAsSpan(sys["contentType"]["sys"]["id"].ToString())}DbEntity";
+      var contentType = $"{FirstCharToUpperAsSpan(payload.Sys.ContentType.Sys.Id)}DbEntity";
       fields["ContentType"] = contentType;
 
-      jsonNode = fields;
-
-      object mapped = MapObjectToDbEntity(jsonNode, contentType);
-
-      if (mapped is ContentComponentDbEntity contentComponent)
-      {
-        var existing = _db.Find(mapped.GetType(), contentComponent.Id);
-
-        if (existing != null)
-        {
-          var properties = mapped.GetType().GetProperties();
-
-          foreach (var property in properties)
-          {
-            property.SetValue(existing, property.GetValue(mapped));
-          }
-        }
-        else
-        {
-          _db.Add(mapped);
-        }
-
-        _db.SaveChanges();
-      }
+      var serialised = JsonSerializer.Serialize(fields);
 
       return true;
+      /*
+            object mapped = MapObjectToDbEntity(jsonNode, contentType);
+
+            if (mapped is ContentComponentDbEntity contentComponent)
+            {
+              var existing = _db.Find(mapped.GetType(), contentComponent.Id);
+
+              if (existing != null)
+              {
+                var properties = mapped.GetType().GetProperties();
+
+                foreach (var property in properties)
+                {
+                  property.SetValue(existing, property.GetValue(mapped));
+                }
+              }
+              else
+              {
+                _db.Add(mapped);
+              }
+
+              _db.SaveChanges();
+            }
+
+            return true;
+            */
     }
 
+    private static bool SerialiseBody(string body, out CmsWebHookPayload? payload)
+    {
+      try
+      {
+        payload = JsonSerializer.Deserialize<CmsWebHookPayload>(body, new JsonSerializerOptions()
+        {
+          PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        });
+
+        return payload != null;
+      }
+      catch (Exception ex)
+      {
+        throw new Exception($"Error serialising body to {typeof(CmsWebHookPayload)}", ex);
+      }
+    }
 
     private object MapObjectToDbEntity(JsonNode jsonNode, string contentType)
     {
