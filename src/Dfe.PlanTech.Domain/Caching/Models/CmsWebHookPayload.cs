e932404f328d518b82a8797ea86c137767ac9f76
Jim Washbrook <jim.washbrook@and.digital>
2023-12-04 12:44:18 +0000

    feat: add fields

--- src/Dfe.PlanTech.Domain/Caching/Models/CmsWebHookPayload.cs
+++ src/Dfe.PlanTech.Domain/Caching/Models/CmsWebHookPayload.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Nodes;
+
 namespace Dfe.PlanTech.Domain.Caching.Models;
 
 public class CmsWebHookPayload
 {
+  public Dictionary<string, JsonNode> Fields { get; init; } = null!;
   public CmsWebHookSystemDetails Sys { get; init; } = null!;
 }
 
