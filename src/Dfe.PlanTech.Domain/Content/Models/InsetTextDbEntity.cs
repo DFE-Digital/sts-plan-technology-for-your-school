b2fa3a0b49ee9e0c4e6436c843359d3c167b5f90
Jim Washbrook <jim.washbrook@and.digital>
2023-12-05 15:06:29 +0000

    feat: InsetText entity

--- src/Dfe.PlanTech.Domain/Content/Models/InsetTextDbEntity.cs
+++ src/Dfe.PlanTech.Domain/Content/Models/InsetTextDbEntity.cs
@@ -3,10 +3,10 @@ using Dfe.PlanTech.Domain.Content.Interfaces;
 namespace Dfe.PlanTech.Domain.Content.Models;
 
 /// <summary>
-/// Model for Inset Text component type
+/// Model for database table for the Inset Text component type
 /// </summary>
 /// <inheritdoc/>
-public class InsetText : ContentComponent, IInsetText
+public class InsetTextDbEntity : ContentComponentDbEntity, IInsetText
 {
     public string Text { get; init; } = null!;
 }
