2aef760ad26bc93d2b36ea54ba29e3b99d66f5b8
Jim Washbrook <jim.washbrook@and.digital>
2023-12-05 15:04:15 +0000

    feat: add bools for content states

--- src/Dfe.PlanTech.Domain/Content/Models/ContentComponentDbEntity.cs
+++ src/Dfe.PlanTech.Domain/Content/Models/ContentComponentDbEntity.cs
@@ -6,6 +6,12 @@ public abstract class ContentComponentDbEntity : IContentComponentDbEntity
 {
   public string Id { get; set; } = null!;
 
+  public bool Published { get; set; }
+
+  public bool Archived { get; set; }
+
+  public bool Deleted { get; set; }
+
   public PageDbEntity[] BeforeTitleContentPages { get; set; } = Array.Empty<PageDbEntity>();
 
   public PageDbEntity[] ContentPages { get; set; } = Array.Empty<PageDbEntity>();
