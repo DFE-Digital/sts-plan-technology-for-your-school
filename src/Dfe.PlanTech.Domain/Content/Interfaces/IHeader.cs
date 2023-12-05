cbe9e9740565397b6e18576969b2531898a375ea
Jim Washbrook <jim.washbrook@and.digital>
2023-12-05 15:01:44 +0000

    feat: header interface

---
+++ src/Dfe.PlanTech.Domain/Content/Interfaces/IHeader.cs
@@ -0,0 +1,21 @@
+using Dfe.PlanTech.Domain.Content.Enums;
+
+namespace Dfe.PlanTech.Domain.Content.Interfaces;
+
+public interface IHeader
+{
+  /// <summary>
+  /// The text to display
+  /// </summary>
+  public string Text { get; }
+
+  /// <summary>
+  /// What tag should be used for header (e.g. H1, H2, etc.)
+  /// </summary>
+  public HeaderTag Tag { get; }
+
+  /// <summary>
+  /// Actual size of header to use (for accessibility)
+  /// </summary>
+  public HeaderSize Size { get; }
+}
--- src/Dfe.PlanTech.Domain/Content/Models/Header.cs
+++ src/Dfe.PlanTech.Domain/Content/Models/Header.cs
@@ -1,24 +1,16 @@
 using Dfe.PlanTech.Domain.Content.Enums;
+using Dfe.PlanTech.Domain.Content.Interfaces;
 
 namespace Dfe.PlanTech.Domain.Content.Models;
 
 /// <summary>
 /// Model for Header content type
 /// </summary>
-public class Header : ContentComponent
+public class Header : ContentComponent, IHeader
 {
-    /// <summary>
-    /// The text to display
-    /// </summary>
     public string Text { get; init; } = null!;
 
-    /// <summary>
-    /// What tag should be used for header (e.g. H1, H2, etc.)
-    /// </summary>
     public HeaderTag Tag { get; init; }
 
-    /// <summary>
-    /// Actual size of header to use (for accessibility)
-    /// </summary>
     public HeaderSize Size { get; init; }
 }
