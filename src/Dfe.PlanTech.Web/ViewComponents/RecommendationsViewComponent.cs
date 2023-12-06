b73c3bd4312e7d1177e0bf348dbc082d0beea1f4
Jim Washbrook <jim.washbrook@and.digital>
2023-12-06 15:04:35 +0000

    feat: fix issues due to changes

--- src/Dfe.PlanTech.Web/ViewComponents/RecommendationsViewComponent.cs
+++ src/Dfe.PlanTech.Web/ViewComponents/RecommendationsViewComponent.cs
@@ -1,5 +1,6 @@
 using Dfe.PlanTech.Domain.Interfaces;
 using Dfe.PlanTech.Domain.Questionnaire.Interfaces;
+using Dfe.PlanTech.Domain.Questionnaire.Models;
 using Dfe.PlanTech.Domain.Submissions.Models;
 using Dfe.PlanTech.Web.Models;
 using Microsoft.AspNetCore.Mvc;
