775cb9673055e5e268860b5649d712f0a6b80036
Jim Washbrook <jim.washbrook@and.digital>
2023-12-07 15:13:37 +0000

    Revert "feat: Migrations -> SQL script"
    This reverts commit 43b3fc5d431d48fabd1b2d99c6fb8313d03171ec.

---
+++ src/Dfe.PlanTech.Infrastructure.Data/Migrations/20231201153317_RemoveParamFromQuestion.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.PlanTech.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveParamFromQuestion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Param",
+                schema: "Contentful",
+                table: "Questions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Param",
+                schema: "Contentful",
+                table: "Questions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
