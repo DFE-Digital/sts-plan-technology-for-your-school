f5f53514df1ecaf09dc09e266ce76e3ae772fce3
Jim Washbrook <jim.washbrook@and.digital>
2023-12-07 16:00:27 +0000

    feat: RichTextContent mapper

--- src/Dfe.PlanTech.AzureFunctions/Mappings/RichTextContentMapper.cs
+++ src/Dfe.PlanTech.AzureFunctions/Mappings/RichTextContentMapper.cs
@@ -14,14 +14,18 @@ public class RichTextContentMapper
     var entity = new RichTextContentDbEntity()
     {
       Content = content.Content.Select(MapContent).ToList(),
-      Data = MapData<RichTextDataDbEntity, RichTextData>(content?.Data),
+      Data = MapData<RichTextData, RichTextDataDbEntity>(content.Data),
+      Marks = content.Marks.Select(MapMark<RichTextMark, RichTextMarkDbEntity>).ToList(),
+      Value = content.Value,
+      NodeType = content.NodeType
+    };
 
-    }
+    return entity;
   }
 
-  protected virtual TOutData? MapData<TOutData, TInData>(TInData? data)
-  where TOutData : class, IRichTextData, new()
+  protected virtual TOutData? MapData<TInData, TOutData>(TInData? data)
   where TInData : class, IRichTextData
+  where TOutData : class, IRichTextData, new()
   {
     if (data == null) return null;
 
@@ -31,5 +35,17 @@ public class RichTextContentMapper
     };
 
     return outData;
+  }
+
+  protected virtual TOutMark MapMark<TInMark, TOutMark>(TInMark mark)
+    where TInMark : class, IRichTextMark
+  where TOutMark : class, IRichTextMark, new()
+  {
+    var outMark = new TOutMark()
+    {
+      Type = mark.Type
+    };
+
+    return outMark;
   }
 }
