6d64b3953bc424b1d623d15ced647cf4b76e153b
Jim Washbrook <jim.washbrook@and.digital>
2023-12-11 13:58:58 +0000

    chore: Remove incorrect code, match updated base class

--- src/Dfe.PlanTech.AzureFunctions/Mappings/AnswerMapper.cs
+++ src/Dfe.PlanTech.AzureFunctions/Mappings/AnswerMapper.cs
@@ -1,5 +1,4 @@
 using System.Text.Json;
-using Dfe.PlanTech.Domain.Caching.Models;
 using Dfe.PlanTech.Domain.Questionnaire.Models;
 using Microsoft.Extensions.Logging;
 
@@ -13,26 +12,7 @@ public class AnswerMapper : JsonToDbMapper<AnswerDbEntity>
 
   public override Dictionary<string, object?> PerformAdditionalMapping(Dictionary<string, object?> values)
   {
-    values = MoveKeyToNewValue(values, "nextQuestion", "nextQuestionId");
-    values = MoveKeyToNewValue(values, "parentQuestion", "parentQuestionId");
-
-    return values;
-  }
-
-  /// <summary>
-  /// Move the value from the current key to the new key
-  /// </summary>
-  /// <param name="values"></param>
-  /// <param name="currentKey"></param>
-  /// <param name="newKey"></param>
-  /// <returns></returns>
-  private static Dictionary<string, object?> MoveKeyToNewValue(Dictionary<string, object?> values, string currentKey, string newKey)
-  {
-    if (values.TryGetValue(currentKey, out object? value) && value is CmsWebHookSystemDetailsInner systemDetailsInner)
-    {
-      values[newKey] = systemDetailsInner.Id;
-      values.Remove(currentKey);
-    }
+    values = MoveValueToNewKey(values, "nextQuestion", "nextQuestionId");
 
     return values;
   }
