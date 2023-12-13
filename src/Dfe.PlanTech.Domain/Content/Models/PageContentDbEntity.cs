bad7505f26ff00d5b7372ea3234e1c46c1721400
Jim Washbrook <jim.washbrook@and.digital>
2023-12-13 15:50:49 +0000

    chore: array->list in page types
    due to EF core problem with arrays

--- src/Dfe.PlanTech.Domain/Content/Models/PageContentDbEntity.cs
+++ src/Dfe.PlanTech.Domain/Content/Models/PageContentDbEntity.cs
@@ -2,7 +2,9 @@ namespace Dfe.PlanTech.Domain.Content.Models;
 
 public class PageContentDbEntity
 {
+    public string PageId { get; set; } = null!;
     public PageDbEntity Page { get; set; } = null!;
 
+    public string ContentComponentId { get; set; } = null!;
     public ContentComponentDbEntity ContentComponent { get; set; } = null!;
 }
