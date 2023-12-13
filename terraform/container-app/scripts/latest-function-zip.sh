c24f83216835b5beed98abe4d0be32ce13cdb0e3
uahussain12 <umar.hussain@and.digital>
2023-12-13 09:06:24 +0000

    deploy function as part of tf apply

--- terraform/container-app/scripts/latest-function-zip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-storage_account_name="$1"
-container_name="function-releases"
-
-latest_blob=$(az storage blob list --account-name $storage_account_name --container-name $container_name --query "[?contains(name,'.zip')].{Name:name,LastModified:properties.lastModified}" --output json | jq -r 'max_by(.LastModified).Name')
-
-echo {\"zip\":\""$latest_blob"\"}
