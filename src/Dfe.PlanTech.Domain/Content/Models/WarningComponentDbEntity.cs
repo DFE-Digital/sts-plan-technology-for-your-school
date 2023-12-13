af60d67f8a190010e31b1ba6b5494708f8f1b958
Jim Washbrook <jim.washbrook@and.digital>
2023-12-13 15:50:24 +0000

    feat: add foreign key to warningcomponent

--- src/Dfe.PlanTech.Domain/Content/Models/WarningComponentDbEntity.cs
+++ src/Dfe.PlanTech.Domain/Content/Models/WarningComponentDbEntity.cs
@@ -5,5 +5,6 @@ namespace Dfe.PlanTech.Domain.Content.Models;
 
 public class WarningComponentDbEntity : ContentComponentDbEntity, IWarningComponent<TextBodyDbEntity>
 {
+    public string TextId { get; set; } = null!;
     public TextBodyDbEntity Text { get; set; } = null!;
 }
