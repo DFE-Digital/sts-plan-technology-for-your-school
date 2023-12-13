9f980898e3e220057522a47fc151125963142ddf
Jim Washbrook <jim.washbrook@and.digital>
2023-12-13 14:50:36 +0000

    tests: TextBodyMapper tests

---
+++ tests/Dfe.PlanTech.AzureFunctions.UnitTests/Mappers/TextBodyMapperTests.cs
@@ -0,0 +1,41 @@
+using Dfe.PlanTech.AzureFunctions.Mappings;
+using Dfe.PlanTech.Domain.Content.Models;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
+namespace Dfe.PlanTech.AzureFunctions.UnitTests;
+
+public class TextBodyMapperTests : BaseMapperTests
+{
+  private string TextBodyId = "text body id";
+  private readonly TextBodyMapper _mapper;
+  private readonly ILogger<TextBodyMapper> _logger;
+  private readonly RichTextContentMapper _richTextMapper = new();
+
+  public TextBodyMapperTests()
+  {
+    _logger = Substitute.For<ILogger<TextBodyMapper>>();
+    _mapper = new TextBodyMapper(_richTextMapper, _logger, JsonOptions);
+  }
+
+  [Fact]
+  public void Mapper_Should_Map_ToDbEntity()
+  {
+    var richText = RichTextContentMapperTests.GenerateContentParent();
+    var fields = new Dictionary<string, object?>()
+    {
+      ["richText"] = WrapWithLocalisation(richText),
+    };
+
+    var payload = CreatePayload(fields, TextBodyId);
+
+    var mapped = _mapper.MapEntity(payload);
+
+    Assert.NotNull(mapped);
+
+    var concrete = mapped as TextBodyDbEntity;
+    Assert.NotNull(concrete);
+
+    Assert.True(RichTextContentMapperTests.ContentMatches<RichTextContent, RichTextData, RichTextMark, RichTextContentDbEntity, RichTextDataDbEntity, RichTextMarkDbEntity>(richText, concrete.RichText));
+  }
+}
