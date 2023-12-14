57e6c7880387cddd711cd8a013adee3fb9c03e9e
Jim Washbrook <jim.washbrook@and.digital>
2023-12-14 16:46:25 +0000

    feat: remaining mappers

---
+++ src/Dfe.PlanTech.AzureFunctions/Mappings/ButtonDbMapper.cs
@@ -0,0 +1,17 @@
+using System.Text.Json;
+using Dfe.PlanTech.Domain.Content.Models.Buttons;
+using Microsoft.Extensions.Logging;
+
+namespace Dfe.PlanTech.AzureFunctions.Mappings;
+
+public class ButtonDbMapper : JsonToDbMapper<ButtonDbEntity>
+{
+  public ButtonDbMapper(ILogger<ButtonDbMapper> logger, JsonSerializerOptions jsonSerialiserOptions) : base(logger, jsonSerialiserOptions)
+  {
+  }
+
+  public override Dictionary<string, object?> PerformAdditionalMapping(Dictionary<string, object?> values)
+  {
+    return values;
+  }
+}
