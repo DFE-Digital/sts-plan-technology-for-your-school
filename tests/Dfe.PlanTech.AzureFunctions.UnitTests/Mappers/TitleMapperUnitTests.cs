795c78bf5b780f48e08a0c717394302f89bc8076
Jim Washbrook <jim.washbrook@and.digital>
2023-12-14 11:31:30 +0000

    tests: title mapper unit tests

---
+++ tests/Dfe.PlanTech.AzureFunctions.UnitTests/Mappers/TitleMapperUnitTests.cs
@@ -0,0 +1,42 @@
+using Dfe.PlanTech.AzureFunctions.Mappings;
+using Dfe.PlanTech.Domain.Content.Models;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
+namespace Dfe.PlanTech.AzureFunctions.UnitTests;
+
+public class TitleMapperUnitTests : BaseMapperTests
+{
+  private const string TitleText = "Title text";
+  private const string TitleId = "Header Id";
+
+  private readonly TitleMapper _mapper;
+  private readonly ILogger<TitleMapper> _logger;
+
+  public TitleMapperUnitTests()
+  {
+    _logger = Substitute.For<ILogger<TitleMapper>>();
+    _mapper = new TitleMapper(_logger, JsonOptions);
+  }
+
+  [Fact]
+  public void Mapper_Should_Map_NavigationLink()
+  {
+    var fields = new Dictionary<string, object?>()
+    {
+      ["text"] = WrapWithLocalisation(TitleText),
+    };
+
+    var payload = CreatePayload(fields, TitleId);
+
+    var mapped = _mapper.MapEntity(payload);
+
+    Assert.NotNull(mapped);
+
+    var concrete = mapped as TitleDbEntity;
+    Assert.NotNull(concrete);
+
+    Assert.Equal(TitleId, concrete.Id);
+    Assert.Equal(TitleText, concrete.Text);
+  }
+}
