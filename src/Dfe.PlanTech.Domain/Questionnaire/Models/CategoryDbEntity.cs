5e98a68c4e389ec6d554f88383c41a50477de03e
Jim Washbrook <jim.washbrook@and.digital>
2023-12-15 12:26:42 +0000

    chore: add headerid to category

--- src/Dfe.PlanTech.Domain/Questionnaire/Models/CategoryDbEntity.cs
+++ src/Dfe.PlanTech.Domain/Questionnaire/Models/CategoryDbEntity.cs
@@ -5,6 +5,7 @@ namespace Dfe.PlanTech.Domain.Questionnaire.Models;
 
 public class CategoryDbEntity : ContentComponentDbEntity, ICategory<HeaderDbEntity, SectionDbEntity>
 {
+    public string HeaderId { get; set; } = null!;
     public HeaderDbEntity Header { get; set; } = null!;
 
     public List<SectionDbEntity> Sections { get; set; } = new();
