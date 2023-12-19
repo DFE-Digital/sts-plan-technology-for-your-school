2e59a09001e5a4953502994d4b3892e0a19e0dbb
Zac King <zac.king@and.digital>
2023-12-19 15:19:43 +0000

    test: fix test

--- tests/Dfe.PlanTech.AzureFunctions.UnitTests/Functions/QueueReceiverTests.cs
+++ tests/Dfe.PlanTech.AzureFunctions.UnitTests/Functions/QueueReceiverTests.cs
@@ -4,17 +4,19 @@ using System.Text.Json.Serialization;
 using Azure.Messaging.ServiceBus;
 using Dfe.PlanTech.AzureFunctions.Mappings;
 using Dfe.PlanTech.AzureFunctions.UnitTests.Mappers;
-using Dfe.PlanTech.Domain.Content.Models;
-using Dfe.PlanTech.Domain.Content.Models.Buttons;
+using Dfe.PlanTech.Domain.Questionnaire.Models;
 using Dfe.PlanTech.Infrastructure.Data;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
+using NSubstitute.ReceivedExtensions;
 
 namespace Dfe.PlanTech.AzureFunctions.UnitTests;
 
 public class QueueReceiverTests
 {
+    private const string bodyJsonStr = "{\"metadata\":{\"tags\":[]},\"fields\":{\"internalName\":{\"en-US\":\"TestingQuestion\"},\"text\":{\"en-US\":\"TestingQuestion\"},\"helpText\":{\"en-US\":\"HelpText\"},\"answers\":{\"en-US\":[{\"sys\":{\"type\":\"Link\",\"linkType\":\"Entry\",\"id\":\"4QscetbCYG4MUsGdoDU0C3\"}}]},\"slug\":{\"en-US\":\"testing-slug\"}},\"sys\":{\"type\":\"Entry\",\"id\":\"2VSR0emw0SPy8dlR9XlgfF\",\"space\":{\"sys\":{\"type\":\"Link\",\"linkType\":\"Space\",\"id\":\"py5afvqdlxgo\"}},\"environment\":{\"sys\":{\"id\":\"dev\",\"type\":\"Link\",\"linkType\":\"Environment\"}},\"contentType\":{\"sys\":{\"type\":\"Link\",\"linkType\":\"ContentType\",\"id\":\"question\"}},\"createdBy\":{\"sys\":{\"type\":\"Link\",\"linkType\":\"User\",\"id\":\"5yhMQOCN9P2vGpfjyZKiey\"}},\"updatedBy\":{\"sys\":{\"type\":\"Link\",\"linkType\":\"User\",\"id\":\"4hiJvkyVWdhTt6c4ZoDkMf\"}},\"revision\":13,\"createdAt\":\"2023-12-04T14:36:46.614Z\",\"updatedAt\":\"2023-12-15T16:16:45.034Z\"}}";
+
     private readonly QueueReceiver _queueReceiver;
 
     private readonly ILoggerFactory _loggerFactoryMock;
@@ -40,13 +42,9 @@ public class QueueReceiverTests
             Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) },
         };
 
-        Type buttonWithLinkDbEntityType = typeof(ButtonWithLinkDbEntity);
+        Type questionDbEntityType = typeof(QuestionDbEntity);
 
-        //_jsonToEntityMappers = new JsonToEntityMappers(new JsonToDbMapper[] { new JsonToDbMapperImplementation(buttonWithLinkDbEntityType, _loggerMock, jsonOptions) }, jsonOptions);
-
-        //JsonToDbMapperImplementation jsonToDbMapperImplementation = Substitute.For<JsonToDbMapperImplementation>(buttonWithLinkDbEntityType, _loggerMock, jsonOptions);
-
-        _jsonToEntityMappers = Substitute.For<JsonToEntityMappers>(new JsonToDbMapper[] { new JsonToDbMapperImplementation(buttonWithLinkDbEntityType, _loggerMock, jsonOptions) }, jsonOptions);
+        _jsonToEntityMappers = Substitute.For<JsonToEntityMappers>(new JsonToDbMapper[] { new JsonToDbMapperImplementation(questionDbEntityType, _loggerMock, jsonOptions) }, jsonOptions);
 
         _queueReceiver = new QueueReceiver(_loggerFactoryMock, _cmsDbContextMock, _jsonToEntityMappers);
     }
@@ -54,19 +52,18 @@ public class QueueReceiverTests
     [Fact]
     public async Task QueueReceiverDbWriter_Should_Execute_Successfully()
     {
+        _cmsDbContextMock.SaveChangesAsync().Returns(1);
+
         ServiceBusReceivedMessage serviceBusReceivedMessageMock = Substitute.For<ServiceBusReceivedMessage>();
         ServiceBusMessageActions serviceBusMessageActionsMock = Substitute.For<ServiceBusMessageActions>();
 
         var subject = "ContentManagement.Entry.create";
-        var body = "{\"Body\":\"RequestBody\"}";
-        var serviceBusMessage = new ServiceBusMessage(body) { Subject = subject };
-
-        ContentComponentDbEntity contentComponentDbEntity = new ButtonWithLinkDbEntity();
-
-        //_jsonToEntityMappers.ToEntity(body).Returns(contentComponentDbEntity);
+        var serviceBusMessage = new ServiceBusMessage(bodyJsonStr) { Subject = subject };
 
         ServiceBusReceivedMessage serviceBusReceivedMessage = ServiceBusReceivedMessage.FromAmqpMessage(serviceBusMessage.GetRawAmqpMessage(), BinaryData.FromBytes(Encoding.UTF8.GetBytes(serviceBusReceivedMessageMock.LockToken)));
 
         await _queueReceiver.QueueReceiverDbWriter(new ServiceBusReceivedMessage[] { serviceBusReceivedMessage }, serviceBusMessageActionsMock);
+
+        await serviceBusMessageActionsMock.Received().CompleteMessageAsync(Arg.Any<ServiceBusReceivedMessage>());
     }
 }
