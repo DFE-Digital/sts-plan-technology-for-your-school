37e52739de684cd3385dccb1642ac9bfc6117dfb
Jim Washbrook <jim.washbrook@and.digital>
2024-01-12 12:08:00 +0000

    fix: fix unit tests

--- tests/Dfe.PlanTech.AzureFunctions.UnitTests/AsyncEnumerable.cs
+++ tests/Dfe.PlanTech.AzureFunctions.UnitTests/AsyncEnumerable.cs
@@ -1,18 +1,17 @@
 using System.Linq.Expressions;
 
-namespace Dfe.PlanTech.AzureFunctions.UnitTests
+namespace Dfe.PlanTech.AzureFunctions.UnitTests;
+
+public class AsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
 {
-  public class AsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+  public AsyncEnumerable(IEnumerable<T> enumerable) : base(enumerable) { }
+
+  public AsyncEnumerable(Expression expression) : base(expression) { }
+
+  public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
   {
-    public AsyncEnumerable(IEnumerable<T> enumerable) : base(enumerable) { }
-
-    public AsyncEnumerable(Expression expression) : base(expression) { }
-
-    public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
-    {
-      return new AsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
-    }
-
-    IQueryProvider IQueryable.Provider => this;
+    return new AsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
   }
+
+  IQueryProvider IQueryable.Provider => this;
 }
