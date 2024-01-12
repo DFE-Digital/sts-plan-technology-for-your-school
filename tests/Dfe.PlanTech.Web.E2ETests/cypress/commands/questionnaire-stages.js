54ae98225e184ac4dda595ef3bde140e52fe83ec
Jim Washbrook <jim.washbrook@and.digital>
2024-01-12 16:52:58 +0000

    feat: add e2e tests

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/commands/questionnaire-stages.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/commands/questionnaire-stages.js
@@ -1,41 +1,64 @@
 const navigateToRecommendationPage = () => {
-    let selectedQuestionsWithAnswers = []; // Not needed by the calling class so just define it here.
-    return cy.navigateToCheckAnswersPage(selectedQuestionsWithAnswers).then((res) => cy.wrap(res)).then(() => cy.submitAnswers()).then((onSelfAssessmentPage) => {
-        if (!onSelfAssessmentPage) {
-            return Promise.resolve();
-        }
+  let selectedQuestionsWithAnswers = []; // Not needed by the calling class so just define it here.
+  return cy
+    .navigateToCheckAnswersPage(selectedQuestionsWithAnswers)
+    .then((res) => cy.wrap(res))
+    .then(() => cy.submitAnswers())
+    .then((onSelfAssessmentPage) => {
+      if (!onSelfAssessmentPage) {
+        return Promise.resolve();
+      }
 
-        cy.get('a[href*="/recommendation/"]').first().click();
+      cy.get('a[href*="/recommendation/"]').first().click();
+    });
+};
+
+const navigateThroughSection = () => {
+  let selectedQuestionsWithAnswers = []; // Not needed by the calling class so just define it here.
+  return cy
+    .navigateToCheckAnswersPage(selectedQuestionsWithAnswers)
+    .then((res) => cy.wrap(res))
+    .then(() => cy.submitAnswers())
+    .then((onSelfAssessmentPage) => {
+      if (!onSelfAssessmentPage) {
+        return Promise.resolve();
+      }
     });
 };
 
 const navigateToCheckAnswersPage = (selectedQuestionsWithAnswers) => {
-    return cy.navigateThroughQuestions(selectedQuestionsWithAnswers).then(() => cy.wrap(selectedQuestionsWithAnswers));
+  return cy
+    .navigateThroughQuestions(selectedQuestionsWithAnswers)
+    .then(() => cy.wrap(selectedQuestionsWithAnswers));
 };
 
 const navigateThroughQuestions = (selectedQuestionsWithAnswers) => {
-    return cy
-        .get("main")
-        .then(($main) => $main.find("form div.govuk-radios").length > 0)
-        .then((onQuestionPage) => {
-            if (!onQuestionPage) {
-                return Promise.resolve();
-            }
+  return cy
+    .get("main")
+    .then(($main) => $main.find("form div.govuk-radios").length > 0)
+    .then((onQuestionPage) => {
+      if (!onQuestionPage) {
+        return Promise.resolve();
+      }
 
-            cy.selectFirstRadioButton().then((questionWithAnswer) =>
-                selectedQuestionsWithAnswers.push(questionWithAnswer)
-            );
-            cy.saveAndContinue();
+      cy.selectFirstRadioButton().then((questionWithAnswer) =>
+        selectedQuestionsWithAnswers.push(questionWithAnswer)
+      );
+      cy.saveAndContinue();
 
-            return navigateThroughQuestions(selectedQuestionsWithAnswers);
-        })
-        .then(() => cy.wrap(selectedQuestionsWithAnswers));
+      return navigateThroughQuestions(selectedQuestionsWithAnswers);
+    })
+    .then(() => cy.wrap(selectedQuestionsWithAnswers));
 };
 
 const submitAnswers = () =>
-    cy.get("button.govuk-button").contains("Save and continue").click();
+  cy.get("button.govuk-button").contains("Save and continue").click();
 
-Cypress.Commands.add("navigateToRecommendationPage", navigateToRecommendationPage);
+Cypress.Commands.add(
+  "navigateToRecommendationPage",
+  navigateToRecommendationPage
+);
 Cypress.Commands.add("navigateToCheckAnswersPage", navigateToCheckAnswersPage);
 Cypress.Commands.add("navigateThroughQuestions", navigateThroughQuestions);
 Cypress.Commands.add("submitAnswers", submitAnswers);
+Cypress.Commands.add("navigateThroughSection", navigateThroughSection);
--- tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/self-assessment.cy.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/self-assessment.cy.js
@@ -5,7 +5,7 @@ describe("Self-assessment page", () => {
     cy.loginWithEnv(url);
 
     cy.injectAxe();
-});
+  });
 
   it("should have heading", () => {
     cy.get("h1.govuk-heading-xl")
@@ -25,13 +25,24 @@ describe("Self-assessment page", () => {
 
   it("each section should link to a page", () => {
     cy.get("ul.app-task-list__items > li a").each((link) => {
-      cy.wrap(link)
-        .should("have.attr", "href")
-        .and("not.be.null");
+      cy.wrap(link).should("have.attr", "href").and("not.be.null");
     });
   });
 
   it("passes accessibility tests", () => {
+    cy.injectAxe();
     cy.runAxe();
+  });
+
+  it("should show recommendation banner", () => {
+    cy.clickFirstSection();
+    cy.clickContinueButton();
+    cy.navigateThroughSection();
+
+    cy.get(".govuk-notification-banner__content").should("exist");
+
+    cy.get(".govuk-notification-banner__content a")
+      .should("have.attr", "href")
+      .and("include", "#recommendations-section");
   });
 });
