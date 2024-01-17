4dfae62df3e363d7fd24a0f322cde6c103076c6a
Jim Washbrook <jim.washbrook@and.digital>
2024-01-17 17:02:52 +0000

    chore: docs

--- src/Dfe.PlanTech.Application/Content/Queries/GetCategorySectionsQuery.cs
+++ src/Dfe.PlanTech.Application/Content/Queries/GetCategorySectionsQuery.cs
@@ -61,8 +61,7 @@ public class GetCategorySectionsQuery : IGetPageChildrenQuery
                 _logger.LogError("Could not find matching category {categoryId} in {pageSlug}", cat.Key, page.Slug);
                 continue;
             }
-
-            var sectionsValid = !sections.Any(cat => cat.InterstitialPage == null);
+            bool sectionsValid = AllSectionsValid(sections);
 
             if (!sectionsValid)
             {
@@ -72,6 +71,18 @@ public class GetCategorySectionsQuery : IGetPageChildrenQuery
             matching.Sections = cat.ToList();
         }
     }
+
+    /// <summary>
+    /// Are all sections in this collection valid?
+    /// </summary>
+    /// <remarks>
+    /// Currently just validates that they have an interstitial page, as this will cause errors
+    /// on the self-assessment page otherwise currently.
+    /// </remarks>
+    /// <param name="sections"></param>
+    /// <returns>All sections in the enumerable are valid (true) or ANY is invalid (false)</returns>
+    private static bool AllSectionsValid(IEnumerable<SectionDbEntity> sections)
+        => !sections.Any(cat => cat.InterstitialPage == null);
 
 
     /// <summary>
