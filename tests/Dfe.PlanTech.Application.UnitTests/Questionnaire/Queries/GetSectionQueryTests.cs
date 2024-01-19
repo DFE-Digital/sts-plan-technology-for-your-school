802e0377ccff468bc58a1920445a8efec048f71f
Jim Washbrook <jim.washbrook@and.digital>
2024-01-19 12:05:02 +0000

    tests: Test DB vs Contentful

--- tests/Dfe.PlanTech.Application.UnitTests/Questionnaire/Queries/GetSectionQueryTests.cs
+++ tests/Dfe.PlanTech.Application.UnitTests/Questionnaire/Queries/GetSectionQueryTests.cs
@@ -52,10 +52,49 @@ public class GetSectionQueryTests
         }
     };
 
+    private readonly List<SectionDbEntity> _dbSections = new(3);
+
     private readonly Section[] _sections = new[] { FirstSection, SecondSection, ThirdSection };
 
+    private readonly IMapper _mapper = Substitute.For<IMapper>();
+
+    public GetSectionQueryTests()
+    {
+        _dbSections.AddRange(_sections.Select((section) =>
+        {
+            var interstitialPageId = $"{section.InterstitialPage.Slug}-ID";
+
+            return new SectionDbEntity()
+            {
+                Name = section.Name,
+                InterstitialPage = new PageDbEntity()
+                {
+                    Slug = section.InterstitialPage.Slug,
+                    Id = interstitialPageId
+                },
+                InterstitialPageId = interstitialPageId,
+                Id = section.Sys.Id
+            };
+        }));
+
+        _mapper.Map<Section>(Arg.Any<SectionDbEntity>())
+                .Returns(callinfo =>
+                {
+                    var section = callinfo.ArgAt<SectionDbEntity>(0);
+
+                    return new Section()
+                    {
+                        Name = section.Name,
+                        Sys = new()
+                        {
+                            Id = section.Id
+                        }
+                    };
+                });
+    }
+
     [Fact]
-    public async Task GetSectionBySlug_Returns_Section_From_Repository()
+    public async Task GetSectionBySlug_Returns_Section_From_Db()
     {
         var sectionSlug = FirstSection.InterstitialPage.Slug;
         var cancellationToken = CancellationToken.None;
@@ -75,14 +114,64 @@ public class GetSectionQueryTests
         });
 
         var db = Substitute.For<ICmsDbContext>();
+        db.Sections.Returns(_dbSections.AsQueryable());
+        db.FirstOrDefaultAsync(Arg.Any<IQueryable<SectionDbEntity>>(), Arg.Any<CancellationToken>())
+                .Returns(callinfo =>
+                {
+                    var queryable = callinfo.ArgAt<IQueryable<SectionDbEntity>>(0);
 
-        var mapper = Substitute.For<IMapper>();
+                    return queryable.FirstOrDefault();
+                });
 
-        var getSectionQuery = new GetSectionQuery(db, repository, mapper);
+        var getSectionQuery = new GetSectionQuery(db, repository, _mapper);
         var section = await getSectionQuery.GetSectionBySlug(sectionSlug, cancellationToken);
 
         Assert.Equal(FirstSection.InterstitialPage.Slug, sectionSlug);
+
+        await db.Received(1).FirstOrDefaultAsync(Arg.Any<IQueryable<SectionDbEntity>>(), Arg.Any<CancellationToken>());
+        await repository.Received(0).GetEntities<Section>(Arg.Any<GetEntitiesOptions>(), Arg.Any<CancellationToken>());
     }
+
+    [Fact]
+    public async Task GetSectionBySlug_Returns_Section_From_Repository_When_Db_Returns_Nothing()
+    {
+        var sectionSlug = FirstSection.InterstitialPage.Slug;
+        var cancellationToken = CancellationToken.None;
+
+        var repository = Substitute.For<IContentRepository>();
+        repository.GetEntities<Section>(Arg.Any<GetEntitiesOptions>(), Arg.Any<CancellationToken>())
+        .Returns((callinfo) =>
+        {
+            var options = callinfo.ArgAt<GetEntitiesOptions>(0);
+
+            var slugQuery = (options.Queries?.FirstOrDefault(query => query is ContentQueryEquals equalsQuery &&
+                                                            equalsQuery.Field == GetSectionQuery.SlugFieldPath) as ContentQueryEquals) ??
+                                            throw new InvalidOperationException("Missing query for slug");
+
+
+            return _sections.Where(section => section.InterstitialPage.Slug == slugQuery.Value);
+        });
+
+        var db = Substitute.For<ICmsDbContext>();
+        var nullList = new List<SectionDbEntity>();
+        db.Sections.Returns(nullList.AsQueryable());
+        db.FirstOrDefaultAsync(Arg.Any<IQueryable<SectionDbEntity>>(), Arg.Any<CancellationToken>())
+                .Returns(callinfo =>
+                {
+                    var queryable = callinfo.ArgAt<IQueryable<SectionDbEntity>>(0);
+
+                    return queryable.FirstOrDefault();
+                });
+
+        var getSectionQuery = new GetSectionQuery(db, repository, _mapper);
+        var section = await getSectionQuery.GetSectionBySlug(sectionSlug, cancellationToken);
+
+        Assert.Equal(FirstSection.InterstitialPage.Slug, sectionSlug);
+
+        await db.Received(1).FirstOrDefaultAsync(Arg.Any<IQueryable<SectionDbEntity>>(), Arg.Any<CancellationToken>());
+        await repository.Received(1).GetEntities<Section>(Arg.Any<GetEntitiesOptions>(), Arg.Any<CancellationToken>());
+    }
+
 
 
     [Fact]
