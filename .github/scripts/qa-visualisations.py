fc8657b98432310a31709a63f17e1bc79c116417
uahussain12 <uahussain12@users.noreply.github.com>
2024-02-09 13:43:54 +0000

    Format visualisations (#506)
    * Add formatting to visualisations, removes cron schedule from workflow and add as job within matrix deploy
    
    * add -y flag for any prompts
    
    ---------
    
    Co-authored-by: uahussain12 <“umar.hussain@and.digital”>

--- .github/scripts/qa-visualisations.py
+++ .github/scripts/qa-visualisations.py
@@ -2,8 +2,18 @@ import os
 import json
 from graphviz import Digraph
 
+def split_text(text, max_length):
+    if len(text) <= max_length:
+        return [text]
+    else:
+        split_index = text.rfind(' ', 0, max_length)
+        if split_index == -1:
+            return [text[:max_length]] + split_text(text[max_length:], max_length)
+        else:
+            return [text[:split_index]] + split_text(text[split_index+1:], max_length)
+
 def create_questionnaire_flowchart(question_data):
-    tree = Digraph(comment='Questionnaire Flowchart', format='png')
+    tree = Digraph(format='png')
 
     end_nodes = set()
 
@@ -12,17 +22,23 @@ def create_questionnaire_flowchart(question_data):
         next_question_id = item["NextQuestionId"]
         answer_text = item["Text"]
 
-        tree.node(current_question, item["QuestionText"], shape='box')
+        question_lines = split_text(item["QuestionText"], 20)
+        question_text = "\n".join(question_lines)
+
+        tree.node(current_question, question_text, shape='box', style='filled', fillcolor='grey:white', width='2', gradient='200')
+
+        answer_lines = split_text(answer_text, 20)
+        answer_text = "\n".join(answer_lines)
 
         if next_question_id:
-            tree.node(next_question_id, "", shape='box')
-            tree.edge(current_question, next_question_id, label=answer_text)
+            tree.node(next_question_id, "Missing Content", shape='diamond', style='filled', fillcolor='red:white', width='2')
+            tree.edge(current_question, next_question_id, label=answer_text, color='black')
         else:
             end_nodes.add(current_question)
 
     for end_node in end_nodes:
-        tree.node("end", "End", shape='box')
-        tree.edge(end_node, "end", label="No More Questions")
+        tree.node("end", "Check Answers", shape='box', style='filled', fillcolor='lightblue:white', width='2', gradient='300')
+        tree.edge(end_node, "end", label="No More Questions", color='black')
 
     return tree
 
@@ -40,6 +56,11 @@ def process_data_files():
 
             flowchart = create_questionnaire_flowchart(json_data)
             output_file = os.path.join(png_folder, f'{os.path.splitext(filename)[0]}')
+            flowchart.graph_attr['rankdir'] = 'LR'
+            flowchart.graph_attr['beautify'] = 'true'
+            flowchart.edge_attr['arrowhead'] = 'vee'
+            flowchart.edge_attr['arrowsize'] = '0.5'
+
             flowchart.render(output_file, cleanup=True)
 
 if __name__ == '__main__':
