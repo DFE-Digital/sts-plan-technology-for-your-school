e82eebacc20ef920f4867af6ce027d46e64a1f27
Jim Washbrook <jim.washbrook@and.digital>
2024-02-12 09:46:48 +0000

    chore: linting + remove unnecessary query

--- src/Dfe.PlanTech.DatabaseUpgrader/Scripts/2024/20240209_1647_CreateRichTextContentWithPageSlugView.sql
+++ src/Dfe.PlanTech.DatabaseUpgrader/Scripts/2024/20240209_1647_CreateRichTextContentWithPageSlugView.sql
@@ -1,47 +1,78 @@
-ALTER VIEW Contentful.[RichTextContentsBySlug] AS
-(
-    SELECT [Slug], [Id], [Value], [NodeType], [DataId], [ParentId], [Published], [Archived], [Deleted]
-    FROM (     
-        SELECT * 
-        FROM (
-                SELECT RichTextId, Slug, Archived, Deleted, Published
-                FROM 
+CREATE 
+OR ALTER VIEW Contentful.[RichTextContentsBySlug] AS (
+  SELECT 
+    [Slug], 
+    [Id], 
+    [Value], 
+    [NodeType], 
+    [DataId], 
+    [ParentId], 
+    [Published], 
+    [Archived], 
+    [Deleted] 
+  FROM 
+    (
+      SELECT 
+        * 
+      FROM 
+        (
+          SELECT 
+            RichTextId, 
+            Slug, 
+            Archived, 
+            Deleted, 
+            Published 
+          FROM 
+            (
+              --- Get RichTextIds for all text bodies and warnings
+              SELECT 
+                RichTextId, 
+                Id AS ContentId 
+              FROM 
+                [Contentful].[TextBodies] AS TB 
+              UNION 
+              SELECT 
+                RichTextId, 
+                TB.Id 
+              FROM 
+                [Contentful].[Warnings] AS W 
+                LEFT JOIN [Contentful].[TextBodies] AS TB ON W.TextId = TB.Id
+            ) AS RichTexts 
+            LEFT JOIN --Get all contents for a page
+            (
+              SELECT 
+                MAX(Slug) AS Slug, 
+                ContentId 
+              FROM 
                 (
-                    --- Get RichTextIds for all text bodies and warnings
-                    SELECT * 
-                    FROM 
-                    (
-                        SELECT RichTextId, Id AS ContentId 
-                        FROM [Contentful].[TextBodies] AS TB
-                        UNION
-                        SELECT RichTextId, TB.Id 
-                        FROM [Contentful].[Warnings] AS W
-                        LEFT JOIN [Contentful].[TextBodies] AS TB ON W.TextId = TB.Id
-                    ) AS RT
-                ) AS RichTexts
-
-                LEFT JOIN 
-                
-                --Get all contents for a page
-                (
-                    SELECT MAX(Slug) AS Slug, ContentId
-                    FROM 
-                    (
-                        SELECT ISNULL([BeforeContentComponentId], [ContentComponentId]) AS ContentId, P.Slug 
-                        FROM [Contentful].[PageContents] PC
-                        LEFT JOIN [Contentful].[Pages] P ON PC.PageId = P.Id
-                    ) AS PC
-                    GROUP BY PC.ContentId
-                ) AS Contents ON Contents.ContentId = RichTexts.ContentId
-
-                LEFT JOIN(
-                    SELECT Id, Archived, Published, DELETED
-                    FROM Contentful.ContentComponents
-                ) AS CC 
-                ON CC.Id = RichTexts.ContentId
-            ) AS RichTextContents
-        ) AS RichTextContentParents
-    OUTER APPLY (
-        SELECT * FROM [Contentful].[SelectAllRichTextContentForParentId](RichTextContentParents.RichTextId) 
+                  SELECT 
+                    ISNULL(
+                      [BeforeContentComponentId], [ContentComponentId]
+                    ) AS ContentId, 
+                    P.Slug 
+                  FROM 
+                    [Contentful].[PageContents] PC 
+                    LEFT JOIN [Contentful].[Pages] P ON PC.PageId = P.Id
+                ) AS PC 
+              GROUP BY 
+                PC.ContentId
+            ) AS Contents ON Contents.ContentId = RichTexts.ContentId 
+            LEFT JOIN(
+              SELECT 
+                Id, 
+                Archived, 
+                Published, 
+                DELETED 
+              FROM 
+                Contentful.ContentComponents
+            ) AS CC ON CC.Id = RichTexts.ContentId
+        ) AS RichTextContents
+    ) AS RichTextContentParents OUTER APPLY (
+      SELECT 
+        * 
+      FROM 
+        [Contentful].[SelectAllRichTextContentForParentId](
+          RichTextContentParents.RichTextId
+        )
     ) AS RichTextContentsWithSlug
 )
