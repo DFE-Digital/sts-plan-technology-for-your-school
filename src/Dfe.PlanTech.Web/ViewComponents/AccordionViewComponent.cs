a752676b43f539b26f61bcbd7fe6e56a348a7adc
uahussain12 <umar.hussain@and.digital>
2024-02-20 17:03:36 +0000

    feat: basic step by step accordion component view

--- src/Dfe.PlanTech.Web/ViewComponents/AccordionViewComponent.cs
+++ src/Dfe.PlanTech.Web/ViewComponents/AccordionViewComponent.cs
@@ -11,10 +11,10 @@ public class AccordionViewComponent : ViewComponent
         //TODO: Replace with actual content.
         IEnumerable<IAccordion> accordionBlock = new List<IAccordion>
         {
-            new Accordion() { Order = 1, Header = "Test Header 1", Link = "Link 1" },
-            new Accordion() { Order = 2, Header = "Test Header 2", Link = "Link 2" },
-            new Accordion() { Order = 3, Header = "Test Header 3", Link = "Link 3" },
-            new Accordion() { Order = 4, Header = "Test Header 4", Link = "Link 4" }
+            new Accordion() { Order = 1, Header = "Test Header 1", Link = "broadband-connection" },
+            new Accordion() { Order = 2, Header = "Test Header 2", Link = "broadband-connection" },
+            new Accordion() { Order = 3, Header = "Test Header 3", Link = "broadband-connection" },
+            new Accordion() { Order = 4, Header = "Test Header 4", Link = "broadband-connection" }
         };
         return View(accordionBlock);
     }
