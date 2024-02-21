a294c2c7e82000df9bc9a384465a25d4f373b5b9
uahussain12 <umar.hussain@and.digital>
2024-02-21 17:19:52 +0000

    wip: building recommendation view

---
+++ src/Dfe.PlanTech.Web/Models/RecommendationsViewModel.cs
@@ -0,0 +1,12 @@
+using Dfe.PlanTech.Domain.Questionnaire.Models;
+
+namespace Dfe.PlanTech.Web.Models;
+
+public class RecommendationsViewModel
+{
+    public String SectionName;
+    
+    public RecommendationIntro RecommendationIntro;
+    
+    public List<RecommendationChunk> RecommendationChunks;
+}
