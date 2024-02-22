cd873deeddf58e710cb6b55c050aba1b2eb7de00
Zac King <zac.king@and.digital>
2024-02-22 11:51:28 +0000

    feat: read recommendation chunks from cms based on answers

--- src/Dfe.PlanTech.Application/Content/Queries/GetRecommendationChunksForSection.cs
+++ src/Dfe.PlanTech.Application/Content/Queries/GetRecommendationChunksForSection.cs
@@ -8,6 +8,22 @@ public class GetRecommendationChunksForSection(IContentRepository repository) //
 {
     private readonly IContentRepository _repository = repository;
 
+    public async Task<IEnumerable<RecommendationChunk>> GetRecommendationChunksFromAnswers(Section subTopic, IEnumerable<Answer> answers, CancellationToken cancellationToken)
+    {
+        SubTopicRecommendation subTopicRecommendation = await GetSubTopicRecommendationFromContentful(subTopic, cancellationToken);
+
+        RecommendationSection recommendationSection = subTopicRecommendation.Section;
+
+        IEnumerable<RecommendationChunk> recommendationChunks = recommendationSection.Chunks;
+
+        if (recommendationSection.Answers.IntersectBy(answers.Select(answer => answer.Sys.Id), recAnswer => recAnswer.Sys.Id).Any())
+        {
+            return recommendationChunks;
+        }
+
+        return recommendationChunks.Where(chunk => chunk.Answers.IntersectBy(answers.Select(answer => answer.Sys.Id), chunkAns => chunkAns.Sys.Id).Any());
+    }
+
     public async Task<RecommendationIntro> GetRecommendationIntroForSubtopic(Section subTopic, Maturity maturity, CancellationToken cancellationToken)
         => (await GetSubTopicRecommendationFromContentful(subTopic, cancellationToken)).Intros.Where(intro => intro.Maturity.Equals(maturity)).First();
 
@@ -15,8 +31,6 @@ public class GetRecommendationChunksForSection(IContentRepository repository) //
     {
         IEnumerable<SubTopicRecommendation> subTopicRecommendations = await _repository.GetEntities<SubTopicRecommendation>(cancellationToken);
 
-        SubTopicRecommendation subTopicRecommendation = subTopicRecommendations.Where(subTopicRecommendation => subTopicRecommendation.Subtopic.Sys.Id.Equals(subTopic.Sys.Id)).First();
-
-        return subTopicRecommendation;
+        return subTopicRecommendations.Where(subTopicRecommendation => subTopicRecommendation.Subtopic.Sys.Id.Equals(subTopic.Sys.Id)).First();
     }
 }
