4e7c6aea4c455037617d83f266848f359eb5c516
Jim Washbrook <jim.washbrook@and.digital>
2024-02-22 13:09:36 +0000

    fix(recommendations): :adhesive_bandage: Remove concrete references from recommendations interfaces

--- src/Dfe.PlanTech.Domain/Questionnaire/Models/RecommendationSection.cs
+++ src/Dfe.PlanTech.Domain/Questionnaire/Models/RecommendationSection.cs
@@ -3,7 +3,7 @@ using Dfe.PlanTech.Domain.Questionnaire.Interfaces;
 
 namespace Dfe.PlanTech.Domain.Questionnaire.Models;
 
-public class RecommendationSection : ContentComponent, IRecommendationSection<Answer, RecommendationChunk>
+public class RecommendationSection : ContentComponent, IRecommendationSection<Answer, ContentComponent, Header, RecommendationChunk>
 {
     public List<Answer> Answers { get; init; } = [];
 
--- src/Dfe.PlanTech.Domain/Questionnaire/Models/SubTopicRecommendation.cs
+++ src/Dfe.PlanTech.Domain/Questionnaire/Models/SubTopicRecommendation.cs
@@ -3,7 +3,7 @@ using Dfe.PlanTech.Domain.Questionnaire.Interfaces;
 
 namespace Dfe.PlanTech.Domain.Questionnaire.Models;
 
-public class SubTopicRecommendation : ContentComponent, ISubTopicRecommendation<RecommendationIntro, RecommendationSection, Section>
+public class SubTopicRecommendation : ContentComponent, ISubTopicRecommendation<Answer, ContentComponent, Header, RecommendationChunk, RecommendationIntro, RecommendationSection, Section>
 {
     public List<RecommendationIntro> Intros { get; init; } = [];
 
