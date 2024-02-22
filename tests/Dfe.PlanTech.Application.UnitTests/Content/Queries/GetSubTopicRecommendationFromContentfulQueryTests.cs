17fd6ca6bc58e5653567c60edb6e84c900818c13
Zac King <zac.king@and.digital>
2024-02-22 15:15:20 +0000

    test: add unit tests for subtopic recommendation query

---
+++ tests/Dfe.PlanTech.Application.UnitTests/Content/Queries/GetSubTopicRecommendationFromContentfulQueryTests.cs
@@ -0,0 +1,67 @@
+using Dfe.PlanTech.Application.Content.Queries;
+using Dfe.PlanTech.Application.Persistence.Interfaces;
+using Dfe.PlanTech.Domain.Content.Models;
+using Dfe.PlanTech.Domain.Questionnaire.Models;
+using NSubstitute;
+
+public class GetSubTopicRecommendationFromContentfulQueryTests
+{
+    private readonly IContentRepository _repoSubstitute = Substitute.For<IContentRepository>();
+    private readonly GetSubTopicRecommendationFromContentfulQuery _getSubTopicRecommendationFromContentfulQuery;
+    private readonly Section _subTopicOne;
+    private readonly Section _subTopicTwo;
+
+    private readonly SubTopicRecommendation _subTopicRecommendationOne;
+    private readonly SubTopicRecommendation _subTopicRecommendationTwo;
+
+    public GetSubTopicRecommendationFromContentfulQueryTests()
+    {
+        _subTopicOne = new()
+        {
+            Sys = new SystemDetails()
+            {
+                Id = "SubTopicId"
+            }
+        };
+
+        _subTopicTwo = new()
+        {
+            Sys = new SystemDetails()
+            {
+                Id = "IdForAnotherSubTopic"
+            }
+        };
+
+        _subTopicRecommendationOne = new()
+        {
+            Subtopic = _subTopicOne
+        };
+
+        _subTopicRecommendationTwo = new()
+        {
+            Subtopic = _subTopicTwo
+        };
+
+        _repoSubstitute.GetEntities<SubTopicRecommendation>().Returns(new List<SubTopicRecommendation>() { _subTopicRecommendationOne, _subTopicRecommendationTwo });
+
+        _getSubTopicRecommendationFromContentfulQuery = new(_repoSubstitute);
+    }
+
+    [Fact]
+    public async Task GetSubTopicRecommendation_Returns_Correct_SubTopicRecommendation_From_SectionOne()
+    {
+        SubTopicRecommendation? subTopicRecommendation = await _getSubTopicRecommendationFromContentfulQuery.GetSubTopicRecommendation(_subTopicOne);
+
+        Assert.NotNull(subTopicRecommendation);
+        Assert.Equal(_subTopicRecommendationOne, subTopicRecommendation);
+    }
+
+    [Fact]
+    public async Task GetSubTopicRecommendation_Returns_Correct_SubTopicRecommendation_From_SectionTwo()
+    {
+        SubTopicRecommendation? subTopicRecommendation = await _getSubTopicRecommendationFromContentfulQuery.GetSubTopicRecommendation(_subTopicTwo);
+
+        Assert.NotNull(subTopicRecommendation);
+        Assert.Equal(_subTopicRecommendationTwo, subTopicRecommendation);
+    }
+}
