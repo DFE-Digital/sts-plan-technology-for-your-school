6bfc2eec65d6a4842dbae0d5b66f19b73892c18e
Jim Washbrook <jim.washbrook@and.digital>
2024-02-22 10:43:53 +0000

    fix: remove GO commands

--- .github/scripts/clear-user-data-from-db.sql
+++ .github/scripts/clear-user-data-from-db.sql
@@ -1,26 +1,12 @@
 BEGIN TRANSACTION;
-GO;
 
   DELETE FROM [dbo].[response];
-  GO;
-
   DELETE FROM [dbo].[submission];
-  GO;
-
   DELETE FROM [dbo].[question];
-  GO;
-
   DELETE FROM [dbo].[answer];
-  GO;
-
   DELETE FROM [dbo].[signin];
-  GO;
-
   DELETE FROM [dbo].[user];
-  GO;
-
   DELETE FROM [dbo].[establishment];
-  GO;
 
 COMMIT;
 GO;
