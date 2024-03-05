0b72f14ba263b3505b3ba5d8174dca35e815c0e4
Jim Washbrook <jim.washbrook@and.digital>
2024-03-05 17:07:02 +0000

    feat(errors): Log error from middleware

--- src/Dfe.PlanTech.Web/Middleware/ServiceExceptionHandlerMiddleWare.cs
+++ src/Dfe.PlanTech.Web/Middleware/ServiceExceptionHandlerMiddleWare.cs
@@ -6,17 +6,20 @@ using Dfe.PlanTech.Domain.Users.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
 namespace Dfe.PlanTech.Web.Middleware;
 
-public class ServiceExceptionHandlerMiddleWare : IExceptionHandlerMiddleware
+public class ServiceExceptionHandlerMiddleWare(ILogger<ServiceExceptionHandlerMiddleWare> logger) : IExceptionHandlerMiddleware
 {
+    private readonly ILogger<ServiceExceptionHandlerMiddleWare> _logger = logger;
+
     public void ContextRedirect(HttpContext context)
     {
         var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
         var exception = exceptionHandlerPathFeature?.Error;
 
+        _logger.LogError(exception, "Exception caught by middleware. Exception is typeof {exceptionType}", exception?.GetType() ?? null);
+
         string redirectUrl = GetRedirectUrlForException(exception);
 
         context.Response.Redirect(redirectUrl);
-
     }
 
     static string GetRedirectUrlForException(Exception? exception) =>
