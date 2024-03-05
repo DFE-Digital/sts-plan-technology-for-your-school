8627788d9a283833bf8dfa4406296ad542680e97
Jim Washbrook <jim.washbrook@and.digital>
2024-03-05 18:47:41 +0000

    chore: revert log

--- src/Dfe.PlanTech.Web/Middleware/ServiceExceptionHandlerMiddleWare.cs
+++ src/Dfe.PlanTech.Web/Middleware/ServiceExceptionHandlerMiddleWare.cs
@@ -6,20 +6,17 @@ using Dfe.PlanTech.Domain.Users.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
 namespace Dfe.PlanTech.Web.Middleware;
 
-public class ServiceExceptionHandlerMiddleWare(ILogger<ServiceExceptionHandlerMiddleWare> logger) : IExceptionHandlerMiddleware
+public class ServiceExceptionHandlerMiddleWare : IExceptionHandlerMiddleware
 {
-    private readonly ILogger<ServiceExceptionHandlerMiddleWare> _logger = logger;
-
     public void ContextRedirect(HttpContext context)
     {
         var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
         var exception = exceptionHandlerPathFeature?.Error;
 
-        _logger.LogError(exception, "Exception caught by middleware. Exception is typeof {exceptionType}", exception?.GetType() ?? null);
-
         string redirectUrl = GetRedirectUrlForException(exception);
 
         context.Response.Redirect(redirectUrl);
+
     }
 
     static string GetRedirectUrlForException(Exception? exception) =>
@@ -31,7 +28,8 @@ public class ServiceExceptionHandlerMiddleWare(ILogger<ServiceExceptionHandlerMi
             ContentfulDataUnavailableException => UrlConstants.ServiceUnavailable,
             DatabaseException => UrlConstants.ServiceUnavailable,
             InvalidEstablishmentException => UrlConstants.ServiceUnavailable,
-            KeyNotFoundException ex when ex.Message.Contains(ClaimConstants.Organisation) => UrlConstants.CombinedErrorPage,
+            PageNotFoundException => UrlConstants.ServiceUnavailable,
+            KeyNotFoundException ex when ex.Message.Contains(ClaimConstants.Organisation) => UrlConstants.ServiceUnavailable,
             _ => GetRedirectUrlForException(exception.InnerException),
         };
 }
