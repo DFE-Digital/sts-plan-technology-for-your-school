fcb4a120bb475c9ead55f2cd33b0aa1f1dfeebdf
Jim Washbrook <jim.washbrook@and.digital>
2024-03-07 11:47:44 +0000

    feat: Lowercase slug

--- src/Dfe.PlanTech.Web/Helpers/ViewHelpers.cs
+++ src/Dfe.PlanTech.Web/Helpers/ViewHelpers.cs
@@ -7,7 +7,7 @@ public static partial class ViewHelpers
   private const string RemoveNonAlphanumericCharactersRegexPattern = @"[^a-zA-Z0-9\s]";
 
   public static string Slugify(this string text)
-  => RemoveNonAlphaNumericCharactersPattern().Replace(text, "").Replace(" ", "-");
+  => RemoveNonAlphaNumericCharactersPattern().Replace(text, "").Replace(" ", "-").ToLower();
 
   [GeneratedRegex(RemoveNonAlphanumericCharactersRegexPattern)]
   private static partial Regex RemoveNonAlphaNumericCharactersPattern();
