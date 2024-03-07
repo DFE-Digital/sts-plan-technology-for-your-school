d57aea597e6171c9e332e6268cf5952542e28921
Jim Washbrook <jim.washbrook@and.digital>
2024-03-07 11:43:30 +0000

    feat: Refactored views slightly

--- src/Dfe.PlanTech.Domain/Questionnaire/Models/RecommendationChunk.cs
+++ src/Dfe.PlanTech.Domain/Questionnaire/Models/RecommendationChunk.cs
@@ -3,7 +3,7 @@ using Dfe.PlanTech.Domain.Questionnaire.Interfaces;
 
 namespace Dfe.PlanTech.Domain.Questionnaire.Models;
 
-public class RecommendationChunk : ContentComponent, IRecommendationChunk<Header, ContentComponent, Answer>
+public class RecommendationChunk : ContentComponent, IRecommendationChunk<Header, ContentComponent, Answer>, IHeaderWithContent
 {
     public string Title { get; init; } = null!;
 
