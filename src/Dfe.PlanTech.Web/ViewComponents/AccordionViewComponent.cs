cfbde512686675ada8b19b6a07f07f90b0d5a9d9
Jim Washbrook <jim.washbrook@and.digital>
2024-03-07 14:25:34 +0000

    wip: add rec intro to accordion

--- src/Dfe.PlanTech.Web/ViewComponents/AccordionViewComponent.cs
+++ src/Dfe.PlanTech.Web/ViewComponents/AccordionViewComponent.cs
@@ -1,24 +1,13 @@
-using Dfe.PlanTech.Domain.Content.Interfaces;
 using Dfe.PlanTech.Domain.Content.Models;
-using Dfe.PlanTech.Domain.Questionnaire.Models;
+using Dfe.PlanTech.Domain.Questionnaire.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dfe.PlanTech.Web.ViewComponents;
 
 public class AccordionViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke(IEnumerable<RecommendationChunk> recommendationChunks)
+    public IViewComponentResult Invoke(IEnumerable<Accordion> accordions)
     {
-
-        IEnumerable<Accordion> accordions = recommendationChunks
-            .Select((chunk, index) => new Accordion() 
-            { 
-                Order = index + 1, 
-                Title = chunk.Title, 
-                Header = chunk.Header.Text
-            })
-            .ToList();
-        
-        return View(accordions);
+        return View(accordions.ToList());
     }
 }
