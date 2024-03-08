857545e057b651b1dd9537171d24e24175349638
gilaineyo <gilaineyoung@gmail.com>
2024-03-08 12:04:11 +0000

    Adds WarningComponent extended tests for null and empty inputs

--- tests/Dfe.PlanTech.Web.UnitTests/ViewComponents/WarningComponentTests.cs
+++ tests/Dfe.PlanTech.Web.UnitTests/ViewComponents/WarningComponentTests.cs
@@ -1,6 +1,7 @@
 ﻿using Dfe.PlanTech.Domain.Content.Models;
 using Dfe.PlanTech.Domain.Questionnaire.Models;
 using Dfe.PlanTech.Web.Models;
+using Dfe.PlanTech.Web.UnitTests.Models;
 using Dfe.PlanTech.Web.ViewComponents;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
 using System;
@@ -14,24 +15,30 @@ namespace Dfe.PlanTech.Web.UnitTests.ViewComponents
 {
     public class WarningComponentTests
     {
+        public string testText = "test text";
+        public string? nullText = null;
+        public string emptyText = "";
+        public ComponentBuilder componentBuilder = new ComponentBuilder();
+
         [Fact]
         public void WarningComponent_Sets_Text()
         {
-            //Arrange/Act
-            var warningComponent = new WarningComponent
-            {
-                Text = new TextBody
-                {
-                    RichText = new RichTextContent
-                    {
-                        Value = "test"
-                    }
-                }
-            };
+            var testWarning = componentBuilder.BuildWarningComponent(testText);
+            Assert.Equal("test text", testWarning.Text.RichText.Value);
+        }
 
-            //Assert
-            Assert.Equal("test", warningComponent.Text.RichText.Value);
+        [Fact]
+        public void WarningComponent_Sets_Empty_String()
+        {
+            var testWarning = componentBuilder.BuildWarningComponent(emptyText);
+            Assert.Equal("", testWarning.Text.RichText.Value);
+        }
 
+        [Fact]
+        public void WarningComponent_Sets_Null()
+        {
+            var testWarning = componentBuilder.BuildWarningComponent(nullText);
+            Assert.Null(testWarning.Text.RichText.Value);
         }
     }
 }
