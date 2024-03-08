e31d93af8063c2ede84d8d74574d134d30f96bfc
Jim Washbrook <jim.washbrook@and.digital>
2024-03-08 10:50:48 +0000

    fix: view to match changes

--- src/Dfe.PlanTech.Domain/Content/Interfaces/IAccordion.cs
+++ src/Dfe.PlanTech.Domain/Content/Interfaces/IAccordion.cs
@@ -3,6 +3,7 @@ namespace Dfe.PlanTech.Domain.Content.Interfaces;
 public interface IAccordion
 {
     public int Order { get; }
-    public string Title { get; }
     public string Header { get; }
+    public string Slug { get; }
+    public string Title { get; }
 }
--- src/Dfe.PlanTech.Domain/Content/Models/Accordion.cs
+++ src/Dfe.PlanTech.Domain/Content/Models/Accordion.cs
@@ -4,11 +4,11 @@ namespace Dfe.PlanTech.Domain.Content.Models;
 
 public class Accordion : IAccordion
 {
+
+
+
     public int Order { get; init; }
-
-    public string Title { get; init; } = null!;
-
     public string Header { get; init; } = null!;
-
     public string Slug { get; init; } = null!;
+    public string Title { get; init; } = null!;
 }
