521c3c45115247260ef4f581f6c9d0bb2fa8edeb
Jim Washbrook <jim.washbrook@and.digital>
2024-03-08 10:35:53 +0000

    chore: Add more fields to view models

--- src/Dfe.PlanTech.Domain/Questionnaire/Interfaces/IHeaderWithContent.cs
+++ src/Dfe.PlanTech.Domain/Questionnaire/Interfaces/IHeaderWithContent.cs
@@ -4,6 +4,11 @@ namespace Dfe.PlanTech.Domain.Questionnaire.Interfaces;
 
 public interface IHeaderWithContent
 {
+  public string Title { get; }
+
+  public string SlugifiedTitle { get; }
+
   public Header Header { get; }
+
   public List<ContentComponent> Content { get; }
 }
--- src/Dfe.PlanTech.Web/Models/RecommendationsViewModel.cs
+++ src/Dfe.PlanTech.Web/Models/RecommendationsViewModel.cs
@@ -1,12 +1,25 @@
+using Dfe.PlanTech.Domain.Content.Models;
+using Dfe.PlanTech.Domain.Questionnaire.Interfaces;
 using Dfe.PlanTech.Domain.Questionnaire.Models;
 
 namespace Dfe.PlanTech.Web.Models;
 
 public class RecommendationsViewModel
 {
-    public String SectionName { get; init; } = null!;
+    public string SectionName { get; init; } = null!;
 
     public RecommendationIntro RecommendationIntro { get; init; } = null!;
 
     public List<RecommendationChunk> RecommendationChunks { get; init; } = null!;
+
+    public IEnumerable<IHeaderWithContent> AllContent => GetAllContent();
+
+    private IEnumerable<IHeaderWithContent> GetAllContent()
+    {
+        yield return RecommendationIntro;
+        foreach (var chunk in RecommendationChunks)
+        {
+            yield return chunk;
+        }
+    }
 }
