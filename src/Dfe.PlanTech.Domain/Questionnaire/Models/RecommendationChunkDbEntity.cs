69fc43e0cb27f9d1654f8bd8fe8e361cc45e0232
Jim Washbrook <jim.washbrook@and.digital>
2024-03-25 15:35:40 +0000

    fix(functions): :bug: Add ignore to remaining child relationships
    Previous fix missed a few child entities; this should (?) be the last of them

--- src/Dfe.PlanTech.Domain/Questionnaire/Models/RecommendationChunkDbEntity.cs
+++ src/Dfe.PlanTech.Domain/Questionnaire/Models/RecommendationChunkDbEntity.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Dfe.PlanTech.Domain.Content.Models;
 using Dfe.PlanTech.Domain.Questionnaire.Interfaces;
 
@@ -11,9 +12,13 @@ public class RecommendationChunkDbEntity : ContentComponentDbEntity, IRecommenda
 
     public HeaderDbEntity Header { get; init; } = null!;
 
+    [NotMapped]
     public List<ContentComponentDbEntity> Content { get; init; } = [];
+
+    [NotMapped]
     public List<RecommendationChunkContentDbEntity> ChunkContentJoins { get; init; } = [];
 
+    [NotMapped]
     public List<AnswerDbEntity> Answers { get; init; } = [];
 
     public List<RecommendationSectionDbEntity> RecommendationSections { get; set; } = [];
