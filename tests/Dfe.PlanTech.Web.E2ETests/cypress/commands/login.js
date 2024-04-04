e307c27f80e61b9737e79bbe4fc54f56a0071bb4
gilaineyo <gilaineyoung@gmail.com>
2024-04-04 09:59:26 +0100

    Re-added a shorter wait for github checks

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/commands/login.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/commands/login.js
@@ -11,7 +11,8 @@ Cypress.Commands.add("login", ({ email, password, url }) => {
   cy.visit(url);
   cy.get("input#username").type(email);
   cy.get("input#password").type(password);
-  cy.get("div.govuk-button-group button.govuk-button").first().click();
+    cy.get("div.govuk-button-group button.govuk-button").first().click();
+    cy.wait(1000);
 }); 
 
 /**
