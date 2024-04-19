519150691f4f620f5a864dda61bc21cc1a146573
Jim Washbrook <jim.washbrook@and.digital>
2024-04-19 16:29:13 +0100

    tests: Update RecommendationSectionMapper test

--- tests/Dfe.PlanTech.AzureFunctions.UnitTests/Mappers/RecommendationSectionMapperTests.cs
+++ tests/Dfe.PlanTech.AzureFunctions.UnitTests/Mappers/RecommendationSectionMapperTests.cs
@@ -5,18 +5,49 @@ using Dfe.PlanTech.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
+using NSubstitute.Extensions;
 
 namespace Dfe.PlanTech.AzureFunctions.UnitTests.Mappers;
 
 public class RecommendationSectionMapperTests : BaseMapperTests
 {
 
+    private readonly DbSet<RecommendationChunkDbEntity> _recommendationChunksDbSet;
     private readonly DbSet<RecommendationSectionAnswerDbEntity> _sectionAnswerDbSet = Substitute.For<DbSet<RecommendationSectionAnswerDbEntity>>();
     private readonly DbSet<RecommendationSectionChunkDbEntity> _sectionChunkDbSet = Substitute.For<DbSet<RecommendationSectionChunkDbEntity>>();
     private static readonly CmsDbContext _db = Substitute.For<CmsDbContext>();
     private static readonly ILogger<RecommendationSectionUpdater> _logger = Substitute.For<ILogger<RecommendationSectionUpdater>>();
     private static RecommendationSectionUpdater CreateMockRecommendationSectionUpdater() => new(_logger, _db);
 
+    private readonly List<RecommendationChunkDbEntity> _chunks = [];
+
+    private readonly string[] _chunkIds = ["chunk1", "chunk2", "chunk3"];
+
+    public RecommendationSectionMapperTests()
+    {
+        foreach (var id in _chunkIds)
+        {
+            var chunk = new RecommendationChunkDbEntity()
+            {
+                Id = id,
+                Order = 99999
+            };
+
+            _chunks.Add(chunk);
+        }
+
+        IQueryable<RecommendationChunkDbEntity> queryable = _chunks.AsQueryable();
+
+        var asyncProvider = new AsyncQueryProvider<RecommendationChunkDbEntity>(queryable.Provider);
+
+        var mockPageDataSet = Substitute.For<DbSet<RecommendationChunkDbEntity>, IQueryable<RecommendationChunkDbEntity>>();
+        ((IQueryable<RecommendationChunkDbEntity>)mockPageDataSet).Provider.Returns(asyncProvider);
+        ((IQueryable<RecommendationChunkDbEntity>)mockPageDataSet).Expression.Returns(queryable.Expression);
+        ((IQueryable<RecommendationChunkDbEntity>)mockPageDataSet).ElementType.Returns(queryable.ElementType);
+        ((IQueryable<RecommendationChunkDbEntity>)mockPageDataSet).GetEnumerator().Returns(queryable.GetEnumerator());
+
+        _db.RecommendationChunks = mockPageDataSet;
+    }
 
     [Fact]
     public void RecommendationSectionsAreMappedCorrectly()
@@ -24,7 +55,7 @@ public class RecommendationSectionMapperTests : BaseMapperTests
         var values = new Dictionary<string, object?>
         {
             ["id"] = "sectionId",
-            ["chunks"] = new string[] { "chunk1", "chunk2", "chunk3" },
+            ["chunks"] = _chunkIds,
             ["answers"] = new string[] { "answer1", "answer2", "answer3" }
         };
 
@@ -34,8 +65,7 @@ public class RecommendationSectionMapperTests : BaseMapperTests
         _db.RecommendationSectionChunks = _sectionChunkDbSet;
         _db.RecommendationSectionAnswers = _sectionAnswerDbSet;
 
-        var mapper = new RecommendationSectionMapper(MapperHelpers.CreateMockEntityRetriever(),
-            CreateMockRecommendationSectionUpdater(), _db, loggerSubstitute, jsonSerializerOptions);
+        var mapper = new RecommendationSectionMapper(MapperHelpers.CreateMockEntityRetriever(), CreateMockRecommendationSectionUpdater(), _db, loggerSubstitute, jsonSerializerOptions);
 
         var recommendationChunk = mapper.PerformAdditionalMapping(values);
 
@@ -43,5 +73,12 @@ public class RecommendationSectionMapperTests : BaseMapperTests
 
         _db.RecommendationSectionAnswers.Received(3).Attach(Arg.Any<RecommendationSectionAnswerDbEntity>());
         _db.RecommendationSectionChunks.Received(3).Attach(Arg.Any<RecommendationSectionChunkDbEntity>());
+
+        for (var x = 0; x < _chunkIds.Length; x++)
+        {
+            var matchingChunk = _chunks.FirstOrDefault(c => c.Id == _chunkIds[x]);
+            Assert.NotNull(matchingChunk);
+            Assert.Equal(x, matchingChunk?.Order);
+        }
     }
 }
