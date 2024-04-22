2d47ce735a156c0389d628d963e716f3559a0e0f
Jim Washbrook <jim.washbrook@and.digital>
2024-04-22 12:22:42 +0100

    feat: chunk + intro comparators

---
+++ tests/Dfe.PlanTech.CmsDbDataValidator/Comparators/RecommendationChunkComparator.cs
@@ -0,0 +1,56 @@
+using System.Text.Json.Nodes;
+using Dfe.PlanTech.Domain.Content.Models;
+using Dfe.PlanTech.Domain.Questionnaire.Models;
+using Dfe.PlanTech.Infrastructure.Data;
+
+namespace Dfe.PlanTech.CmsDbDataValidator.Tests;
+
+public class RecommendationChunksComparatorclass(CmsDbContext db, ContentfulContent contentfulContent) : BaseComparator(db, contentfulContent, ["Title"], "RecommendationChunk")
+{
+  public override Task ValidateContent()
+  {
+    ValidateRecommendationChunks();
+    return Task.CompletedTask;
+  }
+
+  public void ValidateRecommendationChunks()
+  {
+    foreach (var contentfulRecommendationChunk in _contentfulEntities)
+    {
+      ValidateRecommendationChunk(_dbEntities.OfType<RecommendationChunkDbEntity>().ToArray(), contentfulRecommendationChunk);
+    }
+  }
+
+  private void ValidateRecommendationChunk(RecommendationChunkDbEntity[] databaseRecommendationChunks, JsonNode contentfulRecommendationChunk)
+  {
+    var databaseRecommendationChunk = ValidateChildEntityExistsInDb(databaseRecommendationChunks, contentfulRecommendationChunk);
+    if (databaseRecommendationChunk == null)
+    {
+      return;
+    }
+
+    var headerValidationResult = ValidateChild<RecommendationChunkDbEntity>(databaseRecommendationChunk, "HeaderId", contentfulRecommendationChunk, "header");
+    ValidateProperties(contentfulRecommendationChunk, databaseRecommendationChunk, headerValidationResult);
+
+    ValidateChildren(contentfulRecommendationChunk, "answers", databaseRecommendationChunk, dbRecommendationChunk => dbRecommendationChunk.Answers);
+    ValidateChildren(contentfulRecommendationChunk, "content", databaseRecommendationChunk, dbRecommendationChunk => dbRecommendationChunk.Content);
+  }
+
+  protected override IQueryable<ContentComponentDbEntity> GetDbEntitiesQuery()
+  {
+    return _db.RecommendationChunks.Select(chunk => new RecommendationChunkDbEntity()
+    {
+      Id = chunk.Id,
+      Title = chunk.Title,
+      HeaderId = chunk.HeaderId,
+      Answers = chunk.Answers.Select(answer => new AnswerDbEntity()
+      {
+        Id = answer.Id
+      }).ToList(),
+      Content = chunk.Content.Select(content => new ContentComponentDbEntity()
+      {
+        Id = content.Id
+      }).ToList(),
+    });
+  }
+}
