55458b3c51561e6b9125dbe75943e676088470fa
Jim Washbrook <jim.washbrook@and.digital>
2024-04-30 15:01:18 +0100

    chore: remove unused methods

--- tests/Dfe.PlanTech.CmsDbDataValidator/ConfigurationSetup.cs
+++ tests/Dfe.PlanTech.CmsDbDataValidator/ConfigurationSetup.cs
@@ -21,26 +21,6 @@ public static class ConfigurationSetup
                       .Build();
     }
 
-    private static ConfigurationBuilder AddKeyVault(this ConfigurationBuilder builder)
-    {
-        string? keyVaultEndpoint = GetKeyVaultEndPoint(builder);
-
-        var azureServiceTokenProvider = new AzureServiceTokenProvider();
-        var keyVaultClient = new KeyVaultClient(
-            new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
-
-        builder.AddAzureKeyVault(keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
-
-        return builder;
-    }
-
-    private static string? GetKeyVaultEndPoint(ConfigurationBuilder builder)
-    {
-        var configuration = builder.Build();
-        var keyVaultEndpoint = configuration.GetConnectionString("KeyVaultEndpoint");
-        return keyVaultEndpoint ?? throw new ArgumentException("Key Vault Endpoint not found in configuration");
-    }
-
     public static ConfigurationBuilder AddAppSettings(this ConfigurationBuilder builder)
     {
         builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
