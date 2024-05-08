937c36d486cd7461d5f346a118558830961b1e05
Jim Washbrook <jim.washbrook@and.digital>
2024-05-08 14:54:39 +0100

    Stop datamapper creation if no contentful export

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/helpers/contentful-data-loader.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/helpers/contentful-data-loader.js
@@ -2,6 +2,12 @@ import { contentful } from "./contentful";
 import { DataMapper } from "export-processor";
 
 const getContentfulData = () => {
+  if (!contentful ||
+    !contentful.contentTypes || contentful.contentTypes.length == 0 ||
+    !contentful.entries || contentful.entries.length == 0) {
+    return;
+  }
+
   return new DataMapper(contentful);
 };
 
