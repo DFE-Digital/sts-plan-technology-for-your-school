2a20021da75c907a730864956e410b1309131ddb
Jim Washbrook <jim.washbrook@and.digital>
2024-05-14 14:57:22 +0100

    feat: Javascript data migration

---
+++ contentful/export-processor/migrations/get-contents-with-references.js
@@ -0,0 +1,97 @@
+import DataMapper from "#src/data-mapper";
+
+/**
+ * Creates an object with all content entries with their outgoing references
+ * @param {object} params
+ * @param {DataMapper} params.dataMapper 
+ * @param {object[]} params.entries - original exported contentful entries 
+ */
+export const getContentsWithReferences = ({ dataMapper, entries }) => {
+  const contents = getOutgoingReferences(dataMapper);
+
+  getIncomingReferences(contents);
+
+  return addOriginalContentEntries({ contents, entries })
+}
+
+function getOutgoingReferences(dataMapper) {
+  const contents = {};
+
+  for (const [contentTypeName, contentType] of dataMapper.contentTypes.entries()) {
+    const referenceFields = Object.entries(contentType.referenceFields);
+
+    const contentsForType = dataMapper.contents[contentTypeName];
+
+    if (!contentsForType) {
+      console.log(`Found no contents for type ${contentTypeName}`);
+      continue;
+    }
+
+    for (const [id, content] of contentsForType) {
+      contents[id] = getContentsWithReferenceFields({ referenceFields, content, id });
+    }
+  }
+
+  return contents;
+}
+
+function getIncomingReferences(contents) {
+  for (const [id, content] of Object.entries(contents)) {
+    if (content.referencedIds.length == 0) {
+      continue;
+    }
+
+    for (const referencedId of content.referencedIds) {
+      const matching = contents[referencedId];
+
+      if (!matching) {
+        console.error(`Content ${id} has outgoing reference ID ${referencedId} which was not found`);
+        continue;
+      }
+
+      matching.referencedByIds.add(id);
+    }
+  }
+}
+
+function getContentsWithReferenceFields({ referenceFields, content, id }) {
+  const referenceFieldValues = referenceFields.map(field => field[0]).map(field => content.fields[field]).filter(field => !!field).flatMap(field => field);
+
+  return {
+    id: id,
+    referencedIds: Array.from(referenceFieldValues.map(getReferenceId).filter(id => !!id)) ?? [],
+    referencedByIds: new Set()
+  };
+}
+
+/**
+ * Adds the original contentful entry value to each content
+ * @param {*} param0 
+ * @returns 
+ */
+function addOriginalContentEntries({ contents, entries }) {
+  return Object.entries(contents).map(([id, content]) => {
+    const matchingEntry = entries.find(entry => entry.sys.id == id);
+
+    if (!matchingEntry) {
+      console.error(`Couldn't find original entry for ${id}`);
+      return null;
+    }
+
+    return {
+      ...content,
+      referencedByIds: Array.from(content.referencedByIds.values()) ?? [],
+      entry: matchingEntry
+    }
+  });
+
+}
+const getReferenceId = value => {
+  const id = value?.sys?.id;
+
+  if (!id) {
+    console.log('couldnt find reference for ' + value);
+  }
+
+  return id;
+};
---
+++ contentful/export-processor/migrations/post-content-to-webhook.js
@@ -0,0 +1,159 @@
+const SleepTimeInMs = {
+  BetweenGroups: 2000,
+  BetweenContent: 50,
+  OnError: 5000
+}
+
+const MaxRetryCount = 5;
+
+export async function postEntriesToWebhook({ contents }) {
+  let groupsMigrated = 1;
+  let insertedItems = [];
+
+  console.log(`Have ${contents.length} initial content to migrate`);
+
+  while (contents.length > 0) {
+    console.log(`Beginning migration of content. Running group ${groupsMigrated}`);
+
+    const withoutReferences = getContentWithMigratedReferences(contents);
+
+    console.log(`Found ${withoutReferences.length} entries for migrating`);
+
+    await getContentsAndPostToWebhook(withoutReferences);
+
+    insertedItems = [...insertedItems, ...withoutReferences.map(content => content.id)];
+
+    console.log(`POSTed ${withoutReferences.length} items - total inserted now ${insertedItems.length}`);
+
+    contents = [...removeMigratedContent(contents, insertedItems)];
+
+    console.log('items remaining ' + contents.length);
+
+    groupsMigrated++;
+
+    await sleep(SleepTimeInMs.BetweenGroups);
+  }
+}
+
+
+function removeMigratedContent(contentWithReferences, insertedItems) {
+  return contentWithReferences.filter(content => !insertedItems.includes(content.id)).map(content => removeMigratedReferences(content, insertedItems));
+}
+
+function removeMigratedReferences(content, insertedItems) {
+  return {
+    ...content,
+    referencedByIds: content.referencedByIds.filter(id => !insertedItems.includes(id)),
+    referencedIds: content.referencedIds.filter(id => !insertedItems.includes(id))
+  };
+}
+
+/**
+ * Gets content with no remaining references
+ * @param {*} contentWithReferences 
+ * @returns 
+ */
+function getContentWithMigratedReferences(contentWithReferences) {
+  return contentWithReferences.filter(content => content.referencedIds.length == 0);
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+/**
+ * Loops through content, posts content entry to webhook with minor wait inbetween
+ * @param {*} content 
+ */
+async function getContentsAndPostToWebhook(content) {
+  for (let x = 0; x < content.length; x++) {
+    const entry = content[x].entry;
+
+    await tryPostContent(entry);
+
+    sleep(SleepTimeInMs.BetweenContent);
+  }
+}
+
+/**
+ * Posts content to webhook, and retries on error.
+ * @param {*} entry 
+ * @returns 
+ */
+async function tryPostContent(entry) {
+  let retryCount = 0;
+  let errorSleepTime = SleepTimeInMs.OnError;
+
+  while (true) {
+    const result = await postToWebook(entry);
+
+    if (result) {
+      return true;
+    }
+
+    console.log('Error posting entry');
+    retryCount++;
+
+    if (retryCount >= MaxRetryCount) {
+      console.log('Reached max retry count');
+      return false;
+    }
+
+    console.log(`Waiting ${sleepTime / 1000} seconds`);
+    await sleep(errorSleepTime);
+    sleepTime *= 5;
+    retryCount++;
+  }
+}
+
+async function postToWebook(content) {
+  const body = getJsonPayload(content);
+
+  try {
+    const result = await fetch(process.env.FUNCTION_APP_URL,
+      {
+        body: body,
+        method: 'POST',
+        headers: {
+          "X-Contentful-Topic": "ContentManagement.Entry.publish",
+          "X-Contentful-Webhook-Name": "Azure Function - Staging",
+          "X-Contentful-Webhook-Request-Attempt": "1",
+          "X-Contentful-Event-Datetime": "2024-05-14T09:21:37.356Z",
+          "Content-Type": "application/vnd.contentful.management.v1+json",
+          "X-Contentful-CRN": "crn:contentful:::content:spaces/py5afvqdlxgo/environments/master/entries/16FlMU8PF9zYVK9Cxqh0TK",
+          "X-Contentful-Idempotency-Key": "984411c33c2bb09cd835ff44e66d74ea65b65247e3640e9698b19f85762b272f",
+        }
+      }
+    )
+
+    if (!result.ok) {
+      throw 'error ' + result.status + " " + result.text();
+    }
+
+    return true;
+  }
+  catch (e) {
+    console.log('Error posting content ' + content.sys.id + '  ' + e, content, JSON.stringify(content));
+    return false;
+  }
+}
+
+/**
+ * Removes various unneeded fields from content, then returns it as JSON string
+ * @param {*} content 
+ * @returns 
+ */
+function getJsonPayload(content) {
+  const body = {
+    sys: {
+      id: content.sys.id,
+      type: content.sys.type,
+      contentType: content.sys.contentType,
+    },
+    fields: content.fields
+  };
+
+  return JSON.stringify(body);
+}
+
