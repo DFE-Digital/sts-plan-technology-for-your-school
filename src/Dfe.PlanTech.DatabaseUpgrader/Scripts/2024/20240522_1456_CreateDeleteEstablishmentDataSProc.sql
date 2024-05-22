2c0fdbbab12227383969e7390d3e36234e492259
Jim Washbrook <jim.washbrook@and.digital>
2024-05-22 17:37:55 +0100

    fix: delete from both tables.

--- src/Dfe.PlanTech.DatabaseUpgrader/Scripts/2024/20240522_1456_CreateDeleteEstablishmentDataSProc.sql
+++ src/Dfe.PlanTech.DatabaseUpgrader/Scripts/2024/20240522_1456_CreateDeleteEstablishmentDataSProc.sql
@@ -2,10 +2,21 @@ CREATE OR ALTER PROCEDURE [dbo].[deleteDataForEstablishment] @establishmentRef n
 AS
     BEGIN TRY
         BEGIN TRAN
-            DELETE R
-            FROM [dbo].[response] R
-            JOIN [dbo].[submission] S ON R.[submissionId] = S.[id]
-            WHERE [S].[establishmentId] = (SELECT [id] FROM [dbo].[establishment] WHERE establishmentRef = @establishmentRef)
+            DECLARE @establishmentId BIGINT;
+
+            SELECT @establishmentId = [id]
+            FROM [dbo].[establishment]
+            WHERE [establishmentRef] = @establishmentRef;
+
+            -- Delete from the response table
+            DELETE FROM [dbo].[response]
+            WHERE [submissionId] IN (
+                SELECT [id] FROM [dbo].[submission] WHERE [establishmentId] = @establishmentId
+            );
+
+            -- Delete from the submission table
+            DELETE FROM [dbo].[submission]
+            WHERE [establishmentId] = @establishmentId
 
         COMMIT TRAN
 
