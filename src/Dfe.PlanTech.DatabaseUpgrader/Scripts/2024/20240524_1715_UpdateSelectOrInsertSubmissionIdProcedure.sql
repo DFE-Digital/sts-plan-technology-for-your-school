698f53e436536e5a6775ca55226eda0249ada1b1
katie-gardner-AND <katharine.gardner@and.digital>
2024-05-28 09:08:34 +0100

    fix: update sprocs to avoid deleted submissions

---
+++ src/Dfe.PlanTech.DatabaseUpgrader/Scripts/2024/20240524_1715_UpdateSelectOrInsertSubmissionIdProcedure.sql
@@ -0,0 +1,35 @@
+ALTER PROCEDURE SelectOrInsertSubmissionId
+  @sectionId NVARCHAR(50),
+  @sectionName NVARCHAR(50),
+  @establishmentId INT,
+  @submissionId INT OUTPUT
+AS
+
+  BEGIN TRY
+    BEGIN TRAN
+
+    SELECT @submissionId = (SELECT
+                    TOP 1
+                    Id from [dbo].[submission]
+                    WHERE completed = 0
+                      AND deleted = 0
+                      AND sectionId = @sectionId
+                      AND sectionName = @sectionName
+                      AND establishmentId = @establishmentId
+                    ORDER BY dateCreated DESC)
+
+    IF @submissionId IS NULL
+      BEGIN
+        INSERT INTO [dbo].[submission]
+          (establishmentId, completed, sectionId, sectionName)
+        output INSERTED.ID 
+        VALUES
+          (@establishmentId, 0, @sectionId, @sectionName)
+      
+        SELECT @submissionId = Scope_Identity() 
+      END
+    COMMIT TRAN
+  END TRY
+  BEGIN CATCH
+    ROLLBACK TRAN
+  END CATCH
---
+++ src/Dfe.PlanTech.DatabaseUpgrader/Scripts/2024/20240524_1730_UpdateGetSectionStatusesSproc.sql
@@ -0,0 +1,24 @@
+﻿ALTER PROCEDURE GetSectionStatuses
+    @splitStringVal nvarchar(255),
+    @establishmentId int
+AS
+
+SELECT value as sectionId
+INTO #SectionIds
+FROM STRING_SPLIT(@splitStringVal, ',')
+
+Select
+    Sub.sectionId,
+    CAST(Sub.completed as int) completed,
+    Sub.maturity,
+    Sub.dateCreated
+From #SectionIds Ids
+CROSS APPLY (
+    Select top 1 sectionId, completed, maturity, dateCreated
+    From [dbo].submission S
+    Where
+        Ids.sectionId = S.sectionId
+      AND S.establishmentId = @establishmentId
+      AND S.deleted = 0
+    ORDER BY S.dateCreated Desc
+) Sub
