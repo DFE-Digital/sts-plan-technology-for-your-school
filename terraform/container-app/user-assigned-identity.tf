e9f7fe782e34a35c43df7f83ba3b66391e1d1ead
Jim Washbrook <jim.washbrook@and.digital>
2024-05-28 11:58:22 +0100

    chore: remove unused MI

--- terraform/container-app/user-assigned-identity.tf
+++ terraform/container-app/user-assigned-identity.tf
@@ -3,9 +3,3 @@ resource "azurerm_user_assigned_identity" "user_assigned_identity" {
   location            = local.azure_location
   resource_group_name = module.main_hosting.azurerm_resource_group_default.name
 }
-
-resource "azurerm_role_assignment" "uami_storage_data_owner_role" {
-  scope                = azurerm_storage_account.function_storage.id
-  role_definition_name = "Storage Blob Data Owner"
-  principal_id         = azurerm_user_assigned_identity.user_assigned_identity.principal_id
-}
