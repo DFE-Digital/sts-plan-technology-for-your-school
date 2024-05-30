9b44a97661664330bff6558e616792b634c9cbad
Jim Washbrook <jim.washbrook@and.digital>
2024-05-30 15:58:35 +0100

    feat: Finish create function app script

--- terraform/container-app/scripts/create-function-app-resources.sh
+++ terraform/container-app/scripts/create-function-app-resources.sh
@@ -30,6 +30,7 @@ usage() {
   echo "  -s               - Name of the Storage Account for the Function App to use"
   echo "  -p               - Product"
   echo "  -e               - Environment"
+  echo "  -i               - User assigned managed identity ID"
 
   exit 1
 }
@@ -45,7 +46,7 @@ then
 fi
 
 # Get commandline arguments
-while getopts "g:s:l:p:e:h" opt; do
+while getopts "g:l:a:f:s:p:e:i:h" opt; do
   case $opt in
     g)
       RESOURCE_GROUP_NAME=$OPTARG
@@ -54,7 +55,7 @@ while getopts "g:s:l:p:e:h" opt; do
       AZ_LOCATION=$OPTARG
       ;;
     a)
-      APP_SERVICE_PLAN_NAME=$OPTARG
+      APPSERVICE_PLAN_NAME=$OPTARG
       ;;
     f)
       FUNCTION_APP_NAME=$OPTARG
@@ -68,6 +69,9 @@ while getopts "g:s:l:p:e:h" opt; do
     p)
       AZ_PRODUCT=$OPTARG
       ;;
+    i)
+      MI_ID=$OPTARG
+      ;;
     h)
       usage
       exit;;
@@ -77,10 +81,10 @@ while getopts "g:s:l:p:e:h" opt; do
   esac
 done
 
-# Validate calues exist
+# Validate arg values exist
 if [[
   -z "$RESOURCE_GROUP_NAME" ||
-  -z "$APPSERVICE_PLAN_NAME"
+  -z "$APPSERVICE_PLAN_NAME" ||
   -z "$AZ_LOCATION" ||
   -z "$FUNCTION_APP_NAME" ||
   -z "$STORAGE_ACCOUNT_NAME" ||
@@ -90,16 +94,71 @@ if [[
   usage
 fi
 
-#If the app service plan exists and is already on the flex consumption plan, then exit early
-APPSERVICE_PLAN=$(az appservice plan show --resource-group s190d01-plantech --name s190d01-plantechappserviceplan | jq '.sku.size')
+get_app_service_plan() {
+    az appservice plan list --resource-group "$RESOURCE_GROUP_NAME" | jq '.[] | select(.name != "'$APPSERVICE_PLAN_NAME'")'
+}
 
-if ["$APPSERVICE_PLAN" == "FC1"]
-  exit 0
-fi
+check_app_service_plan_SKU() {
+    local APPSERVICE_PLAN="$1"
+    local APPSERVICE_PLAN_NAME=$(echo "$APPSERVICE_PLAN" | jq -r '.name')
+    local APPSERVICE_PLAN_SKU=$(echo "$APPSERVICE_PLAN" | jq -r '.sku.size')
 
-#Delete old FA + app service plan, then recreate
-az functionapp delete --name $FUNCTION_APP_NAME --resource-group $RESOURCE_GROUP_NAME
-az appservice plan delete --name $APPSERVICE_PLAN_NAME --resource-group $RESOURCE_GROUP_NAME
+    if [ "$APPSERVICE_PLAN_SKU" != "FC1" ]; then
+        echo "Deleting $APPSERVICE_PLAN_NAME as SKU is $APPSERVICE_PLAN_SKU"
+        az appservice plan delete --name "$APPSERVICE_PLAN_NAME" --resource-group "$RESOURCE_GROUP_NAME" --yes
+        echo "Deleted plan"
+    fi
+}
 
-az appservice plan create --name $APPSERVICE_PLAN_NAME --resource-group $RESOURCE_GROUP_NAME --sku FC1
-az functionapp create --resource-group $RESOURCE_GROUP_NAME --name $FUNCTION_APP_NAME -p $APPSERVICE_PLAN_NAME --storage-account $STORAGE_ACCOUNT_NAME --flexconsumption-location $AZ_LOCATION --runtime dotnet-isolated --runtime-version 8.0 --tags Environment=$AZ_ENVIRONMENT "Service Offering"="$AZ_PRODUCT" Product="$AZ_PRODUCT"
+get_function_app() {
+    az functionapp list -g "$RESOURCE_GROUP_NAME" | jq '.[]'
+}
+
+check_function_app() {
+    local FUNCTION_APP="$1"
+    local APPSERVICE_PLAN="$2"
+
+    if [ -z "$FUNCTION_APP" ]; then
+        echo "No Function App - moving to create"
+    else
+        local FUNCTION_APP_SERVICE_PLAN=$(echo "$FUNCTION_APP" | jq -r '.appServicePlanId')
+        local APPSERVICE_PLAN_id=$(echo "$APPSERVICE_PLAN" | jq -r '.id')
+
+        if [ "$FUNCTION_APP_SERVICE_PLAN" != "$APPSERVICE_PLAN_id" ]; then
+            local existing_function_app_NAME=$(echo "$FUNCTION_APP" | jq -r '.name')
+            echo "Not using App Service plan - deleting $existing_function_app_NAME"
+            az functionapp delete --resource-group "$RESOURCE_GROUP_NAME" --name "$existing_function_app_NAME"
+        else
+            echo "Function app exists and uses correct App Service plan; exiting"
+            exit 0
+        fi
+    fi
+}
+
+create_function_app(){
+  az functionapp create --resource-group $RESOURCE_GROUP_NAME \
+                      --name $FUNCTION_APP_NAME \
+                      --storage-account $STORAGE_ACCOUNT_NAME \
+                      --flexconsumption-location northeurope \
+                      --runtime dotnet-isolated \
+                      --runtime-version 8.0 \
+                      --functions-version 4 \
+                      --assign-identity $MI_ID \
+                      --https-only true \
+                      --tags Environment=$AZ_ENVIRONMENT "Service Offering"="$AZ_PRODUCT" Product="$AZ_PRODUCT" 
+}
+
+main() {
+    local APPSERVICE_PLAN=$(get_app_service_plan)
+
+    if [ -n "$APPSERVICE_PLAN" ]; then
+        check_app_service_plan_SKU "$APPSERVICE_PLAN"
+    else
+        echo "No app service plan"
+    fi
+
+    local function_app=$(get_function_app)
+    check_function_app "$FUNCTION_APP" "$APPSERVICE_PLAN"
+}
+
+main
