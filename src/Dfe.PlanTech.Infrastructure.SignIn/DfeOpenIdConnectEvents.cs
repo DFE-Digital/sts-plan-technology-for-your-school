ab99707b7e5ba37c7172704cc87ea8f9c217ab4c
jimwashbrook <131891854+jimwashbrook@users.noreply.github.com>
2024-06-12 13:33:15 +0100

    fix(dsi): Remove extra forward slash if existing (#664)
    ## Fixes
    
    - Removes a forward slash from the end of the origin URL _if_ the callback path also starts with a forward slash[^1]
    
    [^1]: The `x-forwarded-host` header ends with a forward slash from Azure Front Door. Our config variables for the callbacks start with a forward slash `/auth/cb`. This could be fixed by tweaking our environment variables slightly, but I think this gives us a bit more security.

--- src/Dfe.PlanTech.Infrastructure.SignIn/DfeOpenIdConnectEvents.cs
+++ src/Dfe.PlanTech.Infrastructure.SignIn/DfeOpenIdConnectEvents.cs
@@ -17,9 +17,7 @@ public static class DfeOpenIdConnectEvents
     {
         var config = context.HttpContext.RequestServices.GetRequiredService<IDfeSignInConfiguration>();
 
-        var originUrl = GetOriginUrl(context, config);
-
-        context.ProtocolMessage.RedirectUri = $"{originUrl}{config.CallbackUrl}";
+        context.ProtocolMessage.RedirectUri = CreateCallbackUrl(context, config, config.CallbackUrl);
 
         return Task.FromResult(0);
     }
@@ -35,15 +33,20 @@ public static class DfeOpenIdConnectEvents
         {
             var config = context.HttpContext.RequestServices.GetRequiredService<IDfeSignInConfiguration>();
 
-            var originUrl = GetOriginUrl(context, config);
-
-            context.ProtocolMessage.PostLogoutRedirectUri = $"{originUrl}{config.SignoutRedirectUrl}";
+            context.ProtocolMessage.PostLogoutRedirectUri = CreateCallbackUrl(context, config, config.SignoutRedirectUrl);
         }
 
         return Task.FromResult(0);
     }
 
 
+    /// <summary>
+    /// Gets the origin URL to be used for OpenID Connect redirects.
+    /// </summary>
+    /// <remarks>
+    /// Uses the X-Forwarded-Host header if available, otherwise the FrontDoorUrl field from the <see cref="IDfeSignInConfiguration">config</see>
+    /// <param name="context">The context of the redirect request</param>
+    /// <param name="config">The configuration for OpenID Connect sign-ins</param>
     public static string GetOriginUrl(RedirectContext context, IDfeSignInConfiguration config)
     {
         var forwardHostHeader = context.HttpContext.Request.Headers
@@ -51,11 +54,26 @@ public static class DfeOpenIdConnectEvents
                                                             .Select(header => header.Value.FirstOrDefault())
                                                             .FirstOrDefault();
 
-        if (forwardHostHeader != null)
+        return forwardHostHeader ?? config.FrontDoorUrl;
+    }
+
+    /// <summary>
+    /// Creates the callback URL by combining the origin URL and the callback path.
+    /// </summary>
+    /// <param name="context">The context of the redirect request.</param>
+    /// <param name="config">The configuration for OpenID Connect sign-ins.</param>
+    /// <param name="callbackPath">The path of the callback URL.</param>
+    public static string CreateCallbackUrl(RedirectContext context, IDfeSignInConfiguration config, string callbackPath)
+    {
+        var originUrl = GetOriginUrl(context, config);
+
+        //Our config uses URls like "/auth/cb" for the redirect callback slug, signout slug, etc.
+        //So we should ensure that the URL does not end with a forward slash when returning
+        if (originUrl.EndsWith('/') && callbackPath.StartsWith('/'))
         {
-            return $"https://{forwardHostHeader}/";
+            originUrl = originUrl[..^1];
         }
 
-        return config.FrontDoorUrl;
+        return $"{originUrl}{callbackPath}";
     }
 }
