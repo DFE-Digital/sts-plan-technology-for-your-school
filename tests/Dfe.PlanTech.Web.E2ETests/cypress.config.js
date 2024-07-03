1cb4aee4bfc147dc9f28e1704159fa99def77124
Jim Washbrook <jim.washbrook@and.digital>
2024-07-03 10:29:51 +0100

    change(e2e): Increase E2E test timeout by ~25%
    - Also increase retries to _3_.

--- tests/Dfe.PlanTech.Web.E2ETests/cypress.config.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress.config.js
@@ -21,23 +21,31 @@ module.exports = defineConfig({
     "configFile": "reporter-config.json"
   },
   retries: {
-    runMode: 1
+    runMode: 3,
   },
+  //Timeouts
+  //Original values: https://docs.cypress.io/guides/references/configuration#Timeouts
+  defaultCommandTimeout: 5000,
+  execTimeout: 75000,
+  taskTimeout: 75000,
+  pageLoadTimeout: 75000,
+  requestTimeout: 7000,
+  responseTimeout: 38000,
   e2e: {
     setupNodeEvents(on, _) {
       on('task', {
         log(message) {
-          console.log(message)
+          console.log(message);
 
-          return null
+          return null;
         },
         table(message) {
-          console.table(message)
+          console.table(message);
 
-          return null
+          return null;
         }
       }),
-        on('file:preprocessor', webpackPreprocessor(webpackOptions))
+        on('file:preprocessor', webpackPreprocessor(webpackOptions));
     },
   },
-})
+});
