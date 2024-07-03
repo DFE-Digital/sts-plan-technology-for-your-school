69fafa5d8fd3995ae5b8f0027636b8d68ac11b67
katie-gardner-AND <katharine.gardner@and.digital>
2024-07-03 13:39:48 +0100

    fix: fix tests and add timezone tests

--- src/Dfe.PlanTech.Domain/CategorySection/CategorySectionDto.cs
+++ src/Dfe.PlanTech.Domain/CategorySection/CategorySectionDto.cs
@@ -1,4 +1,6 @@
 using Dfe.PlanTech.Domain.Constants;
+using Dfe.PlanTech.Domain.Helpers;
+using Dfe.PlanTech.Domain.Interfaces;
 using Dfe.PlanTech.Domain.Questionnaire.Models;
 using Dfe.PlanTech.Domain.Submissions.Models;
 
@@ -16,14 +18,20 @@ public class CategorySectionDto
 
     public CategorySectionRecommendationDto? Recommendation { get; init; }
 
-    public CategorySectionDto(string? slug, string name, bool retrievalError, SectionStatusDto? sectionStatus, CategorySectionRecommendationDto recommendation)
+    public CategorySectionDto(
+        string? slug,
+        string name,
+        bool retrievalError,
+        SectionStatusDto? sectionStatus,
+        CategorySectionRecommendationDto recommendation,
+        ISystemTime systemTime)
     {
         Slug = slug;
         Name = name;
         Recommendation = recommendation;
         var started = sectionStatus != null;
         var completed = sectionStatus?.Completed == true;
-        var lastEdited = LastEditedDate(sectionStatus?.DateUpdated);
+        var lastEdited = LastEditedDate(sectionStatus?.DateUpdated, systemTime);
         if (string.IsNullOrWhiteSpace(slug))
         {
             ErrorMessage = $"{Name} unavailable";
@@ -39,12 +47,12 @@ public class CategorySectionDto
             Tag = new Tag("NOT STARTED", TagColour.Grey);
     }
 
-    private static string? LastEditedDate(DateTime? date)
+    private string? LastEditedDate(DateTime? date, ISystemTime systemTime)
     {
         if (date == null)
             return null;
         var britishTimeZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
         var localTime = TimeZoneInfo.ConvertTimeFromUtc(date.Value, britishTimeZone);
-        return localTime.Date == DateTime.Today.Date ? $"{localTime:hh:mm tt}" : $"{localTime:dd/MM/yyyy}";
+        return localTime.Date == systemTime.Today.Date ? $"{localTime:hh:mm tt}" : $"{localTime:dd/MM/yyyy}";
     }
 }
