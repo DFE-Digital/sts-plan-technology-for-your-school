377074124cb3431f096542d26f99a55fe01896a6
katie-gardner-AND <katharine.gardner@and.digital>
2024-07-10 11:58:31 +0100

    Feat: add support for headings in rich text

---
+++ src/Dfe.PlanTech.Infrastructure.Contentful/Content/Renderers/Models/PartRenderers/HeadingRenderer.cs
@@ -0,0 +1,28 @@
+using Dfe.PlanTech.Domain.Content.Enums;
+using Dfe.PlanTech.Domain.Content.Interfaces;
+using Dfe.PlanTech.Domain.Content.Models;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Dfe.PlanTech.Infrastructure.Contentful.Content.Renderers.Models.PartRenderers;
+
+public partial class HeadingRenderer : BaseRichTextContentPartRender
+{
+    public HeadingRenderer() : base(RichTextNodeType.Heading)
+    {
+    }
+
+    public override StringBuilder AddHtml(RichTextContent content, IRichTextContentPartRendererCollection rendererCollection, StringBuilder stringBuilder)
+    {
+        var tag = GetHeaderTag().Replace(content.NodeType,"$1$2");
+
+        stringBuilder.Append($"<{tag}>");
+        rendererCollection.RenderChildren(content, stringBuilder);
+        stringBuilder.Append($"</{tag}>");
+
+        return stringBuilder;
+    }
+
+    [GeneratedRegex("(h)eading-(\\d)")]
+    private static partial Regex GetHeaderTag();
+}
