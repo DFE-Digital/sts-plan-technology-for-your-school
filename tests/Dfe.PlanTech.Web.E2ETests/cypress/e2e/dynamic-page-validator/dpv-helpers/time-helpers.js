74f97a35ba8c79c2414498346b462180c2f60b66
gilaineyo <gilaineyoung@gmail.com>
2024-07-11 15:03:18 +0100

    Fix for submission time check - handling 59 minutes past the hour

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/dynamic-page-validator/dpv-helpers/time-helpers.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/dynamic-page-validator/dpv-helpers/time-helpers.js
@@ -6,8 +6,10 @@ export const getSubmissionTimeText = (time) => {
 }
 
 export const getSlowTestSubmissionTimeText = (time) => {
-    const hours = time.getHours() < 13 ? time.getHours() : time.getHours() - 12;
-    const minutes = (time.getMinutes() + 1).toString().length === 1 ? `0${time.getMinutes() + 1}` : `${time.getMinutes() + 1}`;
 
-    return `last completed ${hours}:${minutes}${time.getHours() < 12 ? "am" : "pm"}`
+    time.setMinutes(time.getMinutes() + 1)    
+    const hours = time.getHours() <= 12 ? time.getHours() : time.getHours() - 12;
+    const minutes = time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes();
+
+    return `last completed ${hours}:${minutes}${time.getHours() < 1 ? "am" : "pm"}`
 }
