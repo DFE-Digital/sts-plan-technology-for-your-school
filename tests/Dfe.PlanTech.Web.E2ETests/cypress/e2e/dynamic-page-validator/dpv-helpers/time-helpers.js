568dc6b7f71fd4b7412c018bea6a37a863d5959b
gilaineyo <gilaineyoung@gmail.com>
2024-07-18 11:38:04 +0100

    Fixes intermittent errors with getSubmissionTimeText

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/dynamic-page-validator/dpv-helpers/time-helpers.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/dynamic-page-validator/dpv-helpers/time-helpers.js
@@ -1,6 +1,8 @@
 export const getSubmissionTimeText = (time) => {
-    const hours = time.getHours() <= 12 ? time.getHours() : time.getHours() - 12;
-    const minutes = time.getMinutes() < 10 ? time.getMinutes().padStart(2, "0") : time.getMinutes();
 
-    return `last completed ${hours}:${minutes}${time.getHours() < 12 ? "am" : "pm"}`
+    const formatted = time.toLocaleTimeString("en-US", {
+        timeStyle: 'short',
+    }).toLowerCase().replace(" ", "");
+
+    return `last completed ${formatted}`
 }
