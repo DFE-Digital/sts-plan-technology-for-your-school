5b70157293f5eca8e63be6a95f4ec7d702dd4081
Jim Washbrook <jim.washbrook@and.digital>
2024-07-25 18:11:47 +0100

    testing

--- src/Dfe.PlanTech.Application/Submissions/Queries/GetSubmissionStatusesQuery.cs
+++ src/Dfe.PlanTech.Application/Submissions/Queries/GetSubmissionStatusesQuery.cs
@@ -7,23 +7,14 @@ using Dfe.PlanTech.Domain.Users.Interfaces;
 
 namespace Dfe.PlanTech.Application.Submissions.Queries;
 
-public class GetSubmissionStatusesQuery : IGetSubmissionStatusesQuery
+public class GetSubmissionStatusesQuery(IPlanTechDbContext db, IUser userHelper) : IGetSubmissionStatusesQuery
 {
-    private readonly IPlanTechDbContext _db;
-    private readonly IUser _userHelper;
-
-    public GetSubmissionStatusesQuery(IPlanTechDbContext db, IUser userHelper)
-    {
-        _db = db;
-        _userHelper = userHelper;
-
-    }
 
     public async Task<List<SectionStatusDto>> GetSectionSubmissionStatuses(string categoryId)
     {
-        int establishmentId = await _userHelper.GetEstablishmentId();
+        int establishmentId = await userHelper.GetEstablishmentId();
 
-        return await _db.ToListAsync(_db.GetSectionStatuses(categoryId, establishmentId));
+        return await db.ToListAsync(db.GetSectionStatuses(categoryId, establishmentId));
     }
 
     public async Task<SectionStatusNew> GetSectionSubmissionStatusAsync(int establishmentId,
@@ -31,12 +22,12 @@ public class GetSubmissionStatusesQuery : IGetSubmissionStatusesQuery
                                                                          bool completed,
                                                                          CancellationToken cancellationToken)
     {
-        var sectionStatus = _db.GetSubmissions.Where(submission => submission.EstablishmentId == establishmentId &&
+        var sectionStatus = db.GetSubmissions.Where(submission => submission.EstablishmentId == establishmentId &&
                                                  submission.SectionId == section.Sys.Id && !submission.Deleted);
 
         var groupedAndLatest = GetLatestSubmissionStatus(sectionStatus, completed);
 
-        var result = await _db.FirstOrDefaultAsync(groupedAndLatest, cancellationToken);
+        var result = await db.FirstOrDefaultAsync(groupedAndLatest, cancellationToken);
 
         return result ?? new SectionStatusNew()
         {
@@ -56,7 +47,9 @@ public class GetSubmissionStatusesQuery : IGetSubmissionStatusesQuery
     /// <param name="completed"></param>
     /// <returns></returns>
     private static IQueryable<SectionStatusNew> GetLatestSubmissionStatus(IQueryable<Submission> submissionStatuses, bool completed)
-        => submissionStatuses.Select(submission => new SectionStatusNew()
+    => submissionStatuses
+        .Where(submission => completed == submission.Completed)
+        .Select(submission => new SectionStatusNew()
         {
             DateCreated = submission.DateCreated,
             Completed = submission.Completed,
@@ -64,7 +57,6 @@ public class GetSubmissionStatusesQuery : IGetSubmissionStatusesQuery
             SectionId = submission.SectionId,
             Status = submission.Completed ? Status.Completed : submission.Responses.Count != 0 ? Status.InProgress : Status.NotStarted,
         })
-        .Where(submission => !completed || submission.Completed)
         .GroupBy(submission => submission.SectionId)
         .Select(grouping => grouping.OrderByDescending(status => status.DateCreated).First());
 }
