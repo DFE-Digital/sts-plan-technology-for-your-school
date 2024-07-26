a8fec56fb2999fbdfe36cb2baa9e124086dc08e1
Jim Washbrook <jim.washbrook@and.digital>
2024-07-26 13:41:55 +0100

    chore(tf): upgrade azurerm + azureapi verisons

--- terraform/container-app/versions.tf
+++ terraform/container-app/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 3.111.0"
+      version = "= 3.113.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = ">= 1.6.0"
+      version = ">= 1.14.0"
     }
     null = {
       source  = "hashicorp/null"
