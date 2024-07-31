cd1dbea063806e44acec38e95f39404fa120ded7
jack.coggin <jack.coggin@and.digital>
2024-07-31 14:44:57 +0100

    fix: check C&S link columns don't exist before adding

--- src/Dfe.PlanTech.DatabaseUpgrader/Scripts/2024/20240729_1558_AddCSLinkToRecommendationChunk.sql
+++ src/Dfe.PlanTech.DatabaseUpgrader/Scripts/2024/20240729_1558_AddCSLinkToRecommendationChunk.sql
@@ -1,3 +1,17 @@
-ALTER TABLE Contentful.RecommendationChunks
-    ADD [CSLinkText] NVARCHAR(256),
-        [CSUrl] NVARCHAR(256);
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS 
+               WHERE TABLE_NAME = 'RecommendationChunks' 
+               AND TABLE_SCHEMA = 'Contentful' 
+               AND COLUMN_NAME = 'CSLinkText')
+BEGIN
+    ALTER TABLE Contentful.RecommendationChunks
+        ADD [CSLinkText] NVARCHAR(256);
+END;
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS 
+               WHERE TABLE_NAME = 'RecommendationChunks' 
+               AND TABLE_SCHEMA = 'Contentful' 
+               AND COLUMN_NAME = 'CSUrl')
+BEGIN
+    ALTER TABLE Contentful.RecommendationChunks
+        ADD [CSUrl] NVARCHAR(256);
+END;
