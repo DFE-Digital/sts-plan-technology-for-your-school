fba2744e60e2801f1f8aa1af129d5bed2c94d4c7
Jim Washbrook <jim.washbrook@and.digital>
2024-08-01 14:18:24 +0100

    feat: Start of TF upgrade script

---
+++ terraform/container-app/upgrade-scripts/README.md
@@ -0,0 +1,1 @@
+Collection of scripts for manual changes required for various releases.
---
+++ terraform/container-app/upgrade-scripts/sprint_33/main.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+
+usage() {
+  echo "Usage: $(basename "$0") [OPTIONS]" 1>&2
+  echo "  -h               - help"
+  echo "  -g               - Resource group name"
+  echo "  -t               - Terraform variables file path"
+
+  exit 1
+}
+
+##########################
+# Command line arguments #
+##########################
+
+# Validate that commandline arguments exist
+if [ $# -lt 1 ];
+then
+ usage
+fi
+
+# Get commandline arguments
+while getopts "g:t:h" opt; do
+  case $opt in
+    g)
+      RESOURCE_GROUP_NAME=$OPTARG
+      ;;
+    t)
+      TFVAR_PATH=$OPTARG
+      ;;
+    h)
+      usage
+      exit;;
+    *)
+      usage
+      exit;;
+  esac
+done
+
+# Validate calues exist
+if [[
+  -z "$RESOURCE_GROUP_NAME" ||
+  -z "$TFVAR_PATH"
+]]; then
+  usage
+fi
+
+bash $(dirname "$0")/import_keyvault_secrets.sh
