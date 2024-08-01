e78c39cb24bfba49b187c37442918667564c0b5a
Jim Washbrook <jim.washbrook@and.digital>
2024-08-01 15:04:16 +0100

    docs(tf): Add basic documentation

--- terraform/container-app/upgrade-scripts/shared/keyvault/state_import.sh
+++ terraform/container-app/upgrade-scripts/shared/keyvault/state_import.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
+
+## Retrieves the ID for a Key Vault secret, then imports it into TF state for a specified resource
+
+## Usage:
+## Requires exporting the following environment variables:
+## SECRET_NAME: The name of the Key Vault secret (as shown on the Azure Portal)
+## KEYVAULT_NAME: The name of the Key Vault
+## TFVAR_PATH: The location of the Terraform variables file, relative to the dir that the _Terminal_ is running this script (or the calling script of this script) from
+## TF_ID_NAME: The name of the resource in Terraform to import to. E.g. azurerm_key_vault_secret.example
+
 if [[ -z "$SECRET_NAME" || -z "$KEYVAULT_NAME" ]]; then
   echo "Variable(s) not set. \$SECRET_NAME and \$KEYVAULT_NAME are required"
   exit 1
