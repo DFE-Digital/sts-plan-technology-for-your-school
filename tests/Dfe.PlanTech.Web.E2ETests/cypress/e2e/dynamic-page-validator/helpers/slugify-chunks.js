341932d79c22d5992e0e63dd9155711e805d87ab
gilaineyo <gilaineyoung@gmail.com>
2024-08-05 16:49:44 +0100

    test: updated e2e tests for recommendation chunk slug

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/dynamic-page-validator/helpers/slugify-chunks.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/dynamic-page-validator/helpers/slugify-chunks.js
@@ -1,5 +1,5 @@
 export const slugifyChunks = (text) => {
-    const removeNonAlphanumericCharactersRegexPattern = /[^a-zA-Z0-9\s]/g
+    const removeNonAlphanumericExceptHyphensRegexPattern = /[^a-zA-Z0-9\s-]/g
 
-    return text.replace(removeNonAlphanumericCharactersRegexPattern, "").trim().replace(/ /g, "-").toLowerCase();
+    return text.replace(removeNonAlphanumericExceptHyphensRegexPattern, "").trim().replace(/ /g, "-").toLowerCase();
 }
