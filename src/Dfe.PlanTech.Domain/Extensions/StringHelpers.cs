7c3e7912f1b12addb54fe837eaefc85e30cdab03
gilaineyo <gilaineyoung@gmail.com>
2024-08-05 16:37:53 +0100

    feat: change slugify method to retain hyphens from text to be slugified

--- src/Dfe.PlanTech.Domain/Extensions/StringHelpers.cs
+++ src/Dfe.PlanTech.Domain/Extensions/StringHelpers.cs
@@ -4,14 +4,14 @@ namespace Dfe.PlanTech;
 
 public static partial class StringHelpers
 {
-    private const string RemoveNonAlphanumericCharactersRegexPattern = @"[^a-zA-Z0-9\s]";
+    private const string RemoveNonAlphanumericExceptHyphensRegexPattern = @"[^a-zA-Z0-9\s-]";
 
-    // Extension method to slugify a string by removing non-alphanumeric characters, replacing spaces with hyphens, and converting to lowercase.
+    // Extension method to slugify a string by removing non-alphanumeric characters (except hyphens to preserve hyphenated words), replacing spaces with hyphens, and converting to lowercase.
     public static string Slugify(this string text)
-    => RemoveNonAlphaNumericCharactersPattern().Replace(text, "").Replace(" ", "-").ToLower();
+    => RemoveNonAlphaNumericExceptHyphensPattern().Replace(text, "").Replace(" ", "-").ToLower();
 
     public static string TruncateIfOverLength(this string text, int maxLength) => text.Length < maxLength ? text : text[..maxLength];
 
-    [GeneratedRegex(RemoveNonAlphanumericCharactersRegexPattern)]
-    private static partial Regex RemoveNonAlphaNumericCharactersPattern();
+    [GeneratedRegex(RemoveNonAlphanumericExceptHyphensRegexPattern)]
+    private static partial Regex RemoveNonAlphaNumericExceptHyphensPattern();
 }
