41f5446c30601472ac9829be6f5d35c3775328ee
jimwashbrook <131891854+jimwashbrook@users.noreply.github.com>
2024-08-28 10:04:04 +0100

    chore: Rename section status (#758)
    * chore: Rename section status
    
    Renames `SectionStatusNew` to `SectionStatus`.
    
    * chore: Linted code for plan-technology-for-your-school.sln solution
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/Dfe.PlanTech.Domain/Submissions/Models/SectionStatusDto.cs
+++ src/Dfe.PlanTech.Domain/Submissions/Models/SectionStatusDto.cs
@@ -1,16 +1,14 @@
-﻿using Dfe.PlanTech.Domain.Submissions.Enums;
-
 namespace Dfe.PlanTech.Domain.Submissions.Models;
 
-public record SectionStatus
+public class SectionStatusDto
 {
     public string SectionId { get; set; } = null!;
 
     public bool Completed { get; set; }
 
-    public string? Maturity { get; set; }
+    public string? LastMaturity { get; set; }
 
     public DateTime DateCreated { get; set; }
 
-    public Status Status { get; init; }
+    public DateTime DateUpdated { get; set; }
 }
