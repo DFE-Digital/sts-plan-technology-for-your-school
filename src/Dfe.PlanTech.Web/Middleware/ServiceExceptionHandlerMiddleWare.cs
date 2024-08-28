a0ffb36520a97e21ff13fa97b7d5cc93c04396fe
Jim Washbrook <jim.washbrook@and.digital>
2024-08-28 14:31:32 +0100

    feat: Handle missing question/answers better

--- src/Dfe.PlanTech.Web/Middleware/ServiceExceptionHandlerMiddleWare.cs
+++ src/Dfe.PlanTech.Web/Middleware/ServiceExceptionHandlerMiddleWare.cs
@@ -8,15 +8,28 @@ namespace Dfe.PlanTech.Web.Middleware;
 
 public class ServiceExceptionHandlerMiddleWare : IExceptionHandlerMiddleware
 {
-    public void ContextRedirect(HttpContext context)
+    public readonly UserJourneyMissingContentExceptionHandler UserJourneyExceptionHandler;
+
+    public ServiceExceptionHandlerMiddleWare(UserJourneyMissingContentExceptionHandler userJourneyExceptionHandler)
+    {
+        UserJourneyExceptionHandler = userJourneyExceptionHandler;
+    }
+
+    public async Task HandleException(HttpContext context)
     {
         var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
         var exception = exceptionHandlerPathFeature?.Error;
 
+        await UserJourneyExceptionHandler.TryHandleException(exception, context.RequestAborted);
+
+        ContextRedirect(context, exception);
+    }
+
+    public void ContextRedirect(HttpContext context, Exception? exception)
+    {
         string redirectUrl = GetRedirectUrlForException(exception);
 
         context.Response.Redirect(redirectUrl);
-
     }
 
     static string GetRedirectUrlForException(Exception? exception) =>
