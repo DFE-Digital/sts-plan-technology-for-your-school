18f31943addaf4efee61f45a1f6b28aeb43f0afe
Jim Washbrook <jim.washbrook@and.digital>
2024-08-30 13:18:07 +0100

    tests: Add and amend existing unit tests
    fix: equality check
    
    fix: equality check

--- src/Dfe.PlanTech.Web/Authorisation/PageModelAuthorisationPolicy.cs
+++ src/Dfe.PlanTech.Web/Authorisation/PageModelAuthorisationPolicy.cs
@@ -6,30 +6,25 @@ using Microsoft.AspNetCore.Authorization;
 namespace Dfe.PlanTech.Web.Authorisation;
 
 /// <summary>
-/// Retrieves a given <see cref="Page"/> from Contentful, and authorises the user based off that
+/// Checks user authorisation for the current page, and retrieves a given <see cref="Page"/> from Contentful if needed for the request.
 /// </summary>
 public class PageModelAuthorisationPolicy(ILogger<PageModelAuthorisationPolicy> logger) : AuthorizationHandler<PageAuthorisationRequirement>
 {
-    public const string PolicyName = "UsePageAuthentication";
-    public const string RoutesValuesRouteNameKey = "route";
-    public const string RouteValuesControllerNameKey = "controller";
     private const string IndexSlug = "/";
-    private readonly ILogger<PageModelAuthorisationPolicy> _logger;
-
-    public PageModelAuthorisationPolicy(ILogger<PageModelAuthorisationPolicy> logger)
-    {
-        _logger = logger;
-    }
+    public const string PolicyName = "UsePageAuthentication";
+    public const string RouteValuesActionNameKey = "action";
+    public const string RouteValuesControllerNameKey = "controller";
+    public const string RoutesValuesRouteNameKey = "route";
 
     protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, PageAuthorisationRequirement requirement)
     {
         if (context.Resource is not HttpContext httpContext)
         {
-            _logger.LogError("Expected resource to be HttpContext but received {type}", context.Resource?.GetType());
+            logger.LogError("Expected resource to be HttpContext but received {type}", context.Resource?.GetType());
             return;
         }
 
-        bool success = await ProcessPage(httpContext);
+        var success = await UserAuthenticationMatchesRouteRequirements(httpContext);
 
         if (success)
         {
@@ -41,7 +36,22 @@ public class PageModelAuthorisationPolicy(ILogger<PageModelAuthorisationPolicy> 
         }
     }
 
-    private static async Task<bool> ProcessPage(HttpContext httpContext)
+    /// <summary>
+    /// Checks whether the user's authentication matches the requirements for the route
+    /// </summary>
+    /// <remarks>
+    /// Process:
+    /// 1. Is the request to the PagesController?
+    ///    - If not, return whether they are authenticated or not
+    /// 2. If it is the pages controller, retrieve the relevant page for the request from DB/Contentful
+    /// 3. Return whether the user's authentication status matches the page's authentication requirements
+    ///    - Page does not require authentication? Then return true
+    ///    - Page requires authentication? Return whether the user is authenticated or not
+    /// </remarks>
+    /// <param name="httpContext"></param>
+    /// <returns></returns>
+    /// <exception cref="KeyNotFoundException"></exception>
+    private async Task<bool> UserAuthenticationMatchesRouteRequirements(HttpContext httpContext)
     {
         string? slug = GetRequestRoute(httpContext);
 
@@ -53,43 +63,76 @@ public class PageModelAuthorisationPolicy(ILogger<PageModelAuthorisationPolicy> 
         using var scope = httpContext.RequestServices.CreateAsyncScope();
         var pageQuery = scope.ServiceProvider.GetRequiredService<IGetPageQuery>();
 
-        Page page = await GetPageForSlug(httpContext, slug, pageQuery) ?? throw new KeyNotFoundException($"Could not find page for {slug}");
-
-        httpContext.Items.Add(nameof(Page), page);
+        Page page = await GetPageForSlug(httpContext, slug, pageQuery);
 
         return !page.RequiresAuthorisation || UserIsAuthenticated(httpContext);
     }
 
+    /// <summary>
+    /// Retrieves the relevant page from Contentful/DB, and adds it to the HttpContext.
+    /// </summary>
+    /// <remarks>
+    /// The page ias added to the HttpContext for use in the <see cref="PageModelBinder"/>, 
+    /// to prevent the page being loaded multiple times for a single request
+    /// </remarks>
     private static async Task<Page> GetPageForSlug(HttpContext httpContext, string slug, IGetPageQuery pageQuery)
-    => await pageQuery.GetPageBySlug(slug, httpContext.RequestAborted) ?? throw new KeyNotFoundException($"Could not find page with slug {slug}");
+    {
+        var page = await pageQuery.GetPageBySlug(slug, httpContext.RequestAborted) ?? throw new KeyNotFoundException($"Could not find page with slug {slug}");
+        httpContext.Items.Add(nameof(Page), page);
+
+        return page;
+    }
 
     private static bool UserIsAuthenticated(HttpContext httpContext) => httpContext.User.Identity?.IsAuthenticated == true;
 
-    private static string? GetRequestRoute(HttpContext httpContext)
+    /// <summary>
+    /// Gets the slug from the route if on the pages controller, or null if not.
+    /// </summary>
+    /// <param name="httpContext"></param>
+    /// <returns></returns>
+    private string? GetRequestRoute(HttpContext httpContext)
     {
-        var controllerName = httpContext.Request.RouteValues[RouteValuesControllerNameKey];
-        if (controllerName == null || controllerName is not string controllerString || controllerString != PagesController.ControllerName)
+        if (!ControllerIsPagesController(httpContext))
         {
+            logger.LogTrace("Request is not from/to the Pages controller. Request is to controller {ControllerName} and action {ActioName}",
+                            httpContext.Request.RouteValues[RouteValuesControllerNameKey],
+                            httpContext.Request.RouteValues[RouteValuesActionNameKey]);
+
             return null;
         }
 
+        return GetSlugFromRoute(httpContext);
+    }
+
+    /// <summary>
+    /// Gets the slug of the page from the request route values, or the index page slug if none is found
+    /// </summary>
+    /// <param name="httpContext"></param>
+    /// <returns></returns>
+    private string GetSlugFromRoute(HttpContext httpContext)
+    {
         var slug = httpContext.Request.RouteValues[RoutesValuesRouteNameKey];
 
         if (slug is not string slugString)
         {
+            logger.LogTrace("Route is null - request is to index page");
             httpContext.Request.RouteValues[RoutesValuesRouteNameKey] = IndexSlug;
             return IndexSlug;
         }
 
+        logger.LogTrace("Route slug for request is {Slug}", slug);
         return slugString;
     }
-}
 
-
-public class PageAuthorisationRequirement : IAuthorizationRequirement
-{
-    public PageAuthorisationRequirement()
+    /// <summary>
+    /// Checks whether the request is from the PagesController
+    /// </summary>
+    /// <param name="httpContext"></param>
+    /// <returns></returns>
+    private static bool ControllerIsPagesController(HttpContext httpContext)
     {
+        var controllerName = httpContext.Request.RouteValues[RouteValuesControllerNameKey];
 
+        return controllerName is string controllerString && controllerString == PagesController.ControllerName;
     }
 }
