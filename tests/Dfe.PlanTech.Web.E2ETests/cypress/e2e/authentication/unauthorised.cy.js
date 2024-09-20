074ebfd6c4828968ba472084a02199f0afc76453
Jim Washbrook <jim.washbrook@and.digital>
2024-09-20 16:39:58 +0100

    tests(e2e): Move loginWithEnv to before each

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/authentication/unauthorised.cy.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/authentication/unauthorised.cy.js
@@ -1,9 +1,11 @@
 import { selfAssessmentSlug } from "../../helpers/page-slugs.js";
 
 describe("Tests for users who are unauthorised, but authenticated", () => {
-  it("Should redirect user to no org error page if user has no organisation", () => {
+  beforeEach(() => {
     cy.loginWithEnv(selfAssessmentSlug, { userHasOrg: false });
+  });
 
+  it("Should redirect user to no org error page if user has no organisation", () => {
     cy.url().should('include', 'dsi-error-not-associated-organisation');
   });
 });
