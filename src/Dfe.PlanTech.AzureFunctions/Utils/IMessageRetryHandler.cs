37722c298ce67631aec6e6f87c86547744aa44ea
Jim Washbrook <jim.washbrook@and.digital>
2024-09-24 16:54:46 +0100

    chore: delete azurefunctions project, cleanup sln
    note: Doesn't delete the AzureFunctions test project, as I'm going to move all the tests

--- src/Dfe.PlanTech.AzureFunctions/Utils/IMessageRetryHandler.cs
+++
@@ -1,8 +0,0 @@
-using Azure.Messaging.ServiceBus;
-
-namespace Dfe.PlanTech.AzureFunctions.Utils;
-
-public interface IMessageRetryHandler
-{
-    Task<bool> RetryRequired(ServiceBusReceivedMessage message, CancellationToken cancellationToken);
-}
