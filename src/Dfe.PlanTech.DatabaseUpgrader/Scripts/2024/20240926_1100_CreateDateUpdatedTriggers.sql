c24156ec88e36e65b19c9f8465ddde74689da725
katie-gardner-AND <katharine.gardner@and.digital>
2024-09-26 11:34:08 +0100

    feat: utilise the dateLastUpdated columns

---
+++ src/Dfe.PlanTech.DatabaseUpgrader/Scripts/2024/20240926_1100_CreateDateUpdatedTriggers.sql
@@ -0,0 +1,145 @@
+-- Set missing updated date values
+
+UPDATE dbo.[user]
+SET dateLastUpdated = dateCreated
+WHERE
+    dateLastUpdated IS NULL
+
+UPDATE dbo.[establishment]
+SET dateLastUpdated = dateCreated
+WHERE
+    dateLastUpdated IS NULL
+
+UPDATE dbo.[response]
+SET dateLastUpdated = dateCreated
+WHERE
+    dateLastUpdated IS NULL
+
+UPDATE S
+SET dateLastUpdated = ISNULL(S.dateCompleted, ISNULL(LASTRESPONSE.dateCreated, S.dateCreated))
+FROM dbo.submission S
+CROSS APPLY (
+    SELECT TOP 1
+        R.dateCreated
+    FROM [dbo].response R
+    WHERE
+        R.submissionId = S.id
+    ORDER BY R.dateCreated DESC
+) LastResponse
+WHERE
+    dateLastUpdated IS NULL
+
+GO
+
+-- Make all dateUpdated columns non-nullable
+
+ALTER TABLE dbo.[user]
+    ALTER COLUMN dateLastUpdated DATETIME NOT NULL
+ALTER TABLE dbo.[establishment]
+    ALTER COLUMN dateLastUpdated DATETIME NOT NULL
+ALTER TABLE dbo.[response]
+    ALTER COLUMN dateLastUpdated DATETIME NOT NULL
+ALTER TABLE dbo.[submission]
+    ALTER COLUMN dateLastUpdated DATETIME NOT NULL
+
+GO
+
+-- Add triggers that update the dateUpdated columns automatically
+
+CREATE TRIGGER dbo.tr_user
+    ON dbo.[user]
+    FOR INSERT, UPDATE
+    AS
+BEGIN
+    UPDATE U
+    SET dateLastUpdated = GETDATE()
+    FROM dbo.[user] U
+    JOIN inserted I ON I.id = U.id
+END
+
+GO
+
+CREATE TRIGGER dbo.tr_establishment
+    ON dbo.[establishment]
+    FOR INSERT, UPDATE
+    AS
+BEGIN
+    UPDATE E
+    SET dateLastUpdated = GETDATE()
+    FROM dbo.[establishment] E
+    JOIN inserted I ON I.id = E.id
+END
+
+GO
+
+CREATE TRIGGER dbo.tr_response
+    ON dbo.[response]
+    FOR INSERT, UPDATE
+    AS
+BEGIN
+    DECLARE @now DATETIME = GETDATE()
+
+    UPDATE R
+    SET dateLastUpdated = @now
+    FROM dbo.[response] R
+    JOIN inserted I ON I.id = R.id
+
+    -- responses should update the lastUpdated time of the parent submission
+    UPDATE S
+    SET dateLastUpdated = @now
+    FROM dbo.submission S
+    JOIN inserted I ON I.submissionId = S.id
+END
+
+GO
+
+CREATE TRIGGER dbo.tr_submission
+    ON dbo.[submission]
+    FOR INSERT, UPDATE
+    AS
+BEGIN
+    UPDATE S
+    SET dateLastUpdated = GETDATE()
+    FROM dbo.[submission] S
+    JOIN inserted I ON I.id = S.id
+END
+
+GO
+
+-- Simplify GetSectionStatusesProc with the new columns
+
+ALTER PROCEDURE dbo.GetSectionStatuses @categoryId nvarchar(30), @establishmentId int
+AS
+
+SELECT
+    CurrentSubmission.sectionId,
+    CurrentSubmission.completed,
+    LastCompleteSubmission.maturity as lastMaturity,
+    CurrentSubmission.dateCreated,
+    CurrentSubmission.dateLastUpdated
+FROM Contentful.Sections CS
+-- The current submission
+CROSS APPLY (
+    SELECT TOP 1 sectionId, completed, S.id, dateCreated, dateLastUpdated
+    FROM [dbo].submission S
+    WHERE
+          CS.Id = S.sectionId
+      AND S.establishmentId = @establishmentId
+      AND S.deleted = 0
+    ORDER BY
+        S.dateCreated DESC
+) CurrentSubmission
+-- Use maturity from most recent complete submission (if there is one) so that user always sees recommendation
+OUTER APPLY (
+    SELECT TOP 1 maturity
+    FROM [dbo].submission S
+    WHERE
+          CS.Id = S.sectionId
+      AND S.establishmentId = @establishmentId
+      AND S.deleted = 0
+      AND s.completed = 1
+    ORDER BY
+        S.dateCreated DESC
+) LastCompleteSubmission
+WHERE
+    CS.CategoryId = @categoryId
