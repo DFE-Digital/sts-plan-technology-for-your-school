710082858ab2fb6247fa6a24e2bcdb9443da8789
jack-coggin <119428483+jack-coggin@users.noreply.github.com>
2024-10-01 09:19:36 +0100

    remove unused variable

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/interstitial-page.cy.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/pages/interstitial-page.cy.js
@@ -1,14 +1,8 @@
 describe("Interstitial page", () => {
-  // eslint-disable-next-line no-unused-vars
-  let url;
 
   beforeEach(() => {
     cy.loginWithEnv("/self-assessment");
     cy.clickFirstSection();
-
-    cy.url().then((currentUrl) => {
-      url = currentUrl;
-    });
 
     cy.injectAxe();
   });
