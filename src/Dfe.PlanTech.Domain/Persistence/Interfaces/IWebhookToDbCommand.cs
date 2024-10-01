441550f12ad4a8b489b2e085e0a14c1bdeeba8df
Jim Washbrook <jim.washbrook@and.digital>
2024-10-01 09:57:29 +0100

    chore: formatting

--- src/Dfe.PlanTech.Domain/Persistence/Interfaces/IWebhookToDbCommand.cs
+++ src/Dfe.PlanTech.Domain/Persistence/Interfaces/IWebhookToDbCommand.cs
@@ -1,20 +1,19 @@
 using Dfe.PlanTech.Domain.ServiceBus.Models;
 
-namespace Dfe.PlanTech.Domain.Persistence.Interfaces
+namespace Dfe.PlanTech.Domain.Persistence.Interfaces;
+
+/// <summary>
+/// Processes Contentful webhook payloads, and saves them to the DB
+/// </summary>
+public interface IWebhookToDbCommand
 {
     /// <summary>
-    /// Processes Contentful webhook payloads, and saves them to the DB
+    /// Validates, maps, and handles CRUD operations of a Contentful webhook payload to the DB
     /// </summary>
-    public interface IWebhookToDbCommand
-    {
-        /// <summary>
-        /// Validates, maps, and handles CRUD operations of a Contentful webhook payload to the DB
-        /// </summary>
-        /// <param name="subject">The CMS event <see cref="Caching.Enums.CmsEvent"/></param>
-        /// <param name="body">The CMS webhook payload (JSON string)</param>
-        /// <param name="id">The queue message ID</param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        public Task<IServiceBusResult> ProcessMessage(string subject, string body, string id, CancellationToken cancellationToken);
-    }
+    /// <param name="subject">The CMS event <see cref="Caching.Enums.CmsEvent"/></param>
+    /// <param name="body">The CMS webhook payload (JSON string)</param>
+    /// <param name="id">The queue message ID</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public Task<IServiceBusResult> ProcessMessage(string subject, string body, string id, CancellationToken cancellationToken);
 }
