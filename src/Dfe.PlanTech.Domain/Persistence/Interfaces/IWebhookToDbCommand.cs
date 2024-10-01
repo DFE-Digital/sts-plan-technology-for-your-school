2fae55772002b70e47ce081bf151f696d22f3698
Jim Washbrook <jim.washbrook@and.digital>
2024-10-01 09:57:14 +0100

    chore: Finish XML doc

--- src/Dfe.PlanTech.Domain/Persistence/Interfaces/IWebhookToDbCommand.cs
+++ src/Dfe.PlanTech.Domain/Persistence/Interfaces/IWebhookToDbCommand.cs
@@ -8,11 +8,11 @@ namespace Dfe.PlanTech.Domain.Persistence.Interfaces
     public interface IWebhookToDbCommand
     {
         /// <summary>
-        /// 
+        /// Validates, maps, and handles CRUD operations of a Contentful webhook payload to the DB
         /// </summary>
-        /// <param name="subject"></param>
-        /// <param name="body"></param>
-        /// <param name="id"></param>
+        /// <param name="subject">The CMS event <see cref="Caching.Enums.CmsEvent"/></param>
+        /// <param name="body">The CMS webhook payload (JSON string)</param>
+        /// <param name="id">The queue message ID</param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
         public Task<IServiceBusResult> ProcessMessage(string subject, string body, string id, CancellationToken cancellationToken);
