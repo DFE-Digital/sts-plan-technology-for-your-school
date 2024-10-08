ef2c6fb828e8de5000ae11bf06e91a6e54a27211
gilaineyo <gilaineyoung@gmail.com>
2024-10-08 09:58:00 +0100

    test: adds global error handler to preserve logs and handle uncaught exceptions that end journeys

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/support/e2e.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/support/e2e.js
@@ -1,2 +1,8 @@
 import "./commands.js";
 import 'cypress-axe';
+
+Cypress.on("fail", (e, runnable) => {
+    cy.log("runnable", runnable.title);
+    cy.log("message", e.message);
+    cy.log("error", e);
+});
