6f3e138ed5451317c77da8a2883998ad519c66dc
gilaineyo <gilaineyoung@gmail.com>
2024-10-08 17:22:45 +0100

    test: adds hooks for login and determining section status and skips sections in progress to prevent failures

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/dynamic-page-validator/dpv-part-2.cy.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/e2e/dynamic-page-validator/dpv-part-2.cy.js
@@ -6,15 +6,45 @@ import { validateRecommendationForMaturity, validateSections } from "./validator
 const dataMapper = new DataMapper(require('../../fixtures/contentful-data'));
 
 describe("Recommendations", { testIsolation: false }, () => {
-    it('Logs in correctly', () => {
-        cy.loginWithEnv(`/${selfAssessmentSlug}`);
+
+    before(() => {
+        cy.loginWithEnv(`${selfAssessmentSlug}`);
     });
 
     (dataMapper?.mappedSections ?? []).forEach((section) => {
-        section.getMinimumPathsForRecommendations();
-        Object.keys(section.minimumPathsForRecommendations).forEach((maturity) => {
-            validateSections(section, [section.minimumPathsForRecommendations[maturity]], dataMapper, (path) => {
-                validateRecommendationForMaturity(section, maturity, path);
+        describe(`${section.name} recommendations`, { testIsolation: false }, () => {
+
+            // Establish section status using self-assessment page tag
+            let inProgress = false;
+            before(() => {
+                cy.visit(`${selfAssessmentSlug}`)
+                cy.get("a.govuk-link")
+                    .contains(section.name.trim())
+                    .parent()
+                    .next()
+                    .within(() => {
+                        cy.get("strong.app-task-list__tag").invoke("text")
+                            .then((text) => {
+                                inProgress = text.includes("in progress");                           
+                            });
+                    });
+            });
+
+            // Skip any sections that are 'In Progress'
+            before(function () {
+                cy.wrap(null).then(() => {
+                    if (inProgress) {
+                        console.log(`Skipping all tests for section: ${section.name} (status is 'in progress'')`);
+                        this.skip();
+                    }
+                });
+            });
+
+            section.getMinimumPathsForRecommendations();
+            Object.keys(section.minimumPathsForRecommendations).forEach((maturity) => {
+                validateSections(section, [section.minimumPathsForRecommendations[maturity]], dataMapper, (path) => {
+                    validateRecommendationForMaturity(section, maturity, path);
+                });
             });
         });
     });
