45e7ca7a47c46712a42de4f4c61b4055988798e0
Jim Washbrook <jim.washbrook@and.digital>
2024-10-09 21:22:49 +0100

    fix: Move query filter back to dbcontext

--- src/Dfe.PlanTech.Infrastructure.Data/EntityTypeConfigurations/ContentComponentEntityTypeConfiguration.cs
+++ src/Dfe.PlanTech.Infrastructure.Data/EntityTypeConfigurations/ContentComponentEntityTypeConfiguration.cs
@@ -8,16 +8,11 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 namespace Dfe.PlanTech.Infrastructure.Data.EntityTypeConfigurations;
 
 [ExcludeFromCodeCoverage]
-public class ContentComponentEntityTypeConfiguration(ContentfulOptions contentfulOptions) : IEntityTypeConfiguration<ContentComponentDbEntity>
+public class ContentComponentEntityTypeConfiguration: IEntityTypeConfiguration<ContentComponentDbEntity>
 {
     public void Configure(EntityTypeBuilder<ContentComponentDbEntity> builder)
     {
         builder.ToTable("ContentComponents", CmsDbContext.Schema);
         builder.Property(e => e.Id).HasMaxLength(30);
-
-        builder.HasQueryFilter(ShouldShowEntity());
     }
-
-    private Expression<Func<ContentComponentDbEntity, bool>> ShouldShowEntity()
-    =>entity => (contentfulOptions.UsePreview || entity.Published) && !entity.Archived && !entity.Deleted;
 }
