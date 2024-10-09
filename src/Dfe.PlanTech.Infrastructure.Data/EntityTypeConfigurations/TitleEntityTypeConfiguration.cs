0b740a55038b3f46c30e819e654f66076e3d99a3
Jim Washbrook <jim.washbrook@and.digital>
2024-10-09 19:16:12 +0100

    fix: removal of content
    chore: put pagecontent ordering back
    
    chore: Exclude entity type configurations from code coverage
    
    chore: delete file
    
    chore: fix test

--- src/Dfe.PlanTech.Infrastructure.Data/EntityTypeConfigurations/TitleEntityTypeConfiguration.cs
+++ src/Dfe.PlanTech.Infrastructure.Data/EntityTypeConfigurations/TitleEntityTypeConfiguration.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
 using Dfe.PlanTech.Domain.Content.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Dfe.PlanTech.Infrastructure.Data.EntityTypeConfigurations;
 
+[ExcludeFromCodeCoverage]
 public class TitleEntityTypeConfiguration : IEntityTypeConfiguration<TitleDbEntity>
 {
     public void Configure(EntityTypeBuilder<TitleDbEntity> builder)
