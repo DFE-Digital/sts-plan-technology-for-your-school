6bf58261f7e8ce4cc84c71bedd3db6c1e4d9e7f2
jimwashbrook <131891854+jimwashbrook@users.noreply.github.com>
2024-10-14 15:01:59 +0100

    feat: Add extra options for exporting Contentful data (#825)
    * feat: add various options to export processor options
    
    chore: update contentful packages
    
    wip: add contentful management, initial thing toget editor interface
    
    * feat: add option to export various other contentful values
    
    fix: export directory
    
    * feat: add additional CLI arguments for saving file + using draft content

--- contentful/export-processor/data-tools.js
+++ contentful/export-processor/data-tools.js
@@ -3,65 +3,35 @@ import ExportContentfulData from "./exporter.js";
 import GenerateTestSuites from "./generate-test-suites.js";
 import ErrorLogger from "./errors/error-logger.js";
 import WriteUserJourneyPaths from "./write-user-journey-paths.js";
-import fs from "fs";
-import { options, DefaultOutputDirectory } from "./options.js";
-import path from "path";
-
-const getOutputDirPath = ({ outputDir, contentfulEnvironment }) => {
-  const now = new Date().toISOString().replaceAll(":", "").replaceAll("-", "").replace("T", "").split(".")[0];
-
-  const subDirectory = `${outputDir}${contentfulEnvironment}-${now}/`;
-
-  const currentDirectory = process.cwd();
-
-  return path.join(currentDirectory, subDirectory);
-};
-
-const getAndCreateOutputDir = ({ outputDir, contentfulEnvironment }) => {
-  const path = getOutputDirPath({ outputDir, contentfulEnvironment });
-
-  createFileDirectoryIfNonExistent(path);
-
-  return path;
-};
+import { options } from "./options.js";
 
 async function processContentfulData(args) {
-  if (!args.generateTestSuites && !args.exportUserJourneyPaths) {
-    console.error(`No options have been enabled`);
+  const contentfulData = await ExportContentfulData({ ...args });
+
+  if (!args.generateTestSuites && !args.exportUserJourneyPaths && !args.exportUserJourneyPaths) {
+    console.log(`No options set for using data - ending run`);
     return;
   }
 
-  const combinedOutputDir = getAndCreateOutputDir({ contentfulEnvironment: process.env.ENVIRONMENT, outputDir: args.outputDir });
-
-  const contentfulData = await ExportContentfulData({ exportDirectory: combinedOutputDir });
+  if (!contentfulData.entries || !contentfulData.contentTypes) {
+    console.error('Missing entries or content types');
+    return;
+  }
 
   const dataMapper = new DataMapper(contentfulData);
 
+  const outputDir = args.outputDir;
+
   if (args.generateTestSuites) {
-    GenerateTestSuites({ dataMapper, outputDir: combinedOutputDir });
+    GenerateTestSuites({ dataMapper, outputDir });
   }
 
   if (!!args.exportUserJourneyPaths) {
-    WriteUserJourneyPaths({ dataMapper, outputDir: combinedOutputDir, saveAllJourneys: args.saveAllJourneys });
+    WriteUserJourneyPaths({ dataMapper, outputDir, saveAllJourneys: args.saveAllJourneys });
   }
 
-  ErrorLogger.outputDir = combinedOutputDir;
+  ErrorLogger.outputDir = outputDir;
   ErrorLogger.writeErrorsToFile();
-}
-
-function createFileDirectoryIfNonExistent(outputDir) {
-  if (fs.existsSync(outputDir)) {
-    return outputDir;
-  }
-
-  try {
-    fs.mkdirSync(outputDir, { recursive: true });
-    return outputDir;
-  }
-  catch (e) {
-    console.error(`Error creating directory ${outputDir}. Defaulting to ${DefaultOutputDirectory}`, e);
-    return DefaultOutputDirectory;
-  }
 }
 
 await processContentfulData(options);
