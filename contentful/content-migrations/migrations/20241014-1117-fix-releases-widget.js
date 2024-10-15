b361446c4a1976a6a4a44ab2e2c2b724499198c4
jimwashbrook <131891854+jimwashbrook@users.noreply.github.com>
2024-10-15 13:15:49 +0100

    fix: Sidebar release widget  (#826)
    * feat: helpers for migrations
    
    chore: fix comment
    
    * fix: Releases widget editor interface
    
    fix: Correct namespace of widget, and ordering of widget IDs
    
    chore: remove unused param
    
    chore: add comment

---
+++ contentful/content-migrations/migrations/20241014-1117-fix-releases-widget.js
@@ -0,0 +1,25 @@
+const Migration = require("contentful-migration");
+const { runForEachContentType } = require("../helpers");
+
+/**
+ * 
+ * @param {Migration} migration 
+ */
+module.exports = function (migration) {
+  runForEachContentType((contentTypeName) => updateContentType(migration, contentTypeName));
+};
+
+/**
+ * 
+ * @param {Migration} migration 
+ * @param {string} contentType 
+ */
+function updateContentType(migration, contentType) {
+  var contentType = migration.editContentType(contentType);
+
+  contentType.removeSidebarWidget("builtin", "releases-widget");
+
+  //6RKxbgPghdY4llDpwCFvgR was pulled from the Contentful export - _should_ be the "workflows widget"
+  //No idea why it shows as this, but it's there, and the releases widget should be first
+  contentType.addSidebarWidget("sidebar-builtin", "releases-widget", undefined, "6RKxbgPghdY4llDpwCFvgR");
+}
