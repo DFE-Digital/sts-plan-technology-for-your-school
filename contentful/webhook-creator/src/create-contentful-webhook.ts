ca694b5a3ac11f7bc62ddcd065d26ae974fc1253
Jim Washbrook <jim.washbrook@and.digital>
2024-10-16 16:18:27 +0100

    feat: add update functionality
    chore: update readme

--- contentful/webhook-creator/src/create-contentful-webhook.ts
+++ contentful/webhook-creator/src/create-contentful-webhook.ts
@@ -1,13 +1,13 @@
-import { WebhookHeader } from "contentful-management/dist/typings/entities/webhook";
+import { WebhookHeader, WebHooks } from "contentful-management/dist/typings/entities/webhook";
 import { getOptions } from "./options";
 import { WebhookCreateRequestOptions } from "./types";
-import { createClient, CreateWebhooksProps, WebhookFilter } from "contentful-management";
+import { createClient, CreateWebhooksProps, Space, WebhookFilter } from "contentful-management";
 
 /**
  * Fetches webhook options from env variables, then creates a webhook.
  * @returns 
  */
-const createWebhook = async () => {
+const createOrUpdateWebhook = async () => {
   const { result: options, success, error } = getOptions();
 
   if (error || !success || !options) {
@@ -15,29 +15,38 @@ const createWebhook = async () => {
     return;
   }
 
-  const client = createClient(
-    {
-      accessToken: options.MANAGEMENT_TOKEN
-    },
-    {
-      type: 'plain',
-      defaults: {
-        spaceId: options.SPACE_ID,
-        environmentId: options.ENVIRONMENT_ID
-      },
-    }
-  )
+  const space = await getContentfulSpaceClient(options);
+  const webhookOptions = createWebhookOptions(options);
 
+  const webhooks = await space.getWebhooks();
+
+  const matchingWebhook = webhooks.items.find((webhook) => webhook.url == options.WEBHOOK_URL);
+
+  if (matchingWebhook) {
+    await updateWebhook(webhookOptions, matchingWebhook);
+    return;
+  }
+
+  await createWebhook(space, webhookOptions);
+}
+
+
+const getContentfulSpaceClient = (options: WebhookCreateRequestOptions): Promise<Space> => {
   try {
-    const webhookOptions = createWebhookOptions(options);
-    const result = await client.webhook.create({}, webhookOptions);
+    const client = createClient(
+      {
+        accessToken: options.MANAGEMENT_TOKEN,
+      });
 
-    console.log(`Successfully created webhook with ID: ${result.sys.id}`);
+    return client.getSpace(options.SPACE_ID);
   }
   catch (err) {
-    console.error(`Error creating webhook`, err);
+    console.error(`Error creating Contentful space client`, err);
+    throw err;
   }
 }
+
+
 
 /**
  * Creates webhook creation options where:
@@ -64,5 +73,31 @@ const createWebhookOptions = (options: WebhookCreateRequestOptions): CreateWebho
   return webhookOptions;
 }
 
-createWebhook();
+const updateWebhook = async (options: CreateWebhooksProps, webhook: WebHooks) => {
+  try {
+    webhook.headers = options.headers ?? [];
+    webhook.url = options.url;
+    webhook.name = options.name;
+    webhook.filters = options.filters ?? [];
+    webhook.topics = options.topics ?? [];
 
+    await webhook.update();
+    console.log(`Successfully updated webhook with ID: ${webhook.sys.id}`);
+  }
+  catch (err) {
+    console.error(`Error updating webhook`, err);
+  }
+}
+
+const createWebhook = async (space: Space, webhookOptions: CreateWebhooksProps) => {
+  try {
+    const result = await space.createWebhook(webhookOptions);
+    console.log(`Successfully created webhook with ID: ${result.sys.id}`);
+  }
+  catch (err) {
+    console.error(`Error creating webhook`, err);
+  }
+}
+
+
+createOrUpdateWebhook();
