50a99518cdb410e90ea290b3fe69b4b1fbe5ae6c
Jim Washbrook <jim.washbrook@and.digital>
2024-10-16 17:05:36 +0100

    feat: Execute create webhook script via TF
    chore: make webhook name a var
    
    chore: add string length check for token
    
    chore: update docs
    
    fix: use correct var
    
    fix: revert length check
    
    terraform-docs: automated action
    
    fix: resource naming
    
    terraform-docs: automated action

--- contentful/webhook-creator/src/options.ts
+++ contentful/webhook-creator/src/options.ts
@@ -1,26 +1,39 @@
-import dotenv from "dotenv"
-import { Result, WebhookCreateRequestOptions } from './types';
+import dotenv from "dotenv";
+import { Result, WebhookCreateRequestOptions } from "./types.js";
 
 const getOptions = (): Result<WebhookCreateRequestOptions> => {
   dotenv.config();
   const variables: WebhookCreateRequestOptions = {
-    MANAGEMENT_TOKEN: process.env.API_KEY ?? "",
+    MANAGEMENT_TOKEN: process.env.MANAGEMENT_TOKEN ?? "",
     ENVIRONMENT_ID: process.env.ENVIRONMENT_ID ?? "",
     SPACE_ID: process.env.SPACE_ID ?? "",
     ENVIRONMENT_NAME: process.env.ENVIRONMENT_NAME ?? "",
     WEBHOOK_API_KEY: process.env.WEBHOOK_API_KEY ?? "",
     WEBHOOK_NAME: process.env.WEBHOOK_NAME ?? "",
     WEBHOOK_URL: process.env.WEBHOOK_URL ?? "",
-  }
+  };
 
   return validateOptions(variables);
-}
+};
 
-const validateOptions = (WebhookCreateRequestOptions: WebhookCreateRequestOptions): Result<WebhookCreateRequestOptions> => {
-  const keys: (keyof WebhookCreateRequestOptions)[] = ["MANAGEMENT_TOKEN", "ENVIRONMENT_ID", "SPACE_ID", "ENVIRONMENT_NAME", "WEBHOOK_API_KEY", "WEBHOOK_NAME", "WEBHOOK_URL"];
+const validateOptions = (
+  WebhookCreateRequestOptions: WebhookCreateRequestOptions
+): Result<WebhookCreateRequestOptions> => {
+  const keys: (keyof WebhookCreateRequestOptions)[] = [
+    "MANAGEMENT_TOKEN",
+    "ENVIRONMENT_ID",
+    "SPACE_ID",
+    "ENVIRONMENT_NAME",
+    "WEBHOOK_API_KEY",
+    "WEBHOOK_NAME",
+    "WEBHOOK_URL",
+  ];
 
-  const keyIsValid = (key: keyof WebhookCreateRequestOptions): boolean => WebhookCreateRequestOptions[key] == "" || WebhookCreateRequestOptions[key] == undefined;
-  const createErrorMessage = (key: keyof WebhookCreateRequestOptions): string => `Missing ${key}`;
+  const keyIsValid = (key: keyof WebhookCreateRequestOptions): boolean =>
+    WebhookCreateRequestOptions[key] == "" ||
+    WebhookCreateRequestOptions[key] == undefined;
+  const createErrorMessage = (key: keyof WebhookCreateRequestOptions): string =>
+    `Missing ${key}`;
 
   const errors = keys.filter(keyIsValid).map(createErrorMessage);
 
@@ -28,17 +41,14 @@ const validateOptions = (WebhookCreateRequestOptions: WebhookCreateRequestOption
     return {
       success: false,
       result: undefined,
-      error: errors.join(",")
-    }
+      error: errors.join(","),
+    };
   }
 
   return {
     success: true,
-    result: WebhookCreateRequestOptions
-  }
+    result: WebhookCreateRequestOptions,
+  };
+};
 
-}
-
-export {
-  getOptions
-}
+export { getOptions };
