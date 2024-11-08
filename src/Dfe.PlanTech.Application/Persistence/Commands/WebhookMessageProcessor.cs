b9d21af46829f3db548fc8944605f8ccb954d65f
katie-gardner-AND <katharine.gardner@and.digital>
2024-11-08 10:57:59 +0000

    chore: Remove unused code for coverage

--- src/Dfe.PlanTech.Application/Persistence/Commands/WebhookMessageProcessor.cs
+++ src/Dfe.PlanTech.Application/Persistence/Commands/WebhookMessageProcessor.cs
@@ -14,10 +14,8 @@ namespace Dfe.PlanTech.Application.Persistence.Commands;
 
 /// <inheritdoc cref="IWebhookToDbCommand" />
 public class WebhookMessageProcessor(ICmsCache cache,
-                                ContentfulOptions contentfulOptions,
-                                JsonSerializerOptions jsonSerialiserOptions,
-                                ILogger<WebhookMessageProcessor> logger,
-                                IDatabaseHelper<ICmsDbContext> databaseHelper) : IWebhookToDbCommand
+                                    JsonSerializerOptions jsonSerialiserOptions,
+                                    ILogger<WebhookMessageProcessor> logger) : IWebhookToDbCommand
 {
     public async Task<IServiceBusResult> ProcessMessage(string subject, string body, string id, CancellationToken cancellationToken)
     {
@@ -39,64 +37,6 @@ public class WebhookMessageProcessor(ICmsCache cache,
         }
     }
 
-    protected virtual Task<int> ProcessEntityRemovalEvent(MappedEntity mapped, CancellationToken cancellationToken)
-    {
-        if (mapped.ExistingEntity == null)
-        {
-            throw new InvalidOperationException("ExistingEntity is null for removal event but various validations should have prevented this.");
-        }
-
-        return databaseHelper.Database.SetComponentPublishedAndDeletedStatuses(mapped.ExistingEntity, mapped.IncomingEntity.Published, mapped.IncomingEntity.Deleted, cancellationToken);
-    }
-
-    /// <summary>
-    /// Checks if the message with the given CmsEvent should be ignored based on certain conditions.
-    /// </summary>
-    /// <remarks>
-    /// If we receive a create event, we return true.
-    /// If we are NOT using preview mode (i.e. we are ignoring drafts), and the event is just a save or autosave, then return true.
-    ///
-    /// Else, we return false.
-    /// </remarks>
-    /// <param name="cmsEvent"></param>
-    /// <param name="isPublished"></param>
-    /// <returns></returns>
-    private bool ShouldIgnoreMessage(CmsEvent cmsEvent, bool isPublished)
-    {
-        switch (cmsEvent)
-        {
-            case CmsEvent.CREATE:
-                logger.LogInformation("Dropping received event {CmsEvent}", cmsEvent);
-                return true;
-            case CmsEvent.SAVE or CmsEvent.AUTO_SAVE when isPublished && !contentfulOptions.UsePreview:
-                logger.LogInformation("Received {Event} but UsePreview is {UsePreview} - dropping message", cmsEvent,
-                    contentfulOptions.UsePreview);
-                return true;
-            default:
-                return false;
-        }
-    }
-
-    /// <summary>
-    /// Retrieves the CmsEvent based on the provided subject.
-    /// </summary>
-    /// <param name="contentfulEvent">The Contentful event (from the Service Bus message subject), e.g. "ContentManagement.Entry.publish"</param>
-    /// <returns></returns>
-    /// <exception cref="CmsEventException">Exception thrown if we are unable to parse the event to a valid <see cref="CmsEvent"/></exception>
-    private CmsEvent GetCmsEvent(string contentfulEvent)
-    {
-        if (!Enum.TryParse(contentfulEvent.AsSpan()[(contentfulEvent.LastIndexOf('.') + 1)..], true,
-                out CmsEvent cmsEvent))
-        {
-            throw new CmsEventException(string.Format("Cannot parse header \"{0}\" into a valid CMS event",
-                contentfulEvent));
-        }
-
-        logger.LogInformation("CMS Event: {CmsEvent}", cmsEvent);
-
-        return cmsEvent;
-    }
-
     /// <summary>
     /// Maps the incoming message to a CmsWebHookPayload
     /// </summary>
@@ -107,44 +47,5 @@ public class WebhookMessageProcessor(ICmsCache cache,
         logger.LogInformation("Processing mesasge:\n{MessageBody}", body);
 
         return JsonSerializer.Deserialize<CmsWebHookPayload>(body, jsonSerialiserOptions) ?? throw new InvalidOperationException($"Could not serialise body to {typeof(CmsWebHookPayload)}. Body was {body}");
-    }
-
-    /// <summary>
-    /// Either add the entity to DB, or update the properties of the existing entity
-    /// </summary>
-    /// <param name="mappedEntity"></param>
-    private void UpsertEntity(MappedEntity mappedEntity)
-    {
-        if (!mappedEntity.AlreadyExistsInDatabase)
-        {
-            databaseHelper.Add(mappedEntity.IncomingEntity);
-        }
-        else if (mappedEntity.ExistingEntity != null)
-        {
-            databaseHelper.Update(mappedEntity.ExistingEntity!);
-        }
-        else
-        {
-            throw new InvalidDataException($"Entity {mappedEntity.IncomingEntity.Id} does not already exist in database, but existing entity is null");
-        }
-    }
-
-    /// <summary>
-    /// Saves changes in database, and logs information about rows changed
-    /// </summary>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
-    private async Task DbSaveChanges(CancellationToken cancellationToken)
-    {
-        long rowsChangedInDatabase = await databaseHelper.Database.SaveChangesAsync(cancellationToken);
-
-        if (rowsChangedInDatabase == 0L)
-        {
-            logger.LogWarning("No rows changed in the database!");
-        }
-        else
-        {
-            logger.LogInformation("Updated {RowsChangedInDatabase} rows in the database", rowsChangedInDatabase);
-        }
     }
 }
