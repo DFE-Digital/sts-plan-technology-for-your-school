b47fcf00c7c7cd3f4a58c44572701ef54449f7de
jack.coggin <jack.coggin@and.digital>
2024-11-08 15:27:31 +0000

    add script for importing contentful json data

---
+++ contentful/content-management/importContent.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const contentfulImport = require('contentful-import');
+const fs = require('fs');
+
+async function importContentfulData() {
+  const options = {
+    contentFile: process.env.CONTENT_FILE,
+    spaceId: process.env.SPACE_ID,
+    managementToken: process.env.MANAGEMENT_TOKEN,
+    environmentId: process.env.ENVIRONMENT,
+	  skipContentModel: process.env.SKIP_CONTENT_MODEL === 'true' ? true : false
+  };
+
+  if (!fs.existsSync(options.contentFile)) {
+    throw new Error(`File not found: ${options.contentFile}`);
+  }
+
+  try {
+    await contentfulImport(options);
+    console.log(`Import completed successfully from ${options.contentFile}`);
+  } catch (error) {
+    console.error('Error during import:', error);
+    throw error;
+  }
+}
+
+importContentfulData()
