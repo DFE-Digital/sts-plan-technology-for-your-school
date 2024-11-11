ba38a311dbf64332d2e5bcfc955411effa558859
Jim Washbrook <jim.washbrook@and.digital>
2024-11-11 15:32:28 +0000

    change(redis): Return action result if error
    chore: remove redundant thing + add test
    
    chore: Linted code for plan-technology-for-your-school.sln solution
    
    chore: tweak

--- src/Dfe.PlanTech.Domain/Caching/Models/CacheResult.cs
+++ src/Dfe.PlanTech.Domain/Caching/Models/CacheResult.cs
@@ -1,3 +1,6 @@
 namespace Dfe.PlanTech.Domain.Caching.Models;
 
-public record CacheResult<T>(bool? ExistedInCache = null, T? CacheValue = default, string? Error = null);
+public record CacheResult<T>(bool? ExistedInCache = null, T? CacheValue = default, string? Error = null)
+{
+    public bool Errored => !string.IsNullOrEmpty(Error);
+}
