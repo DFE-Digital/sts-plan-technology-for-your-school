aeac9aa03d6beafeadab7ca5f0bf0d7f20d8c2cb
jack.coggin <jack.coggin@and.digital>
2024-11-12 08:53:05 +0000

    add validation for contentful environment

--- contentful/content-management/import-content.js
+++ contentful/content-management/import-content.js
@@ -2,17 +2,18 @@ require('dotenv').config();
 
 const contentfulImport = require('contentful-import');
 const fs = require('fs');
+const validateEnvironment = require("./helpers/validate-environment");
 
 async function importContentfulData() {
   const options = {
     contentFile: process.env.CONTENT_FILE,
     spaceId: process.env.SPACE_ID,
     managementToken: process.env.MANAGEMENT_TOKEN,
-
-    // the default env is master
     environmentId: process.env.ENVIRONMENT,
 	  skipContentModel: process.env.SKIP_CONTENT_MODEL === 'true' ? true : false
   };
+
+  validateEnvironment()
 
   if (!fs.existsSync(options.contentFile)) {
     throw new Error(`File not found: ${options.contentFile}`);
