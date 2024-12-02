8a6d90581fda3a414f76b8e07f210d06c4d236d5
katie-gardner-AND <katharine.gardner@and.digital>
2024-12-02 13:02:21 +0000

    feat: Add script for removing references to invalid content in contentful

--- contentful/content-management/helpers/get-client.js
+++ contentful/content-management/helpers/get-client.js
@@ -1,6 +1,8 @@
 const contentful = require("contentful-management");
+const validateEnvironment = require("./validate-environment");
 
 module.exports = function getClient() {
+    validateEnvironment();
     return contentful.createClient(
         {
             accessToken: process.env.MANAGEMENT_TOKEN,
