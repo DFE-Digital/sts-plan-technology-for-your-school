4fcdc85a586bac5d1bc8002ab3306cea2d44fa0f
Jim Washbrook <jim.washbrook@and.digital>
2024-12-03 17:05:41 +0000

    fix: Add else statements to ensure additional validations not ran
    refactor
    
    chore: add unit test

--- src/Dfe.PlanTech.Domain/Persistence/Models/ApiAuthenticationConfiguration.cs
+++ src/Dfe.PlanTech.Domain/Persistence/Models/ApiAuthenticationConfiguration.cs
@@ -2,5 +2,9 @@ namespace Dfe.PlanTech.Domain.Persistence.Models;
 
 public record ApiAuthenticationConfiguration
 {
-    public string KeyValue { get; init; } = null!;
+    public string KeyValue { get; init; } = "";
+
+    public bool HaveApiKey => !string.IsNullOrEmpty(KeyValue);
+
+    public bool ApiKeyMatches(string key) => KeyValue.Equals(key);
 }
