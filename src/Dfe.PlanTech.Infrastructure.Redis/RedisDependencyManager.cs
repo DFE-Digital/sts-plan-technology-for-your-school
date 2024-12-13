8531c704773e8922240a9caba32c246dbad81387
Jim Washbrook <jim.washbrook@and.digital>
2024-12-13 01:48:09 +0000

    feat: Process Redis dependency setting operation in backgronud task
    tests and refactor
    
    chore: Linted code for plan-technology-for-your-school.sln solution
    
    chore: remove stopwatch
    
    chore: reawait task
    
    chore: add options to program extensions
    
    chore: amend how default options work

---
+++ src/Dfe.PlanTech.Infrastructure.Redis/RedisDependencyManager.cs
@@ -0,0 +1,72 @@
+using Dfe.PlanTech.Domain.Background;
+using Dfe.PlanTech.Domain.Content.Interfaces;
+using StackExchange.Redis;
+
+namespace Dfe.PlanTech.Infrastructure.Redis;
+
+/// <inheritdoc cref="IRedisDependencyManager"/>
+/// <param name="backgroundTaskQueue">To add dependency set operations to a queue for background processing</param>
+public class RedisDependencyManager(IBackgroundTaskQueue backgroundTaskQueue) : IRedisDependencyManager
+{
+    /// <inheritdoc cref="IRedisDependencyManager"/>
+    public Task RegisterDependenciesAsync<T>(IDatabase database, string key, T value, CancellationToken cancellationToken = default)
+    => backgroundTaskQueue.QueueBackgroundWorkItemAsync((cancellationToken) => GetAndSetDependencies(database, key, value));
+
+    /// <inheritdoc cref="IRedisDependencyManager"/>
+    public string GetDependencyKey(string contentComponentId) => $"Dependency:{contentComponentId}";
+
+    /// <summary>
+    /// Retrieves dependencies for the given <see cref="IContentComponent"/> and registers them in the Redis cache.
+    /// </summary>
+    /// <typeparam name="T">Type of the value whose dependencies are to be retrieved.</typeparam>
+    /// <param name="database">The database where dependencies are stored.</param>
+    /// <param name="key">Key for the parent of the dependencies.</param>
+    /// <param name="value">The <see cref="IContentComponent"/> parent of the dependencies.</param>
+    private async Task GetAndSetDependencies<T>(IDatabase database, string key, T value)
+    {
+        var batch = database.CreateBatch();
+        var tasks = GetDependencies(value).Select(dependency => batch.SetAddAsync(GetDependencyKey(dependency), key)).ToArray();
+        batch.Execute();
+        await Task.WhenAll(tasks);
+    }
+
+    /// <summary>
+    /// Retrieves dependencies, in the form of the Id of the <see cref="IContentComponent"/>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException">Thrown when the value object is not a <see cref="IContentComponent"/> or <see cref="IEnumerable{IContentComponent}"/> </exception>
+    private IEnumerable<string> GetDependencies<T>(T? value)
+    => value switch
+    {
+        null => [],
+        IEnumerable<IContentComponent> collection => collection.SelectMany(GetDependencies),
+        IContentComponent item => GetContentDependenciesAsync(item),
+        _ => throw new InvalidOperationException($"{value!.GetType()} is not a {typeof(IContentComponent)} or a {typeof(IEnumerable<IContentComponent>)}"),
+    };
+
+    /// <summary>
+    /// Uses reflection to check for any ContentIds within the <see cref="IContentComponent">, and returns the Id value of any found
+    /// </summary>
+    /// <param name="value"></param>
+    private IEnumerable<string> GetContentDependenciesAsync(IContentComponent value)
+    {
+        // RichText is a sub-component that doesn't have SystemDetails, exit for such types
+        if (value.Sys is null)
+            yield break;
+
+        yield return value.Sys.Id;
+        var properties = value.GetType().GetProperties();
+        foreach (var property in properties)
+        {
+            if (typeof(IContentComponent).IsAssignableFrom(property.PropertyType) || typeof(IEnumerable<IContentComponent>).IsAssignableFrom(property.PropertyType))
+            {
+                foreach (var dependency in GetDependencies(property.GetValue(value)))
+                {
+                    yield return dependency;
+                }
+            }
+        }
+    }
+}
---
+++ src/Dfe.PlanTech.Web/Background/BackgroundTaskHostedService.cs
@@ -0,0 +1,58 @@
+using Dfe.PlanTech.Domain.Background;
+
+namespace Dfe.PlanTech.Web.Background;
+
+/// <summary>
+/// Reads tasks from a <see cref="IBackgroundTaskQueue"/>, and runs them. 
+/// </summary>
+/// <param name="logger"></param>
+/// <param name="taskQueue"></param>
+public class BackgroundTaskHostedService(ILogger<BackgroundTaskHostedService> logger, IBackgroundTaskQueue taskQueue) : BackgroundService
+{
+    /// <summary>
+    /// Starts processing the queue
+    /// </summary>
+    /// <param name="stoppingToken"></param>
+    /// <returns></returns>
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        logger.LogInformation("Starting processing background tasks");
+        await BackgroundProcessing(stoppingToken);
+    }
+
+    /// <summary>
+    /// Processes the queue in a loop. Waits for a task to exist in the queue, reads it, and runs it.
+    /// </summary>
+    /// <param name="stoppingToken"></param>
+    /// <returns></returns>
+    private async Task BackgroundProcessing(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var workItem = await taskQueue.DequeueAsync(stoppingToken);
+
+            logger.LogInformation("Read item from the queue");
+
+            try
+            {
+                await workItem(stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error occurred executing {WorkItem}.", nameof(workItem));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Stops procesing of the queue
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public override async Task StopAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Stopping processing background tasks");
+
+        await base.StopAsync(cancellationToken);
+    }
+}
