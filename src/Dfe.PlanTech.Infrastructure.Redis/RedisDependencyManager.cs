268b330a2c5168f4eed405231bf2b0c188929045
katie-gardner-AND <katharine.gardner@and.digital>
2024-12-17 10:06:53 +0000

    tests: add tests for improved redis cache invalidation

--- src/Dfe.PlanTech.Infrastructure.Redis/RedisDependencyManager.cs
+++ src/Dfe.PlanTech.Infrastructure.Redis/RedisDependencyManager.cs
@@ -30,6 +30,7 @@ public class RedisDependencyManager(IBackgroundTaskQueue backgroundTaskQueue) : 
         var tasks = GetDependencies(value).Select(dependency => batch.SetAddAsync(GetDependencyKey(dependency), key, CommandFlags.FireAndForget)).ToArray();
         if (tasks.Length == 0)
         {
+            // If the value has no dependencies (is empty) it should be invalidated when new content comes in
             tasks = tasks.Append(batch.SetAddAsync(EmptyCollectionDependencyKey, key, CommandFlags.FireAndForget)).ToArray();
         }
         batch.Execute();
