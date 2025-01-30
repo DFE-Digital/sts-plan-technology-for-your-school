d17dc157c012d2bed02827132fc72f53ad85df2c
Kenny Lawrie <kenny.lawrie@airelogic.com>
2025-01-30 10:17:58 +0000

    added update and tests

--- src/Dfe.PlanTech.Application/Extensions/StringExtensions.cs
+++ src/Dfe.PlanTech.Application/Extensions/StringExtensions.cs
@@ -1,7 +1,10 @@
+using System.Net;
+
 namespace Dfe.PlanTech;
 
 public static class StringExtensions
 {
+    public const string NonBreakingHyphen = "&#8209;";
     public static string FirstCharToUpper(this string input)
     {
         if (string.IsNullOrEmpty(input))
@@ -11,5 +14,14 @@ public static class StringExtensions
         Span<char> destination = stackalloc char[1];
         input.AsSpan(0, 1).ToUpperInvariant(destination);
         return $"{destination}{input.AsSpan(1)}";
+    }
+
+    public static string? UseNonBreakingHyphenAndHtmlDecode(this string text)
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            return text;
+        }
+        return WebUtility.HtmlDecode(text.Replace("-", NonBreakingHyphen));
     }
 }
---
+++ tests/Dfe.PlanTech.Web.UnitTests/Helpers/StringExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace Dfe.PlanTech.Web.UnitTests.Helpers
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData("Single test-", "Single test‑")]
+        [InlineData("Single-test", "Single‑test")]
+        [InlineData("This is -a-test-", "This is ‑a‑test‑")]
+        [InlineData("This is-a-test", "This is‑a‑test")]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        public void CheckHyphensConvertedCorrectly(string inputText, string expectedText)
+        {
+            var result = StringExtensions.UseNonBreakingHyphenAndHtmlDecode(inputText);
+
+            Assert.Equal(expectedText, result);
+        }
+    }
+}
