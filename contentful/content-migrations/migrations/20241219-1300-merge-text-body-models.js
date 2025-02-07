82a2810990f6c8403068fb13df837791683db3c7
Jag Nahl <jag.nahl@airelogic.com>
2025-02-07 09:39:05 +0000

    added check if fields are missing or undefined

--- contentful/content-migrations/migrations/20241219-1300-merge-text-body-models.js
+++ contentful/content-migrations/migrations/20241219-1300-merge-text-body-models.js
@@ -13,6 +13,13 @@ module.exports = function (migration) {
             return uuidv4();
         },
         transformEntryForLocale: function (fromFields, currentLocale) {
+            const internalName = fromFields.InternalName?.[currentLocale];
+            const richText = fromFields.RichText?.[currentLocale];
+            
+            if (!internalName && !richText) {
+                // Skip transformation if both fields are missing or undefined
+                return null;
+            }
             return {
                 internalName: fromFields.InternalName[currentLocale],
                 richText: fromFields.RichText[currentLocale],
