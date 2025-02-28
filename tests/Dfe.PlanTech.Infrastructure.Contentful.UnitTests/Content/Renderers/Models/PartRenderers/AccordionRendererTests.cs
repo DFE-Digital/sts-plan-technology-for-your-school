e72835fe37c333d22514b0e6ed0b56ed1fb290c0
Kenny Lawrie <kenny.lawrie@airelogic.com>
2025-02-28 15:58:45 +0000

    added more tests

--- tests/Dfe.PlanTech.Infrastructure.Contentful.UnitTests/Content/Renderers/Models/PartRenderers/AccordionRendererTests.cs
+++ tests/Dfe.PlanTech.Infrastructure.Contentful.UnitTests/Content/Renderers/Models/PartRenderers/AccordionRendererTests.cs
@@ -32,7 +32,7 @@ public class AccordionRendererTests
     }
 
     [Fact]
-    public void CheckContentCorrectlyRenderedByAccordion()
+    public void CheckSingleContentCorrectlyRenderedByAccordion()
     {
         var renderer = new AccordionComponent(_logger);
 
@@ -42,12 +42,49 @@ public class AccordionRendererTests
         {
             NodeType = RichTextNodeType.EmbeddedEntryBlock.ToString(),
             Data = new RichTextContentSupportData()
+            {
+                Target = new RichTextContentData()
+                {
+                    Content = new List<RichTextContentData>()
+                    {
+                        new RichTextContentData()
+                        {
+                            InternalName = "Internal Name 1",
+                            Title = "Title 1",
+                            SummaryLine = "Summary Line 1",
+                            RichText = new RichTextContent()
+                            {
+                                Description = "This is just a description and has no render content"
+                            }
+                        },
+                    }
+                }
+            }
         };
 
         var rendererCollection = new AccordionComponent(_logger);
 
         var result = renderer.AddHtml(content, rendererCollection, stringBuilder);
 
-        Assert.Equal(result, stringBuilder);
+        Assert.Equal(result.ToString(), GetStandardStringBuilderOutput().ToString());
+    }
+
+    private StringBuilder GetStandardStringBuilderOutput()
+    {
+        var stringBuilder = new StringBuilder();
+        stringBuilder.AppendLine($"<div class=\"govuk-accordion\" data-module=\"govuk-accordion\" id=\"accordion-Internal Name 1\">");
+        stringBuilder.Append("<div class=\"govuk-accordion__section\">");
+        stringBuilder.Append("<div class=\"govuk-accordion__section-header\">");
+        stringBuilder.Append("<h2 class=\"govuk-accordion__section-heading\">");
+        stringBuilder.Append("<span class=\"govuk-accordion__section-button\" id=\"Internal Name 1-heading\">");
+        stringBuilder.Append("Title 1");
+        stringBuilder.Append("</span></h2>");
+        stringBuilder.Append("<div class=\"govuk-accordion__section-summary govuk-body\" id=\"Internal Name 1-summary\">");
+        stringBuilder.Append("Summary Line 1");
+        stringBuilder.Append("</div></div>");
+        stringBuilder.Append("<div id=\"Internal Name 1-content\" class=\"govuk-accordion__section-content\">");
+        stringBuilder.Append("</div></div>");
+        stringBuilder.Append("</div>");
+        return stringBuilder;
     }
 }
