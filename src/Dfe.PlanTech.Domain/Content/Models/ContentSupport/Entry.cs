ce1ddf614a0112155cfbae33517461e57f5fea87
Kenny Lawrie <kenny.lawrie@airelogic.com>
2025-03-06 15:58:20 +0000

    removed content support pages and models

--- src/Dfe.PlanTech.Domain/Content/Models/ContentSupport/Entry.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-namespace Dfe.PlanTech.Domain.Content.Models.ContentSupport;
-
-[ExcludeFromCodeCoverage]
-public class Entry : ContentBase
-{
-    public string JumpIdentifier { get; set; } = null!;
-}
