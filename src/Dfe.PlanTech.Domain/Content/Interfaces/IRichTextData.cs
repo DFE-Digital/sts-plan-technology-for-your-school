7d00560408cdf32a1357baca0e1b742f61d9c710
Kenny Lawrie <kenny.lawrie@airelogic.com>
2025-03-07 10:40:18 +0000

    re-added - deleted my mistake

---
+++ src/Dfe.PlanTech.Domain/Content/Interfaces/IRichTextData.cs
@@ -0,0 +1,13 @@
+﻿namespace Dfe.PlanTech.Domain.Content.Interfaces;
+
+/// <summary>
+/// Data for a RichText section
+/// </summary>
+/// <inheritdoc/>
+public interface IRichTextData
+{
+    /// <summary>
+    /// URL for a link
+    /// </summary>
+    public string? Uri { get; init; }
+}
