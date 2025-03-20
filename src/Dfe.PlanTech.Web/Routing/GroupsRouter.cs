185a0c8c33dd79d91352beb53a025db70204838e
gilaineyo <gilaine.young@education.gov.uk>
2025-03-20 15:29:08 +0000

    feat: routing for school selection

---
+++ src/Dfe.PlanTech.Web/Routing/GroupsRouter.cs
@@ -0,0 +1,34 @@
+﻿using Dfe.PlanTech.Domain.Content.Models;
+using Dfe.PlanTech.Domain.Establishments.Models;
+using Dfe.PlanTech.Web.Controllers;
+using Dfe.PlanTech.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfe.PlanTech.Web.Routing
+{
+    public class GroupsRouter : IGroupsRouter
+    {
+        private const string selectASchoolViewName = "~/Views/Groups/GroupsSelectSchool.cshtml";
+        private const string schoolDashboardViewName = "~/Views/Groups/GroupsSchoolDashboard.cshtml";
+
+        public IActionResult GetSchoolDashboard(GroupsController controller, CancellationToken cancellationToken)
+        {
+            var groupsViewModel = new GroupsSelectorViewModel();
+
+            return PageRedirecter.RedirectToSelfAssessment(controller);
+        }
+
+        public IActionResult GetSelectASchool(string groupName, List<EstablishmentLink> schools, Title title, List<ContentComponent> content, GroupsController controller, CancellationToken cancellationToken)
+        {
+            var groupsViewModel = new GroupsSelectorViewModel()
+            {
+                GroupName = groupName,
+                GroupEstablishments = schools,
+                Title = title,
+                Content = content
+            };
+
+            return controller.View(selectASchoolViewName, groupsViewModel);
+        }
+    }
+}
