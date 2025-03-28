6ba665b5100557225236cc7a18081fa6b9b12bf0
adimicoli-airelogic <Alessandro.DI-MICOLI@education.gov.uk>
2025-03-28 10:20:18 +0000

    update

--- terraform/container-app/locals.tf
+++ terraform/container-app/locals.tf
@@ -54,7 +54,7 @@ locals {
   az_sql_max_pool_size          = var.az_sql_max_pool_size
   az_sql_max_size_gb            = local.az_sql_sku == "Basic" ? null : 10
   
-  mssql_firewall_ipv4_allow_list = var.mssql_firewall_ipv4_allow_list
+  mssql_firewall_ipv4_allow_list = var.mssql_firewall_ipv4_allow_list[var.environment]
 
   az_sql_vnet = {
     dns_zone_name = "privatelink.database.windows.net"
--- terraform/container-app/terraform.tfvars
+++ terraform/container-app/terraform.tfvars
@@ -1,4 +1,6 @@
 mssql_firewall_ipv4_allow_list = {
-  dev_1 = { start_ip_range = "192.168.1.1", end_ip_range = "192.168.1.1" }
-  dev_2 = { start_ip_range = "192.168.1.2", end_ip_range = "192.168.1.2" }
+    Dev = {
+        dev_1 = { start_ip_range = "192.168.1.1", end_ip_range = "192.168.1.1" }
+        dev_2 = { start_ip_range = "192.168.1.2", end_ip_range = "192.168.1.2" }
+    }
 }
