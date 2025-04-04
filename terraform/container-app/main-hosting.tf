abfe9744ddc09f1b8be88c16bdc3303254c74eab
adimicoli-airelogic <Alessandro.DI-MICOLI@education.gov.uk>
2025-04-04 16:47:16 +0100

    update

--- terraform/container-app/main-hosting.tf
+++ terraform/container-app/main-hosting.tf
@@ -66,6 +66,6 @@ module "main_hosting" {
   # Deploy an Azure Network Watcher #
   ###################################
   enable_network_watcher = false
-  # network_watcher_flow_log_retention = 90 # Days
+  enable_network_watcher_traffic_analytics = false
 
 }
