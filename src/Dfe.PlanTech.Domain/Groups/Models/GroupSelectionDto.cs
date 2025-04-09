d6257ba2952a58ffee4ede6b5938017809cad8f6
gilaineyo <gilaine.young@education.gov.uk>
2025-04-09 20:14:00 +0100

    feat: add group selection table and sproc to database to record selected school

---
+++ src/Dfe.PlanTech.Domain/Groups/Models/GroupSelectionDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Dfe.PlanTech.Domain.Groups.Interfaces
+{
+    public class GroupSelectionDto
+    {
+        public int UserId { get; set; }
+
+        public int UserEstablishment { get; set; }
+
+        public int SelectedEstablishmentId { get; set; }
+
+        public string? SelectedEstablishmentName { get; set; }
+    }
+}
