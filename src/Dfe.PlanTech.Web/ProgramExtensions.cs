f8d98f6a09eea296ec5efbbe178683b7d969a466
gilaineyo <gilaine.young@education.gov.uk>
2025-04-09 20:15:03 +0100

    feat: add group selection command to services

--- src/Dfe.PlanTech.Web/ProgramExtensions.cs
+++ src/Dfe.PlanTech.Web/ProgramExtensions.cs
@@ -4,6 +4,7 @@ using Dfe.PlanTech.Application.Caching.Interfaces;
 using Dfe.PlanTech.Application.Caching.Models;
 using Dfe.PlanTech.Application.Content.Queries;
 using Dfe.PlanTech.Application.Cookie.Service;
+using Dfe.PlanTech.Application.Groups.Commands;
 using Dfe.PlanTech.Application.Persistence.Interfaces;
 using Dfe.PlanTech.Application.Questionnaire.Queries;
 using Dfe.PlanTech.Application.Responses.Commands;
@@ -21,6 +22,7 @@ using Dfe.PlanTech.Domain.Content.Models.Options;
 using Dfe.PlanTech.Domain.Cookie;
 using Dfe.PlanTech.Domain.Cookie.Interfaces;
 using Dfe.PlanTech.Domain.Database;
+using Dfe.PlanTech.Domain.Groups.Interfaces;
 using Dfe.PlanTech.Domain.Persistence.Models;
 using Dfe.PlanTech.Domain.Questionnaire.Interfaces;
 using Dfe.PlanTech.Domain.Submissions.Interfaces;
@@ -158,6 +160,7 @@ public static class ProgramExtensions
         services.AddTransient<IRecordUserSignInCommand, RecordUserSignInCommand>();
         services.AddTransient<ISubmitAnswerCommand, SubmitAnswerCommand>();
         services.AddTransient<IDeleteCurrentSubmissionCommand, DeleteCurrentSubmissionCommand>();
+        services.AddTransient<IRecordGroupSelectionCommand, RecordGroupSelectionCommand>();
 
         return services;
     }
