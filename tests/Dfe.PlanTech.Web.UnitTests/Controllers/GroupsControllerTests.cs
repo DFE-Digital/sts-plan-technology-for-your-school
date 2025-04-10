e848f86ffed6e81b65eb4381ee24aa74647cfe0d
gilaineyo <gilaine.young@education.gov.uk>
2025-04-10 12:34:02 +0100

    tests: testing cleanup

--- tests/Dfe.PlanTech.Web.UnitTests/Controllers/GroupsControllerTests.cs
+++ tests/Dfe.PlanTech.Web.UnitTests/Controllers/GroupsControllerTests.cs
@@ -1,14 +1,14 @@
-﻿using Dfe.PlanTech.Domain.Content.Interfaces;
+﻿using Dfe.PlanTech.Application.Constants;
+using Dfe.PlanTech.Domain.Content.Interfaces;
 using Dfe.PlanTech.Domain.Establishments.Models;
+using Dfe.PlanTech.Domain.Groups.Interfaces;
 using Dfe.PlanTech.Domain.Submissions.Interfaces;
 using Dfe.PlanTech.Domain.Users.Interfaces;
 using Dfe.PlanTech.Web.Controllers;
 using Dfe.PlanTech.Web.Middleware;
 using Dfe.PlanTech.Web.Models;
 using Dfe.PlanTech.Web.UnitTests.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
 using Xunit;
@@ -24,6 +24,7 @@ namespace Dfe.PlanTech.Web.UnitTests.Controllers
         private readonly IComponentBuilder _componentBuilder;
         private readonly IGetEstablishmentIdQuery _getEstablishmentIdQuery;
         private readonly IGetSubmissionStatusesQuery _getSubmissionStatusesQuery;
+        private readonly IGetGroupSelectionQuery _getGroupSelectionQuery;
 
         public GroupsControllerTests()
         {
@@ -34,6 +35,7 @@ namespace Dfe.PlanTech.Web.UnitTests.Controllers
             _componentBuilder = Substitute.For<IComponentBuilder>();
             _getEstablishmentIdQuery = Substitute.For<IGetEstablishmentIdQuery>();
             _getSubmissionStatusesQuery = Substitute.For<IGetSubmissionStatusesQuery>();
+            _getGroupSelectionQuery = Substitute.For<IGetGroupSelectionQuery>();
         }
 
         [Fact]
@@ -56,40 +58,21 @@ namespace Dfe.PlanTech.Web.UnitTests.Controllers
             Assert.Equal("School 1", model.GroupEstablishments.First().EstablishmentName);
         }
 
-        [Fact]
-        public void SelectSchool_Sets_TempData_And_Redirects()
-        {
-            var controller = new GroupsController(_logger, _exceptionHandler, _user, _getEstablishmentIdQuery, _getSubmissionStatusesQuery);
-            var tempData = new TempDataDictionary(new DefaultHttpContext(), Substitute.For<ITempDataProvider>());
-            controller.TempData = tempData;
-
-            var schoolUrn = "1";
-            var schoolName = "School 1";
-
-            var result = controller.SelectSchool(schoolUrn, schoolName);
-
-            Assert.Equal(schoolUrn, controller.TempData["SelectedSchoolUrn"]);
-            Assert.Equal(schoolName, controller.TempData["SelectedSchoolName"]);
-            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
-            Assert.Equal("GetSchoolDashboardView", redirectResult.ActionName);
-        }
 
         [Fact]
         public async Task GetSchoolDashboardView_Returns_Correct_View()
         {
             var controller = new GroupsController(_logger, _exceptionHandler, _user, _getEstablishmentIdQuery, _getSubmissionStatusesQuery);
-            var tempData = new TempDataDictionary(new DefaultHttpContext(), Substitute.For<ITempDataProvider>());
-            controller.TempData = tempData;
 
-            controller.TempData["SelectedSchoolUrn"] = "1";
-            controller.TempData["SelectedSchoolName"] = "School 1";
             _user.GetOrganisationData().Returns(new EstablishmentDto { OrgName = "Test Group" });
             _getEstablishmentIdQuery.GetEstablishmentId("1").Returns(1);
 
             var pageContent = _componentBuilder.BuildPage();
-            _getPageQuery.GetPageBySlug("dashboard", Arg.Any<CancellationToken>()).Returns(pageContent);
+            _getPageQuery.GetPageBySlug(UrlConstants.GroupsDashboardSlug, Arg.Any<CancellationToken>()).Returns(pageContent);
 
-            var result = await controller.GetSchoolDashboardView(_getPageQuery, CancellationToken.None);
+            // TODO replace TempData with query
+
+            var result = await controller.GetSchoolDashboardView(_getPageQuery, _getGroupSelectionQuery, CancellationToken.None);
 
             var viewResult = Assert.IsType<ViewResult>(result);
             var model = Assert.IsType<GroupsSchoolDashboardViewModel>(viewResult.Model);
