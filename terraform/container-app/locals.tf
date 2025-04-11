3f93542aa274d4718281fe376b855b9bb740a173
Alessandro Di Micoli <Alessandro.DI-MICOLI@education.gov.uk>
2025-04-11 15:18:43 +0100

    Update

--- terraform/container-app/locals.tf
+++ terraform/container-app/locals.tf
@@ -16,7 +16,6 @@ locals {
     "Environment"      = var.az_tag_environment,
     "Service Offering" = var.az_tag_product,
     "Product"          = var.az_tag_product
-    "Terraform"        = "true"
   }
 
   #################
