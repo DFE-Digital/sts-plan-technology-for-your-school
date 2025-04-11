70ea5dff29389c8e9859bef286d3b9d98517cb7e
rian.thwaite <rian.thwaite@airelogic.com>
2025-04-11 17:05:05 +0100

    254054 Added check for MAT users to redirect them to school selection

---
+++ src/Dfe.PlanTech.Domain/Establishments/Models/OrganisationCategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Dfe.PlanTech.Domain.Establishments.Models
+{
+    public class OrganisationCategoryDto
+    {
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+    }
+}
