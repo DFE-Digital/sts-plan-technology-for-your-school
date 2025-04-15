5d7d6515ad979895836b5876cdbed76a67ca845f
jag-nahl-airelogic <jag.nahl@airelogic.com>
2025-04-15 18:14:51 +0100

    Amended text on continue button as it has changed in contentful

--- tests/Dfe.PlanTech.Web.E2ETests/cypress/commands/helpers.js
+++ tests/Dfe.PlanTech.Web.E2ETests/cypress/commands/helpers.js
@@ -2,7 +2,7 @@
  * Click first section link on self-assessment page
  */
 export const clickContinueButton = () => {
-  cy.get('a.govuk-button').contains('Continue').click();
+  cy.get('a.govuk-button').contains('Start self-assessment').click();
 }
 
 Cypress.Commands.add("clickContinueButton", clickContinueButton);
