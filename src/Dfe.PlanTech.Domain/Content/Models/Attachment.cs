950db978d536777c5561bc0c2b49944667fd0ed7
Drew MORGAN <Drew.MORGAN@EDUCATION.GOV.UK>
2025-04-17 11:27:33 +0100

    Fix linter warnings

--- src/Dfe.PlanTech.Domain/Content/Models/Attachment.cs
+++ src/Dfe.PlanTech.Domain/Content/Models/Attachment.cs
@@ -1,7 +1,5 @@
-﻿using Dfe.PlanTech.Domain.Content.Interfaces;
+﻿namespace Dfe.PlanTech.Domain.Content.Models;
 
-namespace Dfe.PlanTech.Domain.Content.Models;
-
-public class Attachment : RichTextContentData, IContentComponent, IHasSlug, IRichTextData
+public class Attachment : RichTextContentData
 {
 }
