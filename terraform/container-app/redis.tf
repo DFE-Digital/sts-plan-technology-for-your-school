96be48a45a9f27b9a9dc4967715f280769caf6ba
Christian Parker <christian.parker@airelogic.com>
2025-07-14 11:41:16 +0100

    apply formatting

--- terraform/container-app/redis.tf
+++ terraform/container-app/redis.tf
@@ -1,11 +1,11 @@
 resource "azurerm_redis_cache" "redis" {
-  name                = local.resource_prefix
-  location            = local.azure_location
-  resource_group_name = local.resource_group_name
-  capacity            = local.redis_capacity
-  family              = local.redis_family
-  sku_name            = local.redis_sku_name
-  minimum_tls_version = local.redis_tls_version
-  tags                = local.tags
+  name                          = local.resource_prefix
+  location                      = local.azure_location
+  resource_group_name           = local.resource_group_name
+  capacity                      = local.redis_capacity
+  family                        = local.redis_family
+  sku_name                      = local.redis_sku_name
+  minimum_tls_version           = local.redis_tls_version
+  tags                          = local.tags
   public_network_access_enabled = false
 }
