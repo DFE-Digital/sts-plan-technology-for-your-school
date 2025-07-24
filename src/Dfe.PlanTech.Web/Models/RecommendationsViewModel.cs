9e9372efb16c71254c62736ae3817075c1f60913
gilaineyo <gilaine.young@education.gov.uk>
2025-07-24 13:25:44 +0100

    story: remove redundant code from RecommendationsViewModel

--- src/Dfe.PlanTech.Web/Models/RecommendationsViewModel.cs
+++ src/Dfe.PlanTech.Web/Models/RecommendationsViewModel.cs
@@ -11,22 +11,11 @@ public class RecommendationsViewModel
 
     public string SectionSlug { get; init; } = null!;
 
-//    public RecommendationIntro Intro { get; init; } = null!;
-
     public List<RecommendationChunk> Chunks { get; init; } = null!;
 
     public string? LatestCompletionDate { get; init; } = null!;
 
-//    public IEnumerable<IHeaderWithContent> AllContent => GetAllContent();
-
     public string? Slug { get; init; } = null;
 
-    //private IEnumerable<IHeaderWithContent> GetAllContent()
-    //{
-    //    foreach (var chunk in Chunks)
-    //    {
-    //        yield return chunk;
-    //    }
-    //}
     public IEnumerable<QuestionWithAnswer> SubmissionResponses { get; init; } = null!;
 }
