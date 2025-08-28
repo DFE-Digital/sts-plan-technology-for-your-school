b1df522140977745681927669c8c86795f752ded
Drew MORGAN <Drew.MORGAN@EDUCATION.GOV.UK>
2025-08-28 16:14:24 +0100

    Add unit tests for Core.Contentful queries

--- src/Dfe.PlanTech.Core/Exceptions/GetEntriesException.cs
+++ src/Dfe.PlanTech.Core/Exceptions/GetEntriesException.cs
@@ -1,8 +1,10 @@
-﻿using System.Runtime.Serialization;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
 
 namespace Dfe.PlanTech.Core.Exceptions;
 
 [Serializable]
+[ExcludeFromCodeCoverage]
 public class GetEntriesException : Exception
 {
     public GetEntriesException(string? message) : base(message)
--- src/Dfe.PlanTech.Core/Exceptions/MissingServiceException.cs
+++ src/Dfe.PlanTech.Core/Exceptions/MissingServiceException.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Dfe.PlanTech.Core.Exceptions;
 
+[ExcludeFromCodeCoverage]
 public class MissingServiceException(string message) : Exception(message)
 {
     public MissingServiceException(Type missingService) : this($"Missing service {missingService.Name}") { }
