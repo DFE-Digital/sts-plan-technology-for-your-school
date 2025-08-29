e6e778708f20df0ea9b64cc5a5e75fdfa47e747e
gilaineyo <gilaine.young@education.gov.uk>
2025-08-29 17:34:05 +0100

    tests: authcontroller unit tests

---
+++ tests/Dfe.PlanTech.Web.UnitTests/Controllers/AuthControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Dfe.PlanTech.Web.Controllers;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfe.PlanTech.Web.Tests.Controllers
+{
+    public class AuthControllerTests
+    {
+        [Fact]
+        public void SignOut_ReturnsSignOutResult_WithCorrectSchemes()
+        {
+            var controller = new AuthController();
+
+            var result = controller.SignOut();
+
+            var signOutResult = Assert.IsType<SignOutResult>(result);
+            Assert.Contains(OpenIdConnectDefaults.AuthenticationScheme, signOutResult.AuthenticationSchemes);
+            Assert.Contains(CookieAuthenticationDefaults.AuthenticationScheme, signOutResult.AuthenticationSchemes);
+        }
+    }
+}
