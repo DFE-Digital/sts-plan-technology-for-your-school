4167f7c7528678dcbe8fd916fbdcdc198430e9d3
Roger Howell <roger.howell@education.gov.uk>
2025-10-01 08:56:26 +0100

    no-op commit

--- src/Dfe.PlanTech.Web/Program.cs
+++ src/Dfe.PlanTech.Web/Program.cs
@@ -7,6 +7,8 @@ using Dfe.PlanTech.Web.Attributes;
 using Dfe.PlanTech.Web.Middleware;
 using GovUk.Frontend.AspNetCore;
 
+// no-op commit
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddResponseCompression(options =>
