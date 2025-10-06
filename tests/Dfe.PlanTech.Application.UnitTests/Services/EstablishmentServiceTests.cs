b7f22a13a0b6e68d075e2ceeb5b5620327125116
Drew MORGAN <Drew.MORGAN@EDUCATION.GOV.UK>
2025-10-06 12:22:55 +0100

    Add unit test for additional coverage

--- tests/Dfe.PlanTech.Application.UnitTests/Services/EstablishmentServiceTests.cs
+++ tests/Dfe.PlanTech.Application.UnitTests/Services/EstablishmentServiceTests.cs
@@ -6,7 +6,6 @@ using Dfe.PlanTech.Core.Exceptions;
 using Dfe.PlanTech.Core.Models;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
-using NSubstitute.ExceptionExtensions;
 
 namespace Dfe.PlanTech.Application.UnitTests.Services;
 
@@ -128,7 +127,8 @@ public class EstablishmentServiceTests
         var groupEstablishments = new List<SqlEstablishmentLinkDto>
         {
             new SqlEstablishmentLinkDto { Id = groupEstablishmentId, Urn = "URN-A", EstablishmentName = "A" },
-            new SqlEstablishmentLinkDto { Id = 102, Urn = "URN-B", EstablishmentName = "B" }
+            new SqlEstablishmentLinkDto { Id = 102, Urn = "URN-B", EstablishmentName = "B" },
+            new SqlEstablishmentLinkDto { Id = 103, Urn = "URN-C", EstablishmentName = "C" }
         };
         _establishmentWorkflow.GetGroupEstablishments(groupEstablishmentId).Returns(groupEstablishments);
 
@@ -137,7 +137,8 @@ public class EstablishmentServiceTests
             new SqlEstablishmentDto { Id = 1001, EstablishmentRef = "URN-A", OrgName = "A" },
             new SqlEstablishmentDto { Id = 1002, EstablishmentRef = "URN-B", OrgName = "B" }
         };
-        _establishmentWorkflow.GetEstablishmentsByReferencesAsync(Arg.Is<IEnumerable<string>>(us => us.SequenceEqual(new[] { "URN-A", "URN-B" })))
+
+        _establishmentWorkflow.GetEstablishmentsByReferencesAsync(Arg.Is<IEnumerable<string>>(us => us.SequenceEqual(new[] { "URN-A", "URN-B", "URN-C" })))
                               .Returns(establishments);
 
         _submissionWorkflow.GetSectionStatusesAsync(1001, Arg.Is<IEnumerable<string>>(ids => ids.SequenceEqual(new[] { "S1", "S2" })))
@@ -148,7 +149,7 @@ public class EstablishmentServiceTests
 
         _submissionWorkflow.GetSectionStatusesAsync(1002, Arg.Any<IEnumerable<string>>())
                            .Returns([
-                               new SqlSectionStatusDto { Completed = false },
+                               new SqlSectionStatusDto { Completed = true, LastCompletionDate = DateTime.UtcNow },
                                new SqlSectionStatusDto { Completed = false }
                            ]);
 
@@ -160,8 +161,10 @@ public class EstablishmentServiceTests
         // Assert
         var a = result.Single(x => x.Urn == "URN-A");
         var b = result.Single(x => x.Urn == "URN-B");
+        var c = result.Single(x => x.Urn == "URN-C");
         Assert.Equal(2, a.CompletedSectionsCount);
-        Assert.Equal(0, b.CompletedSectionsCount);
+        Assert.Equal(1, b.CompletedSectionsCount);
+        Assert.Equal(0, c.CompletedSectionsCount);
     }
 
     [Fact]
