961214d85868f07ccbf639d5948efb84b06c39ad
Drew MORGAN <Drew.MORGAN@EDUCATION.GOV.UK>
2025-10-17 10:41:35 +0100

    Fix warnings

--- src/Dfe.PlanTech.Core/Constants/PageIncludeLevelConstants.cs
+++ src/Dfe.PlanTech.Core/Constants/PageIncludeLevelConstants.cs
@@ -2,7 +2,7 @@
 
 public static class PageIncludeLevelConstants
 {
-    public static Dictionary<string, int> IncludeLevelOverrides = new Dictionary<string, int>
+    public static readonly Dictionary<string, int> IncludeLevelOverrides = new Dictionary<string, int>
     {
         { "home", 2 },
         { "dashboard", 2 }
