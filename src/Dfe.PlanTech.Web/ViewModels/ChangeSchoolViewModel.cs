ef932c664d32e1bc158eb908ceccd7e4b818a7c3
jag-nahl-airelogic <jag.nahl@airelogic.com>
2025-10-17 13:52:46 +0100

    exclude view model from coverage

--- src/Dfe.PlanTech.Web/ViewModels/ChangeSchoolViewModel.cs
+++ src/Dfe.PlanTech.Web/ViewModels/ChangeSchoolViewModel.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Dfe.PlanTech.Web.ViewModels
 {
+    [ExcludeFromCodeCoverage]
     public class ChangeSchoolViewModel
     {
         public string SchoolName { get; set; } = null!;
