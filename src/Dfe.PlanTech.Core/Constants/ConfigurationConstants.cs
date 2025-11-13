fb2405342c30d4de6a5e7b542eb7bcc95789be07
Drew MORGAN <Drew.MORGAN@EDUCATION.GOV.UK>
2025-11-13 16:58:47 +0000

    Added new DfE header to all pages. Removed darker header background.

--- src/Dfe.PlanTech.Core/Constants/ConfigurationConstants.cs
+++ src/Dfe.PlanTech.Core/Constants/ConfigurationConstants.cs
@@ -17,6 +17,8 @@ public static class ConfigurationConstants
     public const string ErrorMessages = "ErrorMessages";
     public const string ErrorPages = "ErrorPages";
     public const string GoogleTagManager = "GTM";
+    public const string GovUkFrontend = "GovUkFrontend";
+    public const string GovUkFrontendAspNetCore = "GovUkFrontendAspNetCore";
     public const string MessageRetryHandlingOptions = "MessageRetryHandlingOptions";
     public const string Robots = "Robots";
     public const string ServiceBusOptions = "ServiceBusOptions";
