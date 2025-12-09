00e48f817b053097fa93df0d7e710110f1a8d7ec
jag-nahl-airelogic <jag.nahl@airelogic.com>
2025-12-09 14:50:50 +0000

    added seed test data to .net project

--- tests/Dfe.PlanTech.Web.SeedTestData/SeedData.cs
+++ tests/Dfe.PlanTech.Web.SeedTestData/SeedData.cs
@@ -24,5 +24,34 @@ Insert into [dbo].[user] (dfeSignInRef) Select CONCAT('sign-in-ref-', n) From Ra
         db.Database.ExecuteSql($@"With Range(n) AS (Select 1 union all select n+1 from Range where n < 100)
 Insert into [dbo].[establishment] (establishmentRef, establishmentType, orgName)
 Select CONCAT('Test Ref ', n), 'Test School', CONCAT('Test Establishment ', n) from Range");
+
+        // Seed test MAT group + links
+        db.Database.ExecuteSql($@"
+                                SET NOCOUNT ON;
+
+                                -- Clear any existing test data for this group
+                                DELETE FROM [dbo].[establishmentLink]
+                                WHERE groupUid = '99999';
+
+                                DELETE FROM [dbo].[establishmentGroup]
+                                WHERE uid = '99999';
+
+                                -- Insert test MAT group
+                                INSERT INTO [dbo].[establishmentGroup] (uid, groupName, groupType, groupStatus)
+                                VALUES (
+                                    '99999',
+                                    'DSI TEST Multi-Academy Trust (010)',
+                                    'Multi-academy trust',
+                                    'Open'
+                                );
+
+                                -- Insert test establishments linked to that MAT
+                                INSERT INTO [dbo].[establishmentLink] (groupUid, establishmentName, urn)
+                                VALUES
+                                ('99999', 'DSI TEST Establishment (001) Community School (01)', 2),
+                                ('99999', 'DSI TEST Establishment (001) Miscellaneous (27)', 18),
+                                ('99999', 'DSI TEST Establishment (001) Foundation School (05)', 5);
+                           ");
+
     }
 }
