5f356e5cf8c7ed6fdf2ead9d408c51bc185f59b9
jag-nahl-airelogic <jag.nahl@airelogic.com>
2025-12-15 09:26:57 +0000

    changed to sql constant

--- tests/Dfe.PlanTech.Web.SeedTestData/SeedData.cs
+++ tests/Dfe.PlanTech.Web.SeedTestData/SeedData.cs
@@ -26,40 +26,7 @@ Insert into [dbo].[establishment] (establishmentRef, establishmentType, orgName)
 Select CONCAT('Test Ref ', n), 'Test School', CONCAT('Test Establishment ', n) from Range");
 
         // Seed test MAT group + links
-        db.Database.ExecuteSql($@"
-                                SET NOCOUNT ON;
-
-                                -- Clear any existing test data for this group
-                                DELETE FROM [dbo].[establishmentLink]
-                                WHERE groupUid = '99999';
-
-                                DELETE FROM [dbo].[establishmentGroup]
-                                WHERE uid = '99999';
-
-                                -- Insert test MAT group
-                                INSERT INTO [dbo].[establishmentGroup] (uid, groupName, groupType, groupStatus)
-                                VALUES (
-                                    '99999',
-                                    'DSI TEST Multi-Academy Trust (010)',
-                                    'Multi-academy trust',
-                                    'Open'
-                                );
-
-                                -- Insert test establishments linked to that MAT
-                                INSERT INTO [dbo].[establishmentLink] (groupUid, establishmentName, urn)
-                                VALUES
-                                ('99999', 'DSI TEST Establishment (001) Community School (01)', '00000002'),
-                                ('99999', 'DSI TEST Establishment (001) Miscellaneous (27)', '00000018'),
-                                ('99999', 'DSI TEST Establishment (001) Foundation School (05)', '00000005');
-
-                                -- Insert test establishments
-                                INSERT INTO [dbo].[establishment] (establishmentRef, establishmentType, orgName, groupUid)
-                                VALUES
-                                ('00000002', 'Community School', 'DSI TEST Establishment (001) Community School (01)', null),
-                                ('00000018', 'Miscellaneous','DSI TEST Establishment (001) Miscellaneous (27)', null),
-                                ('00000005', 'Foundation School', 'DSI TEST Establishment (001) Foundation School (05)', null),
-                                ('00000046', null, 'DSI TEST Multi-Academy Trust (010)', '99999');
-                           ");
+        db.Database.ExecuteSqlRaw(SqlConstants.DsiSeedData);
 
     }
 }
