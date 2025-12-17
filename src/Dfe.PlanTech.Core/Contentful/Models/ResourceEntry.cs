032c9e03efb7edc1a02e45a9dc603bb52a3c73ed
gilaineyo <gilaine.young@education.gov.uk>
2025-12-17 11:54:14 +0000

    poc: new types and constants for Resources

---
+++ src/Dfe.PlanTech.Core/Contentful/Models/ResourceEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace Dfe.PlanTech.Core.Contentful.Models;
+
+public class ResourceEntry : ContentfulEntry
+{
+    public string InternalName { get; set; } = null!;
+    public string Key { get; set; } = null!;
+    public string Value { get; init; } = null!;
+    public List<string> Variables { get; init; } = [];
+}
---
+++ src/Dfe.PlanTech.Core/Contentful/Models/ResourceSetEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace Dfe.PlanTech.Core.Contentful.Models;
+
+public class ResourceSetEntry : ContentfulEntry
+{
+    public string InternalName { get; set; } = null!;
+    public string Key { get; set; } = null!;
+    public IEnumerable<ResourceEntry> Resources { get; set; } = [];
+}
