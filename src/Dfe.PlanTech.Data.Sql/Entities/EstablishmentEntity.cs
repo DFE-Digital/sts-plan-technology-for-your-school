c39ab62b76b1c29ca01dd18119f78fd93979d1f1
gilaineyo <gilaine.young@education.gov.uk>
2026-01-05 15:44:46 +0000

    chore: removed unused vars and methods

--- src/Dfe.PlanTech.Data.Sql/Entities/EstablishmentEntity.cs
+++ src/Dfe.PlanTech.Data.Sql/Entities/EstablishmentEntity.cs
@@ -6,10 +6,6 @@ namespace Dfe.PlanTech.Data.Sql.Entities;
 [Table("establishment")]
 public class EstablishmentEntity
 {
-    private string _establishmentRef = null!;
-    private string? _establishmentType;
-    private string _orgName = null!;
-
     public int Id { get; set; }
 
     public string? EstablishmentRef { get; set; }
@@ -23,23 +19,6 @@ public class EstablishmentEntity
     public DateTime DateCreated { get; set; } = DateTime.UtcNow;
 
     public DateTime? DateLastUpdated { get; set; }
-
-    private static string TrimToMax(string value, int maxLengthInclusive)
-    {
-        return value.Length <= maxLengthInclusive
-            ? value
-            : value.AsSpan(0, maxLengthInclusive).ToString();
-    }
-
-    private static string? TrimToMaxOrNull(string? value, int maxLengthInclusive)
-    {
-        if (value is null)
-            return null;
-
-        return value.Length <= maxLengthInclusive
-            ? value
-            : value.AsSpan(0, maxLengthInclusive).ToString();
-    }
 
     public SqlEstablishmentDto AsDto()
     {
