f4dbd08365c5f687483bfa230122624a9e8c2f8b
gilaineyo <gilaine.young@education.gov.uk>
2026-01-28 18:02:55 +0000

    test: print establishment id in script

--- .github/scripts/clear-submission-data-from-db.sql
+++ .github/scripts/clear-submission-data-from-db.sql
@@ -3,7 +3,9 @@ SET NOCOUNT ON
 DECLARE @id AS BIGINT
 
 EXEC [dbo].[testWorkflow]
-  @establishmentRef = N'$ESTABLISHMENT_REF',
+  @establishmentRef = N'$(ESTABLISHMENT_REF)',
   @establishmentId = @id OUTPUT;
 
 SELECT @id AS EstablishmentId;
+
+PRINT N'Data cleared for establishment ID: ' + COALESCE(CONVERT(nvarchar(50), @id), N'NULL');
