361f4400885eae5555cb427a1f5fadf77f37b182
rian.thwaite <rian.thwaite@airelogic.com>
2026-01-28 10:17:17 +0000

    Rename script to imply context Update remove completed column script

--- src/Dfe.PlanTech.DatabaseUpgrader/Scripts/2026/20260120_1200_RemoveSubmissionCompletedColumnAndConstraints.sql
+++ src/Dfe.PlanTech.DatabaseUpgrader/Scripts/2026/20260120_1200_RemoveSubmissionCompletedColumnAndConstraints.sql
@@ -1,16 +1,25 @@
-DECLARE @df sysname;
+SET NOCOUNT ON;
+SET XACT_ABORT ON;
 
-SELECT @df = dc.name
-FROM sys.default_constraints dc
-JOIN sys.columns c
-  ON c.object_id = dc.parent_object_id
- AND c.column_id = dc.parent_column_id
-WHERE dc.parent_object_id = OBJECT_ID('dbo.submission')
-  AND c.name = 'completed';
+IF COL_LENGTH(N'dbo.submission', N'completed') IS NOT NULL
+BEGIN
+    DECLARE @df sysname;
+    DECLARE @sql nvarchar(max);
 
-IF @df IS NOT NULL
-    EXEC('ALTER TABLE dbo.submission DROP CONSTRAINT [' + @df + ']');
+    SELECT @df = dc.name
+    FROM sys.default_constraints AS dc
+    INNER JOIN sys.columns AS c
+        ON c.object_id = dc.parent_object_id
+       AND c.column_id = dc.parent_column_id
+    WHERE dc.parent_object_id = OBJECT_ID(N'dbo.submission', N'U')
+      AND c.name = N'completed';
 
-ALTER TABLE dbo.submission DROP COLUMN completed;
+    IF @df IS NOT NULL
+    BEGIN
+        SET @sql = N'ALTER TABLE dbo.submission DROP CONSTRAINT ' + QUOTENAME(@df) + N';';
+        EXEC sys.sp_executesql @sql;
+    END
 
+    ALTER TABLE dbo.submission DROP COLUMN completed;
+END
 GO
