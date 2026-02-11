97cfa139f323b4152c16f7102a3e348953180bf7
gilaineyo <gilaine.young@education.gov.uk>
2026-02-11 17:49:19 +0000

    feat: adds constants and helper methods for landing page

--- src/Dfe.PlanTech.Core/Constants/ContentfulMicrocopyConstants.cs
+++ src/Dfe.PlanTech.Core/Constants/ContentfulMicrocopyConstants.cs
@@ -14,9 +14,29 @@ public static class ContentfulMicrocopyConstants
     public const string HomeCardStatusContinue = "cardStatusContinue";
     public const string HomeCardStatusViewRecommendations = "cardStatusViewRecommendations";
 
+    // Landing page
+    public const string LandingPageHeader = "landingHeader";
+    public const string LandingPageBackLink = "landingBackLink";
+    public const string LandingPagePrintLink = "landingPrintLink"; // Variable - standard
+    public const string LandingPageInsetIntroNotStarted = "insetIntroNotStarted"; // Variable - standard
+    public const string LandingPageInsetIntroContinue = "insetIntroContinue"; // Variable - standard
+    public const string LandingPageTopicLinkNotStarted = "topicLinkNotStarted"; // Variable - topic
+    public const string LandingPageTopicIntroContinue = "topicIntroContinue"; // Variable - date
+    public const string LandingPageTopicLinkContinue = "topicLinkContinue"; // Variable - topic
+    public const string LandingPageTopicIntroCompleted = "topicIntroCompleted"; // Variables - topic, date
+    public const string LandingPageTopicLinkCompleted = "topicLinkCompleted"; // Variable - topic
+    public const string LandingPageSuccessPanelHeader = "successHeader"; // Variable - topic
+    public const string LandingPageSuccessPanelBody = "successBody";
+
     public static readonly List<string> EmptyFallback = new()
     {
         HomeHeader,
+        LandingPageHeader,
+        LandingPageInsetIntroNotStarted,
+        LandingPageInsetIntroContinue,
+        LandingPageTopicIntroContinue,
+        LandingPageTopicIntroCompleted,
+        LandingPageSuccessPanelBody,
     };
 
     public static readonly List<string> CardsFallback = new()
@@ -25,5 +45,17 @@ public static class ContentfulMicrocopyConstants
         HomeCardStatusMultipleNotStarted,
         HomeCardStatusContinue,
         HomeCardStatusViewRecommendations,
+    };
+
+    public static readonly List<string> TopicLinksFallback = new()
+    {
+        LandingPageTopicLinkNotStarted,
+        LandingPageTopicLinkContinue,
+        LandingPageTopicLinkCompleted
+    };
+
+    public static readonly List<string> PrintLinksFallback = new()
+    {
+        LandingPagePrintLink
     };
 }
--- src/Dfe.PlanTech.Core/Helpers/ContentfulMicrocopyHelper.cs
+++ src/Dfe.PlanTech.Core/Helpers/ContentfulMicrocopyHelper.cs
@@ -1,11 +1,28 @@
 using Dfe.PlanTech.Core.Constants;
 using Dfe.PlanTech.Core.Contentful.Models;
-using System.Resources;
 
 namespace Dfe.PlanTech.Core.Helpers;
 
 public static class ContentfulMicrocopyHelper
 {
+    public static string GetCategoryLandingInsetText(string categoryName, int sectionsCompleted, List<MicrocopyEntry>? microcopy)
+    {
+        var statusText = string.Empty;
+
+        if (sectionsCompleted == 0)
+        {
+            statusText = microcopy?.FirstOrDefault(r => r.Key == ContentfulMicrocopyConstants.LandingPageInsetIntroNotStarted)?.Value
+                ?? ContentfulMicrocopyHelper.GetFallbackText(ContentfulMicrocopyConstants.LandingPageInsetIntroNotStarted);
+        }
+        else
+        {
+            statusText = microcopy?.FirstOrDefault(r => r.Key == ContentfulMicrocopyConstants.LandingPageInsetIntroContinue)?.Value
+               ?? ContentfulMicrocopyHelper.GetFallbackText(ContentfulMicrocopyConstants.LandingPageInsetIntroContinue);
+        }
+
+        return statusText?.Replace("{{standard}}", categoryName) ?? string.Empty;
+    }
+
     public static string GetCardStatusText(int completedSectionCount, int totalSectionCount, List<MicrocopyEntry>? microcopyEntries)
     {
         string intendedText;
@@ -38,6 +55,14 @@ public static class ContentfulMicrocopyHelper
         else if (ContentfulMicrocopyConstants.CardsFallback.Contains(intendedText))
         {
             return "Go to standard";
+        }
+        else if (ContentfulMicrocopyConstants.TopicLinksFallback.Contains(intendedText))
+        {
+            return "Start, continue or view answers for this self-assessment";
+        }
+        else if (ContentfulMicrocopyConstants.PrintLinksFallback.Contains(intendedText))
+        {
+            return "Print all recommendations";
         }
         else
         {
