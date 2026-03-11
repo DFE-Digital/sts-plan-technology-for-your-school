ee9ef66e6a04016b740ce2da10e6c48eddc4e3e5
gilaineyo <gilaine.young@education.gov.uk>
2026-03-11 15:58:54 +0000

    feat: add microcopy to ViewAnswers

--- src/Dfe.PlanTech.Web/ViewModels/ViewAnswersViewModel.cs
+++ src/Dfe.PlanTech.Web/ViewModels/ViewAnswersViewModel.cs
@@ -1,3 +1,4 @@
+using Dfe.PlanTech.Core.Contentful.Models;
 using Dfe.PlanTech.Core.Models;
 
 namespace Dfe.PlanTech.Web.ViewModels
@@ -7,8 +8,8 @@ namespace Dfe.PlanTech.Web.ViewModels
         public DateTime AssessmentCompletedDate { get; set; }
         public string TopicName { get; set; } = string.Empty;
         public List<QuestionWithAnswerModel> Responses { get; set; } = new();
-
         public string CategorySlug { get; set; } = string.Empty;
         public string SectionSlug { get; set; } = string.Empty;
+        public List<MicrocopyEntry>? MicrocopyEntries { get; set; }
     }
 }
