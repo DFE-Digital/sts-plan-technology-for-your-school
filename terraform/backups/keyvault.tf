0655a92c4617ec98b852465b327548c127009be1
rachelgriffiths-al <rachel.griffiths@airelogic.com>
2026-05-13 12:56:44 +0100

    Update keyvault.tf

--- terraform/backups/keyvault.tf
+++ terraform/backups/keyvault.tf
@@ -20,17 +20,8 @@ resource "azurerm_key_vault" "backup" {
 resource "azurerm_key_vault_access_policy" "vault_access_policy_tf" {
   key_vault_id = azurerm_key_vault.backup.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = local.current_user_id
+  object_id    = data.azurerm_client_config.current.object_id
 
   secret_permissions = ["List", "Get", "Set"]
   key_permissions    = ["List", "Get", "Create", "GetRotationPolicy", "SetRotationPolicy", "Delete", "Purge", "UnwrapKey", "WrapKey"]
-}
-
-resource "azurerm_key_vault_access_policy" "vault_access_policy_mi" {
-  key_vault_id = azurerm_key_vault.backup.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_user_assigned_identity.user_assigned_identity.principal_id
-
-  secret_permissions = ["List", "Get"]
-  key_permissions    = ["List", "Get", "WrapKey", "UnwrapKey"]
 }
