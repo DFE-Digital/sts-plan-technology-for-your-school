354951c97d1443d727a67f0662b84898d84935fd
rachelgriffiths-al <rachel.griffiths@airelogic.com>
2026-05-14 13:31:37 +0100

    Update data.tf

--- terraform/container-app/data.tf
+++ terraform/container-app/data.tf
@@ -1,4 +1,4 @@
-data "azurerm_client_config" "current" {}
+#data "azurerm_client_config" "current" {}
 
 
 #created in the shared container app module, to use in shared waf for AFD private link. output was suggested in PR but
