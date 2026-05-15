2de267855def988c20654909e6391e656d6655a1
rachelgriffiths-al <rachel.griffiths@airelogic.com>
2026-05-15 14:26:55 +0100

    try alternate import formatting to prevent clash

--- terraform/alerts/imports/prodimports.tf
+++ terraform/alerts/imports/prodimports.tf
@@ -1,24 +1,24 @@
 import {
-  to = module.activity_alerts.azurerm_monitor_activity_log_alert.this["container_app_environment_change"]
+  to = module.activity_alerts.azurerm_monitor_activity_log_alert.this[\"container_app_environment_change\"]"
   id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Insights/activityLogAlerts/Container App Environment Change"
 }
 
 import {
-  to = module.activity_alerts.azurerm_monitor_activity_log_alert.this["container_app_image_pushed"]
+  to = module.activity_alerts.azurerm_monitor_activity_log_alert.this[\"container_app_image_pushed\"]"
   id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Insights/activityLogAlerts/Container App Image Pushed"
 }
 
 import {
-  to = module.activity_alerts.azurerm_monitor_activity_log_alert.this["container_app_revision_active"]
+  to = module.activity_alerts.azurerm_monitor_activity_log_alert.this[\"container_app_revision_active\"]"
   id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Insights/activityLogAlerts/Container App Revision Active"
 }
 
 import {
-  to = module.activity_alerts.azurerm_monitor_activity_log_alert.this["container_app_revision_active"]
+  to = module.activity_alerts.azurerm_monitor_activity_log_alert.this[\"container_app_revision_active\"]"
   id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Insights/activityLogAlerts/Container App Revision Deactivation Succeeded"
 }
 
 import {
-  to = module.activity_alerts.azurerm_monitor_activity_log_alert.this["container_app_update_succeeded"]
+  to = module.activity_alerts.azurerm_monitor_activity_log_alert.this[\"container_app_update_succeeded\"]"
   id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Insights/activityLogAlerts/Container App Update Succeeded"
 }
