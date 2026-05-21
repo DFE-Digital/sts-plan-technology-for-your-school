6ae81e3bbfbd04f15094098a1dc836f3ae6d32be
rachelgriffiths-al <rachel.griffiths@airelogic.com>
2026-05-21 09:19:46 +0100

    try split apply

--- terraform/container-app/data.tf
+++ terraform/container-app/data.tf
@@ -1,11 +1,3 @@
 # only needed if not using container app shared module as that already contains it. use to pull user id and subscription id.
 #data "azurerm_client_config" "current" {}
 
-
-#created in the shared container app module, to use in shared waf for AFD private link. output was suggested in PR but
-#not merged, so use data here instead.
-
-data "azurerm_container_app_environment" "env" {
-  name                = "${local.resource_prefix}containerapp"
-  resource_group_name = local.resource_prefix
-}
---
+++ terraform/container-app/modules/shared_waf/outputs.tf
@@ -0,0 +1,3 @@
+output "frontdoor_endpoint_host_name" {
+  value = azurerm_cdn_frontdoor_endpoint.waf.host_name
+}
