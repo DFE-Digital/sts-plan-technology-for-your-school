82de80ec947103f0d810e97a8fec187ccc2a98ce
Drew MORGAN <Drew.MORGAN@EDUCATION.GOV.UK>
2026-05-21 10:39:47 +0100

    Add links_edubasealldata to list of files ingested by GIAS updater

--- utils/update-gias-data/src/fetch_data.py
+++ utils/update-gias-data/src/fetch_data.py
@@ -5,13 +5,37 @@ from pandas import Timedelta
 from pathlib import Path
 from tqdm import tqdm
 
-from src.constants import EDUBASE_DOWNLOADS_BASE_URL, CSV_STEMS
+from src.constants import API_URL, CSV_STEMS
 from src.utils import get_file_path, get_logger
 
 logger = get_logger(__name__)
 
+# General
 CHUNK_SIZE = 65_536
 DEFAULT_TIMEOUT_SECONDS = 60
+
+# _check_data_available return values
+DATA_AVAILABLE = 0
+DATA_MAY_BE_UNDER_PREVIOUS_DATE = 1
+EDUBASE_ERROR = 2
+
+
+def _check_data_available(file_date: date) -> int:
+    for stem in CSV_STEMS:
+        file_path = get_file_path(stem, file_date)
+        url = f"{API_URL}/{file_path.name}"
+        try:
+            _download(url, file_path, is_test_download=True)
+        except requests.HTTPError as e:
+            if e.response.status_code == 404:
+                logger.warning(
+                    "No data available for %s, will use yesterday's date", file_date
+                )
+                return DATA_MAY_BE_UNDER_PREVIOUS_DATE
+            else:
+                return EDUBASE_ERROR
+
+    return DATA_AVAILABLE
 
 
 def _download(url: str, dest: Path, is_test_download: bool = False) -> None:
@@ -20,7 +44,6 @@ def _download(url: str, dest: Path, is_test_download: bool = False) -> None:
         request.raise_for_status()
 
         if is_test_download:
-            logger.info("Test download successful, skipping saving")
             return
 
         total = int(request.headers.get("content-length", 0))
@@ -39,22 +62,19 @@ def fetch_and_save_gias_data() -> None:
     """Download the four GIAS CSV files for today's date into DATA_PATH."""
 
     file_date = date.today()
-    files_available = True
+    data_available = _check_data_available(file_date)
 
-    for stem in CSV_STEMS:
-        file_path = get_file_path(stem, file_date)
-        url = f"{EDUBASE_DOWNLOADS_BASE_URL}/{file_path.name}"
-        try:
-            _download(url, file_path, is_test_download=True)
-        except requests.HTTPError as e:
-            if e.response.status_code == 404:
-                logger.warning(
-                    "No data available for %s, will use yesterday's date", file_date
-                )
-                file_date -= Timedelta(days=1)
-                break
-            else:
-                raise
+    if data_available == DATA_MAY_BE_UNDER_PREVIOUS_DATE:
+        file_date -= Timedelta(days=1)
+        data_available = _check_data_available(file_date)
+
+    if data_available != DATA_AVAILABLE:
+        if data_available == DATA_MAY_BE_UNDER_PREVIOUS_DATE:
+            logger.warning(
+                "Checked for data under today's date and yesterday's date, but it is not available under either"
+            )
+        logger.error("Edubase service error, aborting download")
+        return
 
     for stem in CSV_STEMS:
         file_path = get_file_path(stem, file_date)
@@ -62,5 +82,5 @@ def fetch_and_save_gias_data() -> None:
             logger.info("%s already exists, skipping download", file_path.name)
             continue
 
-        url = f"{EDUBASE_DOWNLOADS_BASE_URL}/{file_path.name}"
+        url = f"{API_URL}/{file_path.name}"
         _download(url, file_path)
