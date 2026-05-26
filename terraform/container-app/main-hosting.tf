0f77a2442515f31b0af42f54cd612db4b92cf818
rachelgriffiths-al <rachel.griffiths@airelogic.com>
2026-05-26 13:03:58 +0100

    Update main-hosting.tf

--- terraform/container-app/main-hosting.tf
+++ terraform/container-app/main-hosting.tf
@@ -1,5 +1,6 @@
 module "main_hosting" {
   source = "github.com/DFE-Digital/terraform-azurerm-container-apps-hosting?ref=v1.20.0"
+
   #source = "github.com/DFE-Digital/terraform-azurerm-container-apps-hosting?ref=v2.6.3"
   depends_on = [azurerm_resource_group.app_rg, azurerm_container_registry.acr_notshared]
   ###########
