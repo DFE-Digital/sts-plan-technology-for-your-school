95708c8aa86c21653b1d0ed78a4e3db89148bbfc
rachelgriffiths-al <rachel.griffiths@airelogic.com>
2026-05-28 16:19:42 +0100

    updates to use metric alerts and import more alerts

--- terraform/alerts/imports/prodimports.tf
+++ terraform/alerts/imports/prodimports.tf
@@ -26,16 +26,26 @@
 #}
 
 import {
-  to = azurerm_monitor_activity_log_alert.this["dependency_calls_failed"]
-  id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Insights/activityLogAlerts/Dependency calls failed"
+  to = azurerm_monitor_metric_alert.this["dependency_calls_failed"]
+  id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Insights/metricAlerts/Dependency calls failed"
 }
 
 import {
-  to = azurerm_monitor_activity_log_alert.this["http_requests_failed"]
-  id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Insights/activityLogAlerts/HTTP Requests Failed"
+  to = azurerm_monitor_metric_alert.this["http_requests_failed"]
+  id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Insights/metricAlerts/HTTP Requests Failed"
 }
 
 import {
-  to = azurerm_monitor_activity_log_alert.this["unhandled_exceptions_thrown"]
-  id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Insights/activityLogAlerts/Unhandled exceptions thrown"
+  to = azurerm_monitor_metric_alert.this["unhandled_exceptions_thrown"]
+  id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Insights/metricAlerts/Unhandled exceptions thrown"
+}
+
+import {
+  to = azurerm_monitor_activity_log_alert.this["terraform_deployment_succeeded"]
+  id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Insights/activityLogAlerts/Terraform Deployment Succeeded"
+}
+
+import {
+  to = azurerm_monitor_activity_log_alert.this["terraform_deployment_failed"]
+  id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Insights/activityLogAlerts/Terraform Deployment Failed"
 }
