1f1e72223d6a3a227938ac227d2aa2a5a8232c47
rachelgriffiths-al <rachel.griffiths@airelogic.com>
2026-06-02 13:25:08 +0100

    Update database.networking.tf

--- terraform/container-app/database.networking.tf
+++ terraform/container-app/database.networking.tf
@@ -15,13 +15,11 @@ resource "azurerm_private_endpoint" "database" {
   tags                          = local.tags
 
   private_dns_zone_group {
-    count = local.create_db_network ? 1 : 0
     name                 = "default"
     private_dns_zone_ids = [azurerm_private_dns_zone.database[0].id]
   }
 
   private_service_connection {
-    count = local.create_db_network ? 1 : 0
     is_manual_connection           = false
     name                           = local.az_sql_vnet.endpoint_name
     private_connection_resource_id = data.azurerm_mssql_server.database.id
