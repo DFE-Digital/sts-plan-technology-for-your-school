4d46665b8fc96dc9ac9857b3c5cf308233cc1073
rachelgriffiths-al <rachel.griffiths@airelogic.com>
2026-06-02 13:22:48 +0100

    Update database.networking.tf

--- terraform/container-app/database.networking.tf
+++ terraform/container-app/database.networking.tf
@@ -1,12 +1,12 @@
 resource "azurerm_private_dns_zone" "database" {
-  count = create_db_network ? 1 : 0
+  count = local.create_db_network ? 1 : 0
   name                = local.az_sql_vnet.dns_zone_name
   resource_group_name = local.resource_group_name
   tags                = local.tags
 }
 
 resource "azurerm_private_endpoint" "database" {
-  count = create_db_network ? 1 : 0
+  count = local.create_db_network ? 1 : 0
   custom_network_interface_name = local.az_sql_vnet.dns_zone_name
   location                      = local.azure_location
   name                          = local.az_sql_vnet.endpoint_name
@@ -15,13 +15,13 @@ resource "azurerm_private_endpoint" "database" {
   tags                          = local.tags
 
   private_dns_zone_group {
-    count = create_db_network ? 1 : 0
+    count = local.create_db_network ? 1 : 0
     name                 = "default"
     private_dns_zone_ids = [azurerm_private_dns_zone.database[0].id]
   }
 
   private_service_connection {
-    count = create_db_network ? 1 : 0
+    count = local.create_db_network ? 1 : 0
     is_manual_connection           = false
     name                           = local.az_sql_vnet.endpoint_name
     private_connection_resource_id = data.azurerm_mssql_server.database.id
@@ -30,7 +30,7 @@ resource "azurerm_private_endpoint" "database" {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "database_default" {
-  count = create_db_network ? 1 : 0
+  count = local.create_db_network ? 1 : 0
   name                  = "default_vnet"
   resource_group_name   = local.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.database[0].name
