b40279e6c5e0c167f0880d49c55c1c2fcef793a5
gilaineyo <gilaine.young@education.gov.uk>
2026-06-09 16:48:06 +0100

    feat: add ViewModel for selecting schools to assess

--- src/Dfe.PlanTech.Web/ViewModels/GroupsSelectSchoolsToAssessViewModel.cs
+++ src/Dfe.PlanTech.Web/ViewModels/GroupsSelectSchoolsToAssessViewModel.cs
@@ -1,3 +1,6 @@
+using Dfe.PlanTech.Core.Contentful.Models;
+using Dfe.PlanTech.Core.Models;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Dfe.PlanTech.Web.ViewModels;
@@ -5,5 +8,7 @@ namespace Dfe.PlanTech.Web.ViewModels;
 [ExcludeFromCodeCoverage]
 public class GroupsSelectSchoolsToAssessViewModel
 {
-    
+    public string? CategorySlug { get; init; }
+    public required QuestionnaireSectionEntry Section { get; init; }
+    public required List<SubmissionInformationModel> SchoolSubmissionInfo { get; init; }
 }
