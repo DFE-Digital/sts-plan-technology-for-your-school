43e2c85ff684038ac659e2c4840644d27a26fb56
rachelgriffiths-al <rachel.griffiths@airelogic.com>
2026-06-12 11:12:02 +0100

    try add roles

---
+++ terraform/container-app/roles.tf
@@ -0,0 +1,21 @@
+
+resource "azurerm_role_assignment" "github_actions_kv_secretsadmin" {
+  scope                = azurerm_key_vault.vault.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = var.github_actions_principal_id
+  principal_type       = "ServicePrincipal"
+}
+
+resource "azurerm_role_assignment" "mi-crypto" {
+  scope                = azurerm_key_vault.vault.id
+  role_definition_name = "Key Vault Crypto Service Encryption User"
+  principal_id         = [azurerm_user_assigned_identity.user_assigned_identity.id]
+  principal_type       = "ServicePrincipal"
+}
+
+resource "azurerm_role_assignment" "mi-secretsuser" {
+  scope                = azurerm_key_vault.vault.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = [azurerm_user_assigned_identity.user_assigned_identity.id]
+  principal_type       = "ServicePrincipal"
+}
