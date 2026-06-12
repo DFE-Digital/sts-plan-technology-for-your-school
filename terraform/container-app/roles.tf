627868dc674212b972f1b978df4cbfa8a929e7e7
rachelgriffiths-al <rachel.griffiths@airelogic.com>
2026-06-12 11:14:33 +0100

    Update roles.tf

--- terraform/container-app/roles.tf
+++ terraform/container-app/roles.tf
@@ -9,13 +9,13 @@ resource "azurerm_role_assignment" "github_actions_kv_secretsadmin" {
 resource "azurerm_role_assignment" "mi-crypto" {
   scope                = azurerm_key_vault.vault.id
   role_definition_name = "Key Vault Crypto Service Encryption User"
-  principal_id         = [azurerm_user_assigned_identity.user_assigned_identity.id]
+  principal_id         = azurerm_user_assigned_identity.user_assigned_identity.id
   principal_type       = "ServicePrincipal"
 }
 
 resource "azurerm_role_assignment" "mi-secretsuser" {
   scope                = azurerm_key_vault.vault.id
   role_definition_name = "Key Vault Secrets User"
-  principal_id         = [azurerm_user_assigned_identity.user_assigned_identity.id]
+  principal_id         = azurerm_user_assigned_identity.user_assigned_identity.id
   principal_type       = "ServicePrincipal"
 }
