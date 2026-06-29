d3d059992252eab2b6349f4ed907ec5f04325a71
rachelgriffiths-al <rachel.griffiths@airelogic.com>
2026-06-29 13:41:18 +0100

    Update outputs.tf

--- terraform/modules/dns_zone/outputs.tf
+++ terraform/modules/dns_zone/outputs.tf
@@ -1,5 +1,5 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.dns-zone[0].name
+  value = length(azurerm_resource_group.dns-zone) > 0 ? azurerm_resource_group.dns-zone[0].name : var.existing_resource_group
 }
 output "primary-zone" {
   value = {
