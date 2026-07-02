e31430a6838a4515127153dac39a860c1721e17b
rachelgriffiths-al <rachel.griffiths@airelogic.com>
2026-07-02 13:48:25 +0100

    Create outputs.tf

---
+++ terraform/container-app/outputs.tf
@@ -0,0 +1,11 @@
+output "frontdoor_profile_id" {
+  value = module.waf.frontdoor_profile_id
+}
+
+output "frontdoor_endpoint_id" {
+  value = module.waf.frontdoor_endpoint_id
+}
+
+output "frontdoor_endpoint_host_name" {
+  value = module.waf.frontdoor_endpoint_host_name
+}
