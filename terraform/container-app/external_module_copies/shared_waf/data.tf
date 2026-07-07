2fb3ae7f9a135dfa7f89659e2445f55d0aec25a8
rachelgriffiths-al <rachel.griffiths@airelogic.com>
2026-07-07 16:46:54 +0100

    e2e 1

--- terraform/container-app/external_module_copies/shared_waf/data.tf
+++ terraform/container-app/external_module_copies/shared_waf/data.tf
@@ -17,11 +17,3 @@ data "azurerm_virtual_network" "vnet" {
   name                = each.value.name
   resource_group_name = each.value.resource_group_name
 }
-
-#created in the shared container app module, to use in shared waf for AFD private link. output was suggested in PR but
-#not merged, so use data here instead.
-
-data "azurerm_container_app_environment" "env" {
-  name                = "${local.resource_prefix}containerapp"
-  resource_group_name = local.existing_resource_group
-}
