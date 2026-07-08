544f0517e8e8f8cec2613af11f9da7dcb87de0f8
rachelgriffiths-al <rachel.griffiths@airelogic.com>
2026-07-08 09:54:04 +0100

    separate out the webhook setup

--- terraform/container-app/outputs.tf
+++ terraform/container-app/outputs.tf
@@ -13,3 +13,7 @@ output "frontdoor_endpoint_id" {
 output "frontdoor_endpoint_host_name" {
   value = module.waf.frontdoor_endpoint_host_name
 }
+
+output "cdn_hostname" {
+  value = local.cdn_hostname
+}
