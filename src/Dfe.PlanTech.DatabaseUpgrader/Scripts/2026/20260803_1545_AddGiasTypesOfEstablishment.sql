957b8edf948661d429b0cd676f165b761736a875
Drew MORGAN <Drew.MORGAN@EDUCATION.GOV.UK>
2026-09-02 12:24:44 +0100

    fix: Only make database changes if necessary

--- src/Dfe.PlanTech.DatabaseUpgrader/Scripts/2026/20260803_1545_AddGiasTypesOfEstablishment.sql
+++ src/Dfe.PlanTech.DatabaseUpgrader/Scripts/2026/20260803_1545_AddGiasTypesOfEstablishment.sql
@@ -8,11 +8,34 @@ BEGIN
 END;
 GO
 
-ALTER TABLE gias.establishment
-ADD [typeOfEstablishmentCode] INT NULL;
+IF COL_LENGTH(N'gias.establishment', N'typeOfEstablishmentCode') IS NULL
+BEGIN
+    ALTER TABLE gias.establishment
+    ADD [typeOfEstablishmentCode] INT NULL;
+END
+ELSE IF EXISTS (
+    SELECT 1
+    FROM sys.columns c
+    INNER JOIN sys.types t ON t.user_type_id = c.user_type_id
+    WHERE c.object_id = OBJECT_ID(N'gias.establishment')
+      AND c.name = N'typeOfEstablishmentCode'
+      AND (t.name <> N'int' OR c.is_nullable <> 1)
+)
+BEGIN
+    ALTER TABLE gias.establishment
+    ALTER COLUMN [typeOfEstablishmentCode] INT NULL;
+END;
 GO
 
-ALTER TABLE gias.establishment
-ADD CONSTRAINT FK_giasEstablishment_typeOfEstablishment
-    FOREIGN KEY (typeOfEstablishmentCode) REFERENCES gias.typeOfEstablishment (typeOfEstablishmentCode);
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.foreign_keys
+    WHERE name = N'FK_giasEstablishment_typeOfEstablishment'
+      AND parent_object_id = OBJECT_ID(N'gias.establishment')
+)
+BEGIN
+    ALTER TABLE gias.establishment
+    ADD CONSTRAINT FK_giasEstablishment_typeOfEstablishment
+        FOREIGN KEY (typeOfEstablishmentCode) REFERENCES gias.typeOfEstablishment (typeOfEstablishmentCode);
+END;
 GO
